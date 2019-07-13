36ac54682bf8b4293cb9c9c168ee9d3848094899
Will Molloy <willjoemolloy@gmail.com>
2019-07-13 15:32:06 +1200

    leetcode 6
    GitOrigin-RevId: ba12911103060d4904ba6fcc97f198b5aa9f5579

--- leetcode/src/main/java/com/wilmol/leetcode/problems/medium/P6ZigZagConversion.java
+++ leetcode/src/main/java/com/wilmol/leetcode/problems/medium/P6ZigZagConversion.java
@@ -1,14 +1,41 @@
 package com.wilmol.leetcode.problems.medium;
 
+import java.util.Arrays;
+
 /**
  * Created by Will on 2019-07-13 at 14:45.
  *
  * <p><a
  * href=https://leetcode.com/problems/zigzag-conversion>https://leetcode.com/problems/zigzag-conversion</a>
  *
- * <p>Runtime: O()
+ * <p>Runtime: O(n) - one pass on characters in {@code s}
  */
-final class P6ZigZagConversion {
+class P6ZigZagConversion {
 
-  private P6ZigZagConversion() {}
+  public String convert(String s, int numRows) {
+    if (numRows <= 1) {
+      return s;
+    }
+    int i = 0;
+    boolean forward = true;
+    String[] array = new String[numRows];
+    Arrays.fill(array, "");
+
+    // Go until n (numRows), back and forth
+    // storing in array of strings until input string is depleted
+    // then join the array of strings
+    // e.g. PAYPALISHIRING -> PAHNAPLSIIGYIR
+    // i = 0: P   A   H   N
+    // i = 1: A P L S I I G
+    // i = 2: Y   I   R
+    // i,e. i = (0, 1, 2, 1, 0, 1, 2, 1, 0, 1, 2, 1, 0, 1)
+    for (char c : s.toCharArray()) {
+      array[i] += c;
+      i += forward ? 1 : -1;
+      if (i == numRows - 1 || i == 0) {
+        forward = !forward;
+      }
+    }
+    return String.join("", array);
+  }
 }
