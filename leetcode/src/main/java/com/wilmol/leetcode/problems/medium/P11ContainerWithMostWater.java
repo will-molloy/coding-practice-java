bf7b265f0caee665694e8246f0ffc867dc759ef1
Will Molloy <willjoemolloy@gmail.com>
2019-07-15 20:26:30 +1200

    leetcode 11
    GitOrigin-RevId: d896e3cfe886bfde3a259ab7c5d24f9fab91e19c

---
+++ leetcode/src/main/java/com/wilmol/leetcode/problems/medium/P11ContainerWithMostWater.java
@@ -0,0 +1,34 @@
+package com.wilmol.leetcode.problems.medium;
+
+/**
+ * Created by Will on 2019-07-15 at 20:05.
+ *
+ * <p><a
+ * href=https://leetcode.com/problems/container-with-most-water>https://leetcode.com/problems/container-with-most-water</a>
+ *
+ * <p>Runtime: O(n) single pass on each element (two pointers), O(1) memory too
+ */
+class P11ContainerWithMostWater {
+
+  public int maxArea(int[] height) {
+    int maxArea = 0;
+    int l = 0;
+    int r = height.length - 1;
+
+    while (l < r) {
+      int y;
+      int x = r - l;
+      // always move in the minimum of the two heights (greedy)
+      // have to use the minimum height as per the problem definition (must form a rectangle)
+      if (height[l] < height[r]) {
+        y = height[l];
+        l += 1;
+      } else {
+        y = height[r];
+        r -= 1;
+      }
+      maxArea = Math.max(x * y, maxArea);
+    }
+    return maxArea;
+  }
+}
