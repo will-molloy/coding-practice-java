24fc08e2dda9acdb1cec48474266194700a9661c
wilmol <willjoemolloy@gmail.com>
2019-07-17 21:17:46 +1200

    deprecate javatuples
    - since apache commons has it
    - also bad practice to use many tuples in Java...
    
    GitOrigin-RevId: e17c9e83cbe2399fc164edfeff7c01683996e8b2

--- leetcode/src/main/java/com/wilmol/leetcode/problemset/concurrency/P1114PrintInOrder.java
+++ leetcode/src/main/java/com/wilmol/leetcode/problemset/concurrency/P1114PrintInOrder.java
@@ -3,11 +3,28 @@ package com.wilmol.leetcode.problemset.concurrency;
 /**
  * Created by will on 2019-07-17 at 8:57 PM.
  *
- * <p><a href=https://leetcode.com/problems/print-in-order>https://leetcode.com/problems/print-in-order</a>
+ * <p><a
+ * href=https://leetcode.com/problems/print-in-order>https://leetcode.com/problems/print-in-order</a>
  *
  * <p>Runtime: O(n)
  */
-class P1114PrintInOrder
-{
+class P1114PrintInOrder {
 
+  private volatile int ran;
+
+  public void first(Runnable printFirst) throws InterruptedException {
+    printFirst.run();
+    ran = 1;
+  }
+
+  public void second(Runnable printSecond) throws InterruptedException {
+    while (ran < 1) {}
+    printSecond.run();
+    ran = 2;
+  }
+
+  public void third(Runnable printThird) throws InterruptedException {
+    while (ran < 2) {}
+    printThird.run();
+  }
 }
---
+++ leetcode/src/test/java/com/wilmol/leetcode/problemset/concurrency/P1114PrintInOrderTest.java
@@ -0,0 +1,10 @@
+package com.wilmol.leetcode.problemset.concurrency;
+
+import org.junit.jupiter.api.Test;
+
+/** Created by will on 2019-07-17 at 9:04 PM. */
+class P1114PrintInOrderTest {
+
+  @Test
+  void oneTwoThree() {}
+}
