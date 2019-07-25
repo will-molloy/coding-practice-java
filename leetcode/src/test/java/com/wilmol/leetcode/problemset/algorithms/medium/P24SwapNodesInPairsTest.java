36476d23c4e779fabe1452eed2892072f90cb46b
Will Molloy <willjoemolloy@gmail.com>
2019-07-25 20:07:50 +1200

    leetcode 24 swap nodes in pairs (medium)
    - actually really easy
    
    GitOrigin-RevId: e93f472d3ba589dac72211af63e48968379ac862

---
+++ leetcode/src/test/java/com/wilmol/leetcode/problemset/algorithms/medium/P24SwapNodesInPairsTest.java
@@ -0,0 +1,39 @@
+package com.wilmol.leetcode.problemset.algorithms.medium;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.wilmol.leetcode.common.ListNode;
+import org.junit.jupiter.api.Test;
+
+/** Created by Will on 2019-07-25 at 19:58. */
+class P24SwapNodesInPairsTest {
+
+  private final P24SwapNodesInPairs fun = new P24SwapNodesInPairs();
+
+  @Test
+  void example() {
+    assertThat(fun.swapPairs(ListNode.of(1, 2, 3, 4))).isEqualTo(ListNode.of(2, 1, 4, 3));
+  }
+
+  @Test
+  void manyNodes() {
+    assertThat(fun.swapPairs(ListNode.of(1, 2, 3, 4, 5, 6)))
+        .isEqualTo(ListNode.of(2, 1, 4, 3, 6, 5));
+  }
+
+  @Test
+  void singleNode() {
+    assertThat(fun.swapPairs(ListNode.of(1))).isEqualTo(ListNode.of(1));
+  }
+
+  @Test
+  void oddAmountOfRows() {
+    assertThat(fun.swapPairs(ListNode.of(1, 2, 3))).isEqualTo(ListNode.of(2, 1, 3));
+    assertThat(fun.swapPairs(ListNode.of(1, 2, 3, 4, 5))).isEqualTo(ListNode.of(2, 1, 4, 3, 5));
+  }
+
+  @Test
+  void noNodes() {
+    assertThat(fun.swapPairs(ListNode.of())).isEqualTo(ListNode.of());
+  }
+}
