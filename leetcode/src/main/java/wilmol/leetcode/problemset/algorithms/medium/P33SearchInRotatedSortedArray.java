acad146086f5c55a8697f2b3de93735bd49f3621
Will Molloy <willjoemolloy@gmail.com>
2019-08-13 21:24:13 +1200

    leetcode P33SearchInRotatedSortedArray (medium)
    - best to break problem down
    - i.e. findRotation -> rotatedSearch
    - rather than using complicated concepts like 'pivot' elements
    
    GitOrigin-RevId: 73093755b94d784f2e06279b87b0055d2b04cb11

---
+++ leetcode/src/main/java/wilmol/leetcode/problemset/algorithms/medium/P33SearchInRotatedSortedArray.java
@@ -0,0 +1,61 @@
+package wilmol.leetcode.problemset.algorithms.medium;
+
+/**
+ * Created by wilmol on 2019-08-13.
+ *
+ * <p><a
+ * href=https://leetcode.com/problems/search-in-rotated-sorted-array>https://leetcode.com/problems/search-in-rotated-sorted-array</a>
+ *
+ * <p>Runtime: O(log n)
+ */
+class P33SearchInRotatedSortedArray {
+
+  public int search(int[] nums, int target) {
+    int rotation = getLeftRotation(nums);
+    return rotatedSearch(nums, target, rotation);
+  }
+
+  private int getLeftRotation(int[] nums) {
+    int left = 0;
+    int right = nums.length - 1;
+
+    // find index of smallest value (using binary search)
+    // this is the number of places rotated (to the left) since un-rotated it would have index 0
+    while (left < right) {
+      int mid = (left + right) / 2;
+
+      if (nums[mid] > nums[right]) {
+        // rotated case, smaller values are on right side
+        left = mid + 1;
+      } else {
+        // normal case, smaller values are on left side
+        right = mid;
+      }
+    }
+    // index of smallest value is in both left and right
+    return left;
+  }
+
+  private int rotatedSearch(int[] nums, int target, int leftRotation) {
+    int left = 0;
+    int right = nums.length - 1;
+
+    // usual binary search, accounting for rotation
+    while (left <= right) {
+      int mid = (left + right) / 2;
+      int realMid = (mid + leftRotation) % nums.length;
+
+      if (nums[realMid] > target) {
+        // target is on left side
+        right = mid - 1;
+      } else if (nums[realMid] < target) {
+        // target is on right side
+        left = mid + 1;
+      } else {
+        // target found
+        return realMid;
+      }
+    }
+    return -1;
+  }
+}
---
+++ leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/medium/P33SearchInRotatedSortedArrayTest.java
@@ -0,0 +1,36 @@
+package wilmol.leetcode.problemset.algorithms.medium;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+/** Created by wilmol on 2019-08-13. */
+class P33SearchInRotatedSortedArrayTest {
+
+  private final P33SearchInRotatedSortedArray fun = new P33SearchInRotatedSortedArray();
+
+  @Test
+  void example1() {
+    assertThat(fun.search(new int[] {4, 5, 6, 7, 0, 1, 2}, 0)).isEqualTo(4);
+  }
+
+  @Test
+  void example2() {
+    assertThat(fun.search(new int[] {4, 5, 6, 7, 0, 1, 2}, 3)).isEqualTo(-1);
+  }
+
+  @Test
+  void notRotated() {
+    assertThat(fun.search(new int[] {0, 1, 2, 3, 4, 5, 6, 7}, 5)).isEqualTo(5);
+  }
+
+  @Test
+  void largerGapsBetweenElements() {
+    assertThat(fun.search(new int[] {40, 500, 20, 25, 26, 31}, 40)).isEqualTo(0);
+  }
+
+  @Test
+  void emptyArray() {
+    assertThat(fun.search(new int[0], 1)).isEqualTo(-1);
+  }
+}
