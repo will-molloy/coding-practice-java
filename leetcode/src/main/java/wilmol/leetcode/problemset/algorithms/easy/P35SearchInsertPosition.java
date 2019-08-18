924f9a3b411ae728de278f758677c2a78fa1824f
Will Molloy <willjoemolloy@gmail.com>
2019-08-18 16:08:09 +1200

    leetcode P35SearchInsertPosition (easy)
    - more like medium
    - maybe easy cause it accepts O(n) rather than log(n)
    
    GitOrigin-RevId: a7e029e5bc59096f9f70a83a273823c1d6db2298

---
+++ leetcode/src/main/java/wilmol/leetcode/problemset/algorithms/easy/P35SearchInsertPosition.java
@@ -0,0 +1,60 @@
+package wilmol.leetcode.problemset.algorithms.easy;
+
+import java.util.Arrays;
+
+/**
+ * Created by wilmol on 2019-08-18.
+ *
+ * <p><a
+ * href=https://leetcode.com/problems/search-insert-position>https://leetcode.com/problems/search-insert-position</a>
+ *
+ * <p>Runtime: O(log n) (given its under easy, maybe can just solve in O(n)...)
+ */
+class P35SearchInsertPosition {
+
+  public int searchInsert(int[] nums, int target) {
+    // normal binary search
+    // but check target would fit between mid and next/prev element i.e.
+    // mid < target && mid + 1 > target -> return mid + 1 (insert after smaller element)
+    // mid > target && mid - 1 < target -> return mid (insert at larger element)
+
+    // check less than min or greater than max
+    if (target <= nums[0]) {
+      return 0;
+    }
+    if (target > nums[nums.length - 1]) {
+      return nums.length;
+    }
+
+    int low = 0;
+    int high = nums.length - 1;
+    while (low <= high) {
+      int mid = (low + high) >>> 1;
+
+      if (nums[mid] < target) {
+        // check target would fit here
+        if (mid < nums.length - 1 && nums[mid + 1] > target) {
+          return mid + 1;
+        }
+        // target on right
+        low = mid + 1;
+
+      } else if (nums[mid] > target) {
+        // check target would fit here
+        if (mid > 0 && nums[mid - 1] < target) {
+          return mid;
+        }
+        // target on left
+        high = mid - 1;
+
+      } else {
+        // target found
+        return mid;
+      }
+    }
+    // should never reach here
+    throw new AssertionError(
+        String.format(
+            "Failed to find insert position. nums=%s, target=%d", Arrays.toString(nums), target));
+  }
+}
---
+++ leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/easy/P35SearchInsertPositionTest.java
@@ -0,0 +1,51 @@
+package wilmol.leetcode.problemset.algorithms.easy;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+/** Created by wilmol on 2019-08-18. */
+class P35SearchInsertPositionTest {
+
+  private final P35SearchInsertPosition fun = new P35SearchInsertPosition();
+
+  @Test
+  void example1EqualToSomeElementOnRight() {
+    assertThat(fun.searchInsert(new int[] {1, 3, 5, 6}, 5)).isEqualTo(2);
+  }
+
+  @Test
+  void example2LeftSide() {
+    assertThat(fun.searchInsert(new int[] {1, 3, 5, 6}, 2)).isEqualTo(1);
+  }
+
+  @Test
+  void example3NewMax() {
+    assertThat(fun.searchInsert(new int[] {1, 3, 5, 6}, 7)).isEqualTo(4);
+  }
+
+  @Test
+  void example4NewMin() {
+    assertThat(fun.searchInsert(new int[] {1, 3, 5, 6}, 0)).isEqualTo(0);
+  }
+
+  @Test
+  void equalToMin() {
+    assertThat(fun.searchInsert(new int[] {1, 3, 5}, 1)).isEqualTo(0);
+  }
+
+  @Test
+  void equalToMax() {
+    assertThat(fun.searchInsert(new int[] {1, 3, 5}, 5)).isEqualTo(2);
+  }
+
+  @Test
+  void equalToSomeElementOnLeft() {
+    assertThat(fun.searchInsert(new int[] {1, 2, 3, 4, 5, 10}, 2)).isEqualTo(1);
+  }
+
+  @Test
+  void rightSide() {
+    assertThat(fun.searchInsert(new int[] {1, 2, 3, 4, 5, 10}, 7)).isEqualTo(5);
+  }
+}
