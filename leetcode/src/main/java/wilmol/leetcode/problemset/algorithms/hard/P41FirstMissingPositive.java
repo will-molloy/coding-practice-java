e1c764431b32e060d3699b0a047d9047cae72973
Will Molloy <willjoemolloy@gmail.com>
2019-08-26 21:46:53 +1200

    refactor leetcode P41FirstMissingPositive
    - add some more tests
    
    GitOrigin-RevId: ae971f23a732dadaf3e7f62fa43fc6bc4c70a812

--- leetcode/src/main/java/wilmol/leetcode/problemset/algorithms/hard/P41FirstMissingPositive.java
+++ leetcode/src/main/java/wilmol/leetcode/problemset/algorithms/hard/P41FirstMissingPositive.java
@@ -6,19 +6,19 @@ package wilmol.leetcode.problemset.algorithms.hard;
  * <p><a
  * href=https://leetcode.com/problems/first-missing-positive>https://leetcode.com/problems/first-missing-positive</a>
  *
- * <p>Runtime: O(n), O(1) extra memory
+ * <p>Runtime: O(n), O(1) extra memory (TODO does decrementing index break O(n)??)
  */
 class P41FirstMissingPositive {
 
   public int firstMissingPositive(int[] nums) {
     // first pass: swap positive values to their position ('sort' them for this problem)
     // i.e. index 0 = 1, index 1 = 2...
-    // works because there is at most 'nums.length' positive values
+    // works because there is at most 'n' positive values, dont care about smaller or larger values
     for (int i = 0; i < nums.length; i++) {
       int num = nums[i];
       if (num > 0 && num < nums.length && i != num - 1) {
         swap(nums, num - 1, i);
-        // may have swapped a value out of position, so decrement the index
+        // may have swapped an element out of position, so decrement the index to check it
         // (check not same value else infinite loop)
         if (nums[num - 1] != nums[i]) {
           i -= 1;
--- leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/hard/P41FirstMissingPositiveTest.java
+++ leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/hard/P41FirstMissingPositiveTest.java
@@ -25,8 +25,13 @@ class P41FirstMissingPositiveTest {
   }
 
   @Test
-  void smallerValuesAtStart() {
-    assertThat(fun.firstMissingPositive(new int[] {3, 2, 1})).isEqualTo(4);
+  void noneMissingDescendingOrder() {
+    assertThat(fun.firstMissingPositive(new int[] {4, 3, 2, 1})).isEqualTo(5);
+  }
+
+  @Test
+  void noneMissingAscendingOrder() {
+    assertThat(fun.firstMissingPositive(new int[] {1, 2, 3, 4})).isEqualTo(5);
   }
 
   @Test
@@ -35,17 +40,25 @@ class P41FirstMissingPositiveTest {
   }
 
   @Test
-  void noneMissingInOrder() {
-    assertThat(fun.firstMissingPositive(new int[] {1, 2, 3, 4})).isEqualTo(5);
+  void onlyOnes() {
+    assertThat(fun.firstMissingPositive(new int[] {1, 1})).isEqualTo(2);
   }
 
   @Test
-  void naiveSwapPutsElementsOutOfOrder() {
+  void onlyTwos() {
+    assertThat(fun.firstMissingPositive(new int[] {2, 2})).isEqualTo(1);
+  }
+
+  @Test
+  void naiveSwapPutsSmallerElementOutOfOrder() {
     assertThat(fun.firstMissingPositive(new int[] {-1, 4, 2, 1, 9, 10})).isEqualTo(3);
   }
 
   @Test
-  void onlyOnes() {
-    assertThat(fun.firstMissingPositive(new int[] {1, 1})).isEqualTo(2);
+  void naiveSwapPutsLargerElementOutOfOrder() {
+    assertThat(
+            fun.firstMissingPositive(
+                new int[] {3, 4, 2, 2, 9, 5, 1, 13, -1, -1, -4, 3, 15, -10, 6, 10}))
+        .isEqualTo(7);
   }
 }
