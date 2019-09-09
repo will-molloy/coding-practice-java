11a8a5cfd6bc7297f7eb34c90767685eff9ab834
Will Molloy <willjoemolloy@gmail.com>
2019-09-09 20:27:31 +1200

    hackerrank IPK search HashTablesIceCreamParlor (medium)
    - actually leetcode easy (disguised two sum problem)
    
    GitOrigin-RevId: 4ac93ba53a7efa8d277a5ca308a70cd272bfe7b5

---
+++ hackerrank/src/test/java/wilmol/hackerrank/interviewpreparationkit/search/medium/HashTablesIceCreamParlorTest.java
@@ -0,0 +1,41 @@
+package wilmol.hackerrank.interviewpreparationkit.search.medium;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+/** Created by wilmol on 2019-09-09. */
+class HashTablesIceCreamParlorTest {
+
+  @Test
+  void example1() {
+    assertThat(HashTablesIceCreamParlor.whatFlavors(new int[] {1, 4, 5, 3, 2}, 4))
+        .asList()
+        .containsExactly(1, 4)
+        .inOrder();
+  }
+
+  @Test
+  void example2() {
+    assertThat(HashTablesIceCreamParlor.whatFlavors(new int[] {2, 2, 4, 3}, 4))
+        .asList()
+        .containsExactly(1, 2)
+        .inOrder();
+  }
+
+  @Test
+  void example3() {
+    assertThat(HashTablesIceCreamParlor.whatFlavors(new int[] {1, 2, 3, 5, 6}, 5))
+        .asList()
+        .containsExactly(2, 3)
+        .inOrder();
+  }
+
+  @Test
+  void example4() {
+    assertThat(HashTablesIceCreamParlor.whatFlavors(new int[] {1, 2, 3, 5, 6}, 5))
+        .asList()
+        .containsExactly(2, 3)
+        .inOrder();
+  }
+}
--- leetcode/src/main/java/wilmol/leetcode/problemset/algorithms/easy/P1TwoSum.java
+++ leetcode/src/main/java/wilmol/leetcode/problemset/algorithms/easy/P1TwoSum.java
@@ -9,6 +9,8 @@ import java.util.Map;
  * <p><a href=https://leetcode.com/problems/two-sum>https://leetcode.com/problems/two-sum</a>
  *
  * <p>Runtime: O(n) - single pass on ints, but O(n) memory too
+ *
+ * @see wilmol.hackerrank.interviewpreparationkit.search.medium.HashTablesIceCreamParlor
  */
 class P1TwoSum {
 
