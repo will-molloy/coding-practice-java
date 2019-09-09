5743c0801f9da48055afcb3d437263baeb896484
Will Molloy <willjoemolloy@gmail.com>
2019-09-09 21:10:15 +1200

    hackerrank IPK stacksqueues QueuesATaleOfTwoStacks (medium)
    - copied existing solution, solved it in 2017 and 2018
    
    GitOrigin-RevId: b302178df5649b137a010fee75e04c5f29915082

--- hackerrank/src/test/java/wilmol/hackerrank/interviewpreparationkit/stacksqueues/medium/QueuesATaleOfTwoStacksTest.java
+++ hackerrank/src/test/java/wilmol/hackerrank/interviewpreparationkit/stacksqueues/medium/QueuesATaleOfTwoStacksTest.java
@@ -1,10 +1,23 @@
 package wilmol.hackerrank.interviewpreparationkit.stacksqueues.medium;
 
+import static com.google.common.truth.Truth.assertThat;
+
 import org.junit.jupiter.api.Test;
+import wilmol.hackerrank.interviewpreparationkit.stacksqueues.medium.QueuesATaleOfTwoStacks.MyQueue;
 
 /** Created by wilmol on 2019-09-09. */
 class QueuesATaleOfTwoStacksTest {
 
+  // TODO more than 1 test
+
   @Test
-  void QueuesATaleOfTwoStacksTest() {}
+  void sample1() {
+    MyQueue<Integer> queue = new MyQueue<>();
+    queue.enqueue(35);
+    queue.enqueue(15);
+    assertThat(queue.peek()).isEqualTo(35);
+    assertThat(queue.dequeue()).isEqualTo(35);
+    assertThat(queue.peek()).isEqualTo(15);
+    assertThat(queue.dequeue()).isEqualTo(15);
+  }
 }
