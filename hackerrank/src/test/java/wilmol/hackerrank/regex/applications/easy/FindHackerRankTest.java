ab8a509d735ba5388a5359d2440df70ffc8f58d1
Will Molloy <willjoemolloy@gmail.com>
2019-09-15 17:34:50 +1200

    hackerrank regex FindHackerRank (easy)
    - use multiple regex to try match
    
    GitOrigin-RevId: 0d1d12580e4bc13a9aea4ec349c1bb03f829096f

---
+++ hackerrank/src/test/java/wilmol/hackerrank/regex/applications/easy/FindHackerRankTest.java
@@ -0,0 +1,40 @@
+package wilmol.hackerrank.regex.applications.easy;
+
+import static com.google.common.truth.Truth.assertThat;
+import static wilmol.hackerrank.regex.applications.easy.FindHackerRank.findHackerRank;
+
+import org.junit.jupiter.api.Test;
+import wilmol.hackerrank.regex.applications.easy.FindHackerRank.Type;
+
+/** Created by wilmol on 2019-09-15. */
+class FindHackerRankTest {
+
+  @Test
+  void ends() {
+    assertThat(findHackerRank(" hackerrank")).isEqualTo(Type.END);
+    assertThat(findHackerRank("i love hackerrank")).isEqualTo(Type.END);
+  }
+
+  @Test
+  void starts() {
+    assertThat(findHackerRank("hackerrank ")).isEqualTo(Type.START);
+    assertThat(findHackerRank("hackerrank is an awesome place for programmers"))
+        .isEqualTo(Type.START);
+  }
+
+  @Test
+  void both() {
+    assertThat(findHackerRank("hackerrank")).isEqualTo(Type.START_AND_END);
+    assertThat(findHackerRank("hackerrankhackerrank")).isEqualTo(Type.START_AND_END);
+    assertThat(findHackerRank("hackerrank hackerrank")).isEqualTo(Type.START_AND_END);
+    assertThat(findHackerRank("hackerrank abc123 hackerrank")).isEqualTo(Type.START_AND_END);
+  }
+
+  @Test
+  void none() {
+    assertThat(findHackerRank(" hackerrank ")).isEqualTo(Type.NONE);
+    assertThat(findHackerRank("i think hackerrank is a great place to hangout"))
+        .isEqualTo(Type.NONE);
+    assertThat(findHackerRank("nothing at all")).isEqualTo(Type.NONE);
+  }
+}
