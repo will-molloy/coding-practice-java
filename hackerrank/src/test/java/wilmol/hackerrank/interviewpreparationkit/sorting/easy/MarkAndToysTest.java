51c9d2a6b86b6e89526996fdc250b3b932e28724
Will Molloy <willjoemolloy@gmail.com>
2019-09-21 22:31:04 +1200

    hackerrank IPK MarkAndToys (easy)
    - very easy, O(n log n) solution, just sort first
    - test coverage quite poor though, have heard hackerrank has poor coverage too
    - easy to prove correctness with induction though
    
    GitOrigin-RevId: d712c73911467614bc1781cc1aae33e684ae79b3

---
+++ hackerrank/src/test/java/wilmol/hackerrank/interviewpreparationkit/sorting/easy/MarkAndToysTest.java
@@ -0,0 +1,29 @@
+package wilmol.hackerrank.interviewpreparationkit.sorting.easy;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+/** Created by wilmol on 2019-09-21. */
+class MarkAndToysTest {
+
+  @Test
+  void sampleTestCase0() {
+    assertThat(MarkAndToys.maximumToys(new int[] {1, 12, 5, 111, 200, 1000, 10}, 50)).isEqualTo(4);
+  }
+
+  @Test
+  void sampleTestCase1() {
+    assertThat(MarkAndToys.maximumToys(new int[] {1, 2, 3, 4}, 7)).isEqualTo(3);
+  }
+
+  @Test
+  void sampleTestCase2() {
+    assertThat(MarkAndToys.maximumToys(new int[] {3, 7, 2, 9, 4}, 15)).isEqualTo(3);
+  }
+
+  @Test
+  void singleToyWithExactPrice() {
+    assertThat(MarkAndToys.maximumToys(new int[] {50}, 50)).isEqualTo(1);
+  }
+}
