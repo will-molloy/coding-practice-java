7f9171af87995ed805427c25857a958fc5c8f561
Will Molloy <willjoemolloy@gmail.com>
2019-09-21 22:09:38 +1200

    hackerrank IPK FrequencyQueries (medium)
    - hash tables
    - easy to get 'correct' solution but difficult to pass the TLE
    - note had to modify boilerplate code to pass TLE on java (use array rather than list)
    
    GitOrigin-RevId: d1a579aae7100ee41a3991d906ac9fd2e29e5c80

---
+++ hackerrank/src/test/java/wilmol/hackerrank/interviewpreparationkit/dictionarieshashmaps/medium/FrequencyQueriesTest.java
@@ -0,0 +1,71 @@
+package wilmol.hackerrank.interviewpreparationkit.dictionarieshashmaps.medium;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+/** Created by wilmol on 2019-09-21. */
+class FrequencyQueriesTest {
+
+  @Test
+  void sampleInput0() {
+    List<int[]> input =
+        Arrays.asList(
+            new int[] {1, 5},
+            new int[] {1, 6},
+            new int[] {3, 2},
+            new int[] {1, 10},
+            new int[] {1, 10},
+            new int[] {1, 6},
+            new int[] {2, 5},
+            new int[] {3, 2});
+    assertThat(FrequencyQueries.freqQuery(input)).containsExactly(0, 1).inOrder();
+  }
+
+  @Test
+  void sampleInput1() {
+    List<int[]> input =
+        Arrays.asList(new int[] {3, 4}, new int[] {2, 1003}, new int[] {1, 16}, new int[] {3, 1});
+    assertThat(FrequencyQueries.freqQuery(input)).containsExactly(0, 1).inOrder();
+  }
+
+  @Test
+  void sampleInput2() {
+    List<int[]> input =
+        Arrays.asList(
+            new int[] {1, 3},
+            new int[] {2, 3},
+            new int[] {3, 2},
+            new int[] {1, 4},
+            new int[] {1, 5},
+            new int[] {1, 5},
+            new int[] {1, 4},
+            new int[] {3, 2},
+            new int[] {2, 4},
+            new int[] {3, 2});
+    assertThat(FrequencyQueries.freqQuery(input)).containsExactly(0, 1, 1).inOrder();
+  }
+
+  @Test
+  void freqDropsBelowZero() {
+    List<int[]> input =
+        Arrays.asList(new int[] {2, 1}, new int[] {2, 1}, new int[] {1, 1}, new int[] {3, 1});
+    assertThat(FrequencyQueries.freqQuery(input)).containsExactly(1).inOrder();
+  }
+
+  @Test
+  void freqDropsButAnotherNumberRetainsIt() {
+    List<int[]> input =
+        Arrays.asList(
+            new int[] {1, 1},
+            new int[] {1, 1},
+            new int[] {1, 2},
+            new int[] {1, 2},
+            new int[] {2, 1},
+            new int[] {3, 2},
+            new int[] {3, 1});
+    assertThat(FrequencyQueries.freqQuery(input)).containsExactly(1, 1).inOrder();
+  }
+}
