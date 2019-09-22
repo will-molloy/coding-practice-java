f12cbcd7d9d5ba237c51693d28d60736f0e4e654
Will Molloy <willjoemolloy@gmail.com>
2019-09-22 23:57:32 +1200

    hackerrank IPK CountTriplets (medium, hashmaps)
    - got O(n^3) solution
    - almost got O(n) solution, didnt see index restriction in problem statement
    - so had to get hints from discussion
    - trick was to track COUNTS not RESULTS using 2 hashmaps (one for forming pairs, one for forming triplets)
    
    GitOrigin-RevId: ae9c02fcc09bbec124c838c06c3cc3b884201c16

---
+++ hackerrank/src/test/java/wilmol/hackerrank/interviewpreparationkit/dictionarieshashmaps/medium/CountTripletsTest.java
@@ -0,0 +1,71 @@
+package wilmol.hackerrank.interviewpreparationkit.dictionarieshashmaps.medium;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Timeout;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+/** Created by wilmol on 2019-09-22. */
+@Timeout(value = 1)
+class CountTripletsTest {
+
+  @ParameterizedTest
+  @MethodSource("functions")
+  void example(CountTriplets fun) {
+    assertThat(fun.countTriplets(Arrays.asList(1L, 4L, 16L, 64L), 4)).isEqualTo(2);
+  }
+
+  @ParameterizedTest
+  @MethodSource("functions")
+  void sampleInput0(CountTriplets fun) {
+    assertThat(fun.countTriplets(Arrays.asList(1L, 2L, 2L, 4L), 2)).isEqualTo(2);
+  }
+
+  @ParameterizedTest
+  @MethodSource("functions")
+  void sampleInput1(CountTriplets fun) {
+    assertThat(fun.countTriplets(Arrays.asList(1L, 3L, 9L, 9L, 27L, 81L), 3)).isEqualTo(6);
+  }
+
+  @ParameterizedTest
+  @MethodSource("functions")
+  void sampleInput2(CountTriplets fun) {
+    assertThat(fun.countTriplets(Arrays.asList(1L, 5L, 5L, 25L, 125L), 5)).isEqualTo(4);
+  }
+
+  @ParameterizedTest
+  @MethodSource("functions")
+  void ratioOfOneOneTriplet(CountTriplets fun) {
+    assertThat(fun.countTriplets(Arrays.asList(1L, 1L, 1L), 1)).isEqualTo(1);
+  }
+
+  @ParameterizedTest
+  @MethodSource("functions")
+  void ratioOfOneManyOverlappingTriplets(CountTriplets fun) {
+    // 100 choose 3 = 161700
+    assertThat(fun.countTriplets(Collections.nCopies(100, 1L), 1)).isEqualTo(161700);
+  }
+
+  @ParameterizedTest
+  @MethodSource("functions")
+  void ratioOfOneTwoTriplets(CountTriplets fun) {
+    assertThat(fun.countTriplets(Arrays.asList(1L, 1L, 1L, 2L, 2L, 2L), 1)).isEqualTo(2);
+  }
+
+  @ParameterizedTest
+  @MethodSource("functions")
+  void unsortedArrayBreaksIndexOrder(CountTriplets fun) {
+    assertThat(fun.countTriplets(Arrays.asList(1L, 2L, 1L, 2L, 4L), 2)).isEqualTo(3);
+  }
+
+  static Stream<Arguments> functions() {
+    return Stream.of(
+        Arguments.of(new CountTriplets.NaiveSolution()),
+        Arguments.of(new CountTriplets.LinearSolution()));
+  }
+}
