663b7447cbe16e55a384111f5cde3c02a81af5f0
Will Molloy <willjoemolloy@gmail.com>
2019-11-30 20:29:12 +1300

    leetcode P43MultiplyStrings (medium)
    - silly problem so cheated with bigint
    
    GitOrigin-RevId: a75451d4535ac20b8598d99c9c479d368f6af1a7

---
+++ leetcode/src/main/java/wilmol/leetcode/problemset/algorithms/medium/P43MultiplyStrings.java
@@ -0,0 +1,20 @@
+package wilmol.leetcode.problemset.algorithms.medium;
+
+import java.math.BigInteger;
+
+/**
+ * Created by wilmol on 2019-11-30.
+ *
+ * <p><a
+ * href=https://leetcode.com/problems/multiply-strings>https://leetcode.com/problems/multiply-strings</a>
+ *
+ * <p>Runtime: O(nm)
+ *
+ * <p>Key: cheat and use bigint TODO supposed to implement multiplication algorithm!
+ */
+class P43MultiplyStrings {
+
+  public String multiply(String num1, String num2) {
+    return new BigInteger(num1).multiply(new BigInteger(num2)).toString();
+  }
+}
