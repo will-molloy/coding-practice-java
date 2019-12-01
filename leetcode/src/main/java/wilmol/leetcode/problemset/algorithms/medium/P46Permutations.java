c1da378d56b78c9116b73de1405892e5002d441e
Will Molloy <willjoemolloy@gmail.com>
2019-12-01 14:00:32 +1300

    leetcode P46Permutations (medium)
    - recursive backtracking solution
    - have seen while loop before (Scala sdk source code???)
    
    GitOrigin-RevId: fb06f111c2d3d8a7c554b337b7333e10a7aba305

---
+++ leetcode/src/main/java/wilmol/leetcode/problemset/algorithms/medium/P46Permutations.java
@@ -0,0 +1,52 @@
+package wilmol.leetcode.problemset.algorithms.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by wilmol on 2019-12-01.
+ *
+ * <p>Input doesn't have duplicates.
+ *
+ * <p><a
+ * href=https://leetcode.com/problems/permutations>https://leetcode.com/problems/permutations</a>
+ *
+ * <p>Runtime: O(n!), Space: O(n!)
+ *
+ * <p>Key: bruteforce, recursive backtracking
+ */
+class P46Permutations {
+
+  public List<List<Integer>> permute(int[] nums) {
+    List<List<Integer>> result = new ArrayList<>();
+    List<Integer> temp = new ArrayList<>();
+    permute(result, temp, nums);
+    return result;
+  }
+
+  // temp (call stack size)[state]:
+  // (1)[1] -> (2)[1,2], -> (3)[1,2,3] -> (3)[1,2] -> (2)[1] -> (2)[1,3] -> (3)[1,3,2] etc.
+  // So E.g. first add '2' after '1' (2nd state above), then next time will add '3' (6th state)
+
+  private void permute(List<List<Integer>> result, List<Integer> temp, int[] nums) {
+    if (temp.size() == nums.length) {
+      // add a copy of temp, want to keep current contents for backtracking
+      result.add(new ArrayList<>(temp));
+      // (optimisation) return here because the loop won't do anything
+      return;
+    }
+
+    for (int num : nums) {
+      // TODO can optimise O(n) contains here by using boolean array or Set, or specify start
+      // index
+      if (temp.contains(num)) {
+        continue;
+      }
+      temp.add(num);
+      // dfs
+      permute(result, temp, nums);
+      // remove most recently added (backtrack)
+      temp.remove(temp.size() - 1);
+    }
+  }
+}
---
+++ leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/medium/P46PermutationsTest.java
@@ -0,0 +1,24 @@
+package wilmol.leetcode.problemset.algorithms.medium;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.common.collect.ImmutableList;
+import org.junit.jupiter.api.Test;
+
+/** Created by wilmol on 2019-12-01. */
+class P46PermutationsTest {
+
+  private final P46Permutations fun = new P46Permutations();
+
+  @Test
+  void example() {
+    assertThat(fun.permute(new int[] {1, 2, 3}))
+        .containsExactly(
+            ImmutableList.of(1, 2, 3),
+            ImmutableList.of(1, 3, 2),
+            ImmutableList.of(2, 1, 3),
+            ImmutableList.of(2, 3, 1),
+            ImmutableList.of(3, 1, 2),
+            ImmutableList.of(3, 2, 1));
+  }
+}
