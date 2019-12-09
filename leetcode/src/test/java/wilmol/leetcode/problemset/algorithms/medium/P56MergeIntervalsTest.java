17c66b18367b009992c598358dee2331bc0620e0
Will Molloy <willjoemolloy@gmail.com>
2019-12-09 21:02:14 +1300

    leetcode P56MergeIntervals optimal solution
    - copied the 'sort first' solution from leetcode
    - interestingly runs slower than my solution when submitted, but better
    time complexity
    
    GitOrigin-RevId: e50878239ad57af4020fc2115c1b7bae9e5ce3ab

--- leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/medium/P56MergeIntervalsTest.java
+++ leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/medium/P56MergeIntervalsTest.java
@@ -2,36 +2,48 @@ package wilmol.leetcode.problemset.algorithms.medium;
 
 import static com.google.common.truth.Truth.assertThat;
 
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 /** Created by wilmol on 2019-12-02. */
 class P56MergeIntervalsTest {
 
-  private final P56MergeIntervals fun = new P56MergeIntervals();
-
-  @Test
-  void example1() {
+  @ParameterizedTest
+  @MethodSource("funs")
+  void example1(P56MergeIntervals fun) {
     assertThat(fun.merge(new int[][] {{1, 3}, {2, 6}, {8, 10}, {15, 18}}))
         .isEqualTo(new int[][] {{1, 6}, {8, 10}, {15, 18}});
   }
 
-  @Test
-  void example2() {
+  @ParameterizedTest
+  @MethodSource("funs")
+  void example2(P56MergeIntervals fun) {
     assertThat(fun.merge(new int[][] {{1, 4}, {4, 5}})).isEqualTo(new int[][] {{1, 5}});
   }
 
-  @Test
-  void withDoubleMerge() {
+  @ParameterizedTest
+  @MethodSource("funs")
+  void withDoubleMerge(P56MergeIntervals fun) {
     assertThat(fun.merge(new int[][] {{1, 2}, {3, 4}, {2, 3}})).isEqualTo(new int[][] {{1, 4}});
   }
 
-  @Test
-  void firstRangeIsEnclosing() {
+  @ParameterizedTest
+  @MethodSource("funs")
+  void firstRangeIsEnclosing(P56MergeIntervals fun) {
     assertThat(fun.merge(new int[][] {{1, 4}, {2, 3}})).isEqualTo(new int[][] {{1, 4}});
   }
 
-  @Test
-  void secondRangeIsEnclosing() {
+  @ParameterizedTest
+  @MethodSource("funs")
+  void secondRangeIsEnclosing(P56MergeIntervals fun) {
     assertThat(fun.merge(new int[][] {{2, 3}, {1, 4}})).isEqualTo(new int[][] {{1, 4}});
+  }
+
+  static Stream<Arguments> funs() {
+    return Stream.of(
+        Arguments.of(new P56MergeIntervals.MergeIntervalsInEncounterOrder()),
+        Arguments.of(new P56MergeIntervals.SortFirst()));
   }
 }
