69722fbd91e5c0390bab217bfdfeb718e36ec5eb
Will Molloy <willjoemolloy@gmail.com>
2019-12-10 21:49:34 +1300

    leetcode P58LengthOfLastWord (easy)
    GitOrigin-RevId: 5be9b595c9fc790a063027e11e6518c6db52b29d

---
+++ leetcode/src/main/java/wilmol/leetcode/problemset/algorithms/easy/P58LengthOfLastWord.java
@@ -0,0 +1,22 @@
+package wilmol.leetcode.problemset.algorithms.easy;
+
+/**
+ * Created by wilmol on 2019-12-10.
+ *
+ * <p><a
+ * href=https://leetcode.com/problems/length-of-last-word>https://leetcode.com/problems/length-of-last-word</a>
+ *
+ * <p>Runtime: O(n)
+ *
+ * <p>Key: knowledge of {@link String} class.
+ */
+class P58LengthOfLastWord {
+
+  public int lengthOfLastWord(String s) {
+    String[] split = s.split(" ");
+    if (split.length > 0) {
+      return split[split.length - 1].length();
+    }
+    return 0;
+  }
+}
