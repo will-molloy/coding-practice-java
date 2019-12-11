8a50f8823dbd0d3afcb8b133bd1baa80ab0e5da2
Will Molloy <willjoemolloy@gmail.com>
2019-12-11 23:17:01 +1300

    leetcode P62UniquePaths (medium)
    - ok getting good at the dp stuff (pat on back)
    - TODO on the recursive runtime/space use...
    
    GitOrigin-RevId: 816ac24969504220a078ab93c4b5cc79c92d1418

---
+++ leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/medium/P62UniquePathsTest.java
@@ -0,0 +1,28 @@
+package wilmol.leetcode.problemset.algorithms.medium;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import org.junit.jupiter.api.Test;
+
+/** Created by wilmol on 2019-12-11. */
+class P62UniquePathsTest {
+
+  private final P62UniquePaths recursive = new P62UniquePaths.TopDownRecursive();
+
+  private final P62UniquePaths dp = new P62UniquePaths.BottomUpDp();
+
+  @Test
+  void example1() {
+    assertAll(
+        () -> assertThat(recursive.uniquePaths(3, 2)).isEqualTo(3),
+        () -> assertThat(dp.uniquePaths(3, 2)).isEqualTo(3));
+  }
+
+  @Test
+  void example2() {
+    assertAll(
+        () -> assertThat(recursive.uniquePaths(7, 3)).isEqualTo(28),
+        () -> assertThat(dp.uniquePaths(7, 3)).isEqualTo(28));
+  }
+}
