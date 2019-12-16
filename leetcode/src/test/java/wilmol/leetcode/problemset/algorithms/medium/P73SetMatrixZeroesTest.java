0aa999031eafd4d72afde0ed5cd057c81e003bcb
Will Molloy <willjoemolloy@gmail.com>
2019-12-16 22:35:50 +1300

    leetcode P73SetMatrixZeroes (medium)
    GitOrigin-RevId: a3a6a98c8debc83c272b859cdab268ab88830558

---
+++ leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/medium/P73SetMatrixZeroesTest.java
@@ -0,0 +1,45 @@
+package wilmol.leetcode.problemset.algorithms.medium;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+/** Created by wilmol on 2019-12-16. */
+class P73SetMatrixZeroesTest {
+
+  private final P73SetMatrixZeroes fun = new P73SetMatrixZeroes();
+
+  @Test
+  void example1() {
+    int[][] matrix = {
+      {1, 1, 1},
+      {1, 0, 1},
+      {1, 1, 1},
+    };
+    fun.setZeroes(matrix);
+    assertThat(matrix)
+        .isEqualTo(
+            new int[][] {
+              {1, 0, 1},
+              {0, 0, 0},
+              {1, 0, 1},
+            });
+  }
+
+  @Test
+  void example2() {
+    int[][] matrix = {
+      {0, 1, 2, 0},
+      {3, 4, 5, 2},
+      {1, 3, 1, 5},
+    };
+    fun.setZeroes(matrix);
+    assertThat(matrix)
+        .isEqualTo(
+            new int[][] {
+              {0, 0, 0, 0},
+              {0, 4, 5, 0},
+              {0, 3, 1, 0},
+            });
+  }
+}
