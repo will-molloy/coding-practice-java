7e3d685a1402545a501af163a04ec83a89c44270
Will Molloy <willjoemolloy@gmail.com>
2019-12-16 23:37:39 +1300

    leetcode P74SearchA2dMatrix (medium)
    GitOrigin-RevId: a38d7272f59202a3b08ce5a661d5710f6f77134c

---
+++ leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/medium/P74SearchA2dMatrixTest.java
@@ -0,0 +1,42 @@
+package wilmol.leetcode.problemset.algorithms.medium;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+/** Created by wilmol on 2019-12-16. */
+class P74SearchA2dMatrixTest {
+
+  private final P74SearchA2dMatrix fun = new P74SearchA2dMatrix();
+
+  @Test
+  void example1() {
+    assertThat(
+            fun.searchMatrix(
+                new int[][] {
+                  {1, 3, 5, 7},
+                  {10, 11, 16, 20},
+                  {23, 30, 34, 50},
+                },
+                3))
+        .isTrue();
+  }
+
+  @Test
+  void example2() {
+    assertThat(
+            fun.searchMatrix(
+                new int[][] {
+                  {1, 3, 5, 7},
+                  {10, 11, 16, 20},
+                  {23, 30, 34, 50},
+                },
+                13))
+        .isFalse();
+  }
+
+  @Test
+  void emptyMatrix() {
+    assertThat(fun.searchMatrix(new int[][] {}, 123)).isFalse();
+  }
+}
