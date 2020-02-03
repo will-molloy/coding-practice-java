e2e0c656957f388b3dd4df3f93eab5db48af9291
Will Molloy <willjoemolloy@gmail.com>
2020-02-03 22:04:37 +1300

    leetcode P88MergeSortedArray (easy)
    looked at discussions page for the O(1) space solution
    
    GitOrigin-RevId: 989836d7e92c94b0ce7f9afbe2348fc13441f7c2

---
+++ leetcode/src/main/java/wilmol/leetcode/problemset/algorithms/easy/P88MergeSortedArray.java
@@ -0,0 +1,39 @@
+package wilmol.leetcode.problemset.algorithms.easy;
+
+/**
+ * Created by wilmol on 2020-02-03.
+ *
+ * <p><a
+ * href=https://leetcode.com/problems/merge-sorted-array>https://leetcode.com/problems/merge-sorted-array</a>
+ *
+ * <p>Runtime: O(m + n)
+ *
+ * <p>Space: O(1)
+ *
+ * <p>Key: merging in reverse order to make use of 'empty array space' and achieve O(1) memory use
+ */
+class P88MergeSortedArray {
+
+  public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+    // index over nums1 (starting at last element)
+    int i1 = m - 1;
+    // index over nums2 (starting at last element)
+    int i2 = n - 1;
+    // index over merged nums1 and nums2 (starting at last element)
+    int i = m + n - 1;
+
+    // merge from back, i.e. choose largest element at each step
+    while (i1 >= 0 && i2 >= 0) {
+      if (nums1[i1] >= nums2[i2]) {
+        nums1[i--] = nums1[i1--];
+      } else {
+        nums1[i--] = nums2[i2--];
+      }
+    }
+    // only need to merge remaining nums2, remaining nums1 would already be in the list
+    while (i2 >= 0) {
+      nums1[i--] = nums2[i2--];
+    }
+  }
+}
