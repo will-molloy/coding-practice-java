23a7af6184940202bfc5ae910b2b7d21eb282025
Will Molloy <willjoemolloy@gmail.com>
2020-02-06 21:20:38 +1300

    leetcode P86PartitionList (medium)
    GitOrigin-RevId: a361c3a05940ccea110401170c02c99023896f10

---
+++ leetcode/src/main/java/wilmol/leetcode/problemset/algorithms/medium/P86PartitionList.java
@@ -0,0 +1,44 @@
+package wilmol.leetcode.problemset.algorithms.medium;
+
+import wilmol.leetcode.common.ListNode;
+
+/**
+ * Created by wilmol on 2020-02-06.
+ *
+ * <p><a
+ * href=https://leetcode.com/problems/partition-list>https://leetcode.com/problems/partition-list</a>
+ *
+ * <p>Runtime: O(n)
+ *
+ * <p>Space: O(1)
+ *
+ * <p>Key: partition into two lists and join at the end, also dummy nodes to avoid null problems
+ */
+class P86PartitionList {
+
+  public ListNode partition(final ListNode head, int x) {
+    // partition into lessThan list and greaterThanEqual list
+    // use dummy node to avoid NPE in case the list is empty
+    ListNode ltList = new ListNode(0);
+    final ListNode ltHead = ltList;
+    ListNode gteList = new ListNode(0);
+    final ListNode gteHead = gteList;
+
+    ListNode node = head;
+    while (node != null) {
+      if (node.val >= x) {
+        gteList.next = node;
+        gteList = node;
+      } else {
+        ltList.next = node;
+        ltList = node;
+      }
+      node = node.next;
+    }
+
+    // join the lists
+    gteList.next = null; // done first to avoid cycle when debugging
+    ltList.next = gteHead.next;
+    return ltHead.next;
+  }
+}
---
+++ leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/medium/P86PartitionListTest.java
@@ -0,0 +1,63 @@
+package wilmol.leetcode.problemset.algorithms.medium;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.Test;
+import wilmol.leetcode.common.ListNode;
+
+/** Created by wilmol on 2020-02-06. */
+class P86PartitionListTest {
+
+  private final P86PartitionList func = new P86PartitionList();
+
+  @Test
+  void example() {
+    assertThat(func.partition(ListNode.of(1, 4, 3, 2, 5, 2), 3))
+        .isEqualTo(ListNode.of(1, 2, 2, 4, 3, 5));
+  }
+
+  @Test
+  void exampleFirstRunOnly() {
+    assertThat(func.partition(ListNode.of(1, 4, 3, 2), 3)).isEqualTo(ListNode.of(1, 2, 4, 3));
+  }
+
+  @Test
+  void singleSwap() {
+    assertThat(func.partition(ListNode.of(2, 1), 2)).isEqualTo(ListNode.of(1, 2));
+  }
+
+  @Test
+  void nullInput() {
+    assertThat(func.partition(null, 0)).isEqualTo(null);
+  }
+
+  @Test
+  void singleNode() {
+    assertThat(func.partition(ListNode.of(1), 0)).isEqualTo(ListNode.of(1));
+  }
+
+  @Test
+  void allGreaterThanX() {
+    assertThat(func.partition(ListNode.of(1, 2, 3), 0)).isEqualTo(ListNode.of(1, 2, 3));
+  }
+
+  @Test
+  void allEqualToX() {
+    assertThat(func.partition(ListNode.of(3, 3, 3), 3)).isEqualTo(ListNode.of(3, 3, 3));
+  }
+
+  @Test
+  void allLessThanX() {
+    assertThat(func.partition(ListNode.of(1, 2, 3), 4)).isEqualTo(ListNode.of(1, 2, 3));
+  }
+
+  @Test
+  void firstGreaterThanEqualToX() {
+    assertThat(func.partition(ListNode.of(3, 1, 2), 3)).isEqualTo(ListNode.of(1, 2, 3));
+  }
+
+  @Test
+  void secondGreaterThanEqualToX() {
+    assertThat(func.partition(ListNode.of(1, 3, 1, 2), 3)).isEqualTo(ListNode.of(1, 1, 2, 3));
+  }
+}
