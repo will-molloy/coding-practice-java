8f2c86c12330e674afa6293bcd6fb6e02dd99a9c
Will Molloy <willjoemolloy@gmail.com>
2020-02-07 21:48:12 +1300

    leetcode P89GrayCode (medium)
    GitOrigin-RevId: 591567e2969a58bdb509d0e99c8087080b9894e2

---
+++ leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/medium/P89GrayCodeTest.java
@@ -0,0 +1,42 @@
+package wilmol.leetcode.problemset.algorithms.medium;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.common.collect.ImmutableList;
+import org.junit.jupiter.api.Test;
+
+/** Created by wilmol on 2020-02-07. */
+class P89GrayCodeTest {
+
+  private final P89GrayCode func = new P89GrayCode();
+
+  @Test
+  void n0() {
+    assertThat(func.grayCode(0)).containsExactly(0);
+  }
+
+  @Test
+  void n1() {
+    assertThat(func.grayCode(1)).containsExactly(0, 1).inOrder();
+  }
+
+  @Test
+  void n2() {
+    assertThat(func.grayCode(2))
+        .isAnyOf(ImmutableList.of(0, 2, 3, 1), ImmutableList.of(0, 1, 3, 2));
+  }
+
+  @Test
+  void n3() {
+    // technically other answers are valid... but didn't bother computing
+    assertThat(func.grayCode(3)).containsExactly(0, 1, 3, 2, 6, 7, 5, 4).inOrder();
+  }
+
+  @Test
+  void n4() {
+    // technically other answers are valid... but didn't bother computing
+    assertThat(func.grayCode(4))
+        .containsExactly(0, 1, 3, 2, 6, 7, 5, 4, 12, 13, 15, 14, 10, 11, 9, 8)
+        .inOrder();
+  }
+}
