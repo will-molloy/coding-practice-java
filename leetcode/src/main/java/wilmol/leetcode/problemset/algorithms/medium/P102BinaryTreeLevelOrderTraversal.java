f3f39a1bb7f6b91635d4e83b219609565e3af1f7
Will Molloy <willjoemolloy@gmail.com>
2020-02-11 22:05:46 +1300

    alternative solution; leetcode P102BinaryTreeLevelOrderTraversal (medium)
    - BFS/Queue
    
    GitOrigin-RevId: 070a4b4b5f4f21874a24bec16995f83df31cce83

--- leetcode/src/main/java/wilmol/leetcode/problemset/algorithms/medium/P102BinaryTreeLevelOrderTraversal.java
+++ leetcode/src/main/java/wilmol/leetcode/problemset/algorithms/medium/P102BinaryTreeLevelOrderTraversal.java
@@ -1,6 +1,8 @@
 package wilmol.leetcode.problemset.algorithms.medium;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 import wilmol.leetcode.common.BinaryTreeNode;
 
@@ -9,32 +11,94 @@ import wilmol.leetcode.common.BinaryTreeNode;
  *
  * <p><a
  * href=https://leetcode.com/problems/binary-tree-level-order-traversal>https://leetcode.com/problems/binary-tree-level-order-traversal</a>
- *
- * <p>Runtime: O(n)
- *
- * <p>Space: O(log n) best case, O(n) worst case (recursion stack size)
- *
- * <p>Key: recursion, knowledge of BT traversal
  */
-class P102BinaryTreeLevelOrderTraversal {
+interface P102BinaryTreeLevelOrderTraversal {
 
-  public List<List<Integer>> levelOrder(BinaryTreeNode root) {
-    return levelOrder(new ArrayList<>(), root, 0);
-  }
+  List<List<Integer>> levelOrder(BinaryTreeNode root);
 
-  public List<List<Integer>> levelOrder(
-      List<List<Integer>> levels, BinaryTreeNode node, int depth) {
-    if (node == null) {
+  /**
+   * Dfs; tracks depth to know level to insert node into.
+   *
+   * <p>Runtime: O(n)
+   *
+   * <p>Space: O(h) (i.e. height of tree, corresponds to max recursion stack size)
+   *
+   * <p>Key: recursion, knowledge of BT traversal
+   */
+  class Dfs implements P102BinaryTreeLevelOrderTraversal {
+
+    @Override
+    public List<List<Integer>> levelOrder(BinaryTreeNode root) {
+      return levelOrder(new ArrayList<>(), root, 0);
+    }
+
+    public List<List<Integer>> levelOrder(
+        List<List<Integer>> levels, BinaryTreeNode node, int depth) {
+      if (node == null) {
+        return levels;
+      }
+
+      if (!(levels.size() > depth)) {
+        levels.add(new ArrayList<>());
+      }
+      levels.get(depth).add(node.val);
+
+      levelOrder(levels, node.left, depth + 1);
+      levelOrder(levels, node.right, depth + 1);
       return levels;
     }
 
-    if (!(levels.size() > depth)) {
-      levels.add(new ArrayList<>());
+    @Override
+    public String toString() {
+      return getClass().getSimpleName();
     }
-    levels.get(depth).add(node.val);
+  }
 
-    levelOrder(levels, node.left, depth + 1);
-    levelOrder(levels, node.right, depth + 1);
-    return levels;
+  /**
+   * Bfs; uses a queue to traverse nodes breadth first (naturally in level order).
+   *
+   * <p>Runtime: O(n)
+   *
+   * <p>Space: O(w) (i.e. max width of tree, corresponds to max queue size)
+   *
+   * <p>Key: caching the queue size before processing nodes in the queue
+   */
+  class Bfs implements P102BinaryTreeLevelOrderTraversal {
+
+    @Override
+    public List<List<Integer>> levelOrder(BinaryTreeNode root) {
+      List<List<Integer>> levels = new ArrayList<>();
+      if (root == null) {
+        return levels;
+      }
+
+      Deque<BinaryTreeNode> queue = new ArrayDeque<>();
+      queue.add(root);
+
+      while (!queue.isEmpty()) {
+        List<Integer> level = new ArrayList<>();
+        // cache size since we'll be adding the next level's nodes
+        int levelSize = queue.size();
+        for (int i = 0; i < levelSize; i++) {
+          // remove from front of queue
+          BinaryTreeNode node = queue.removeFirst();
+          level.add(node.val);
+          // add to back of queue
+          if (node.left != null) {
+            queue.addLast(node.left);
+          }
+          if (node.right != null) {
+            queue.addLast(node.right);
+          }
+        }
+        levels.add(level);
+      }
+      return levels;
+    }
+
+    @Override
+    public String toString() {
+      return getClass().getSimpleName();
+    }
   }
 }
--- leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/medium/P102BinaryTreeLevelOrderTraversalTest.java
+++ leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/medium/P102BinaryTreeLevelOrderTraversalTest.java
@@ -3,19 +3,27 @@ package wilmol.leetcode.problemset.algorithms.medium;
 import static com.google.common.truth.Truth.assertThat;
 
 import com.google.common.collect.ImmutableList;
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import wilmol.leetcode.common.BinaryTreeNode;
 
 /** Created by wilmol on 2020-02-11. */
 class P102BinaryTreeLevelOrderTraversalTest {
 
-  private final P102BinaryTreeLevelOrderTraversal fun = new P102BinaryTreeLevelOrderTraversal();
-
-  @Test
-  void example() {
+  @ParameterizedTest
+  @MethodSource("funs")
+  void example(P102BinaryTreeLevelOrderTraversal fun) {
     BinaryTreeNode root = BinaryTreeNode.fromLevelOrder(3, 9, 20, null, null, 15, 7);
     assertThat(fun.levelOrder(root))
         .containsExactly(ImmutableList.of(3), ImmutableList.of(9, 20), ImmutableList.of(15, 7))
         .inOrder();
+  }
+
+  static Stream<Arguments> funs() {
+    return Stream.of(
+        Arguments.of(new P102BinaryTreeLevelOrderTraversal.Dfs()),
+        Arguments.of(new P102BinaryTreeLevelOrderTraversal.Bfs()));
   }
 }
