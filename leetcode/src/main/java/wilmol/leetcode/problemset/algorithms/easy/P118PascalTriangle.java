ae10408b177a1bf6f3becca1b5a017d66ab2992b
Will Molloy <willjoemolloy@gmail.com>
2020-02-20 21:18:24 +1300

    leetcode P118PascalTriangle (easy)
    GitOrigin-RevId: 122c72f075dd42ae6e58691cbc7fce9aaa8108e3

---
+++ leetcode/src/main/java/wilmol/leetcode/problemset/algorithms/easy/P118PascalTriangle.java
@@ -0,0 +1,41 @@
+package wilmol.leetcode.problemset.algorithms.easy;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by wilmol on 2020-02-20.
+ *
+ * <p><a
+ * href=https://leetcode.com/problems/pascals-triangle>https://leetcode.com/problems/pascals-triangle</a>
+ *
+ * <p>Runtime: O(n<sup>2</sup>)
+ *
+ * <p>Extra space: O(1)
+ *
+ * <p>Key: just coded the steps in the visualisation
+ */
+class P118PascalTriangle {
+
+  public List<List<Integer>> generate(int n) {
+    if (n == 0) {
+      return Collections.emptyList();
+    }
+
+    List<List<Integer>> rows = new ArrayList<>();
+    rows.add(Collections.singletonList(1));
+
+    for (int i = 1; i < n; i++) {
+      List<Integer> aboveRow = rows.get(i - 1);
+      List<Integer> row = new ArrayList<>();
+      row.add(1);
+      for (int j = 1; j < i; j++) {
+        row.add(aboveRow.get(j - 1) + aboveRow.get(j));
+      }
+      row.add(1);
+      rows.add(row);
+    }
+    return rows;
+  }
+}
