c0fa054fa267809d364c4390b7f76f2bea40bebd
Will Molloy <willjoemolloy@gmail.com>
2020-02-25 22:38:30 +1300

    ListNode EqualsTester
    (leetcode module)
    
    GitOrigin-RevId: ae37cd1aaa0f16d2b8fdd5f255ad645f72b95ec6

--- leetcode/src/test/java/wilmol/leetcode/common/ListNodeTest.java
+++ leetcode/src/test/java/wilmol/leetcode/common/ListNodeTest.java
@@ -2,6 +2,7 @@ package wilmol.leetcode.common;
 
 import static com.google.common.truth.Truth.assertThat;
 
+import com.google.common.testing.EqualsTester;
 import org.junit.jupiter.api.Test;
 
 /** Created by wilmol on 2020-02-08. */
@@ -20,5 +21,26 @@ class ListNodeTest {
     node.next = node2;
     node2.next = node;
     assertThat(node.toString()).isEqualTo("ListNode[1, 2] (cycling)");
+  }
+
+  @Test
+  void testEquals() {
+    ListNode threeElements = new ListNode(1);
+    threeElements.next = new ListNode(2);
+    threeElements.next.next = new ListNode(3);
+
+    ListNode threeElementsCycling = new ListNode(1);
+    threeElementsCycling.next = new ListNode(2);
+    threeElementsCycling.next.next = new ListNode(3);
+    threeElementsCycling.next.next.next = threeElementsCycling;
+
+    ListNode threeElementsCyclingViaStaticFactory = ListNode.of(1, 2, 3);
+    threeElementsCyclingViaStaticFactory.next.next.next = threeElementsCyclingViaStaticFactory;
+
+    new EqualsTester()
+        .addEqualityGroup(new ListNode(1), ListNode.of(1))
+        .addEqualityGroup(threeElements, ListNode.of(1, 2, 3))
+        .addEqualityGroup(threeElementsCycling, threeElementsCyclingViaStaticFactory)
+        .testEquals();
   }
 }
