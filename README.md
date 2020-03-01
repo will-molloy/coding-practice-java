620617691526f378ccc202fd3b4090bf7a10d2fe
Will Molloy <willjoemolloy@gmail.com>
2020-03-01 23:07:27 +1300

    leetcode P96UniqueBinarySearchTrees (medium)
    GitOrigin-RevId: f4d3bb0d3ab8b69f127e7856422fb9d99214ab91

--- README.md
+++ README.md
@@ -8,8 +8,8 @@ projecteuler, leetcode, hackerrank etc. using Java
 - [ ] Project Euler
   - [ ] [Problems Archives](project-euler/src/main/java/wilmol/projecteuler/problems) (65/687)
 - [ ] LeetCode
-  - [ ] [Algorithms](leetcode/src/main/java/wilmol/leetcode/problemset/algorithms) (176/1248)
-  - [ ] [Database](leetcode/src/main/java/wilmol/leetcode/problemset/database) (5/98)
+  - [ ] [Algorithms](leetcode/src/main/java/wilmol/leetcode/problemset/algorithms) (177/1256)
+  - [ ] [Database](leetcode/src/main/java/wilmol/leetcode/problemset/database) (5/99)
   - [ ] [Shell](leetcode/src/main/java/wilmol/leetcode/problemset/shell) (0/4)
   - [ ] [Concurrency](leetcode/src/main/java/wilmol/leetcode/problemset/concurrency) (5/9)
 - [ ] HackerRank
---
+++ leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/medium/P96UniqueBinarySearchTreesTest.java
@@ -0,0 +1,47 @@
+package wilmol.leetcode.problemset.algorithms.medium;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+/** Created by wilmol on 2020-03-01. */
+class P96UniqueBinarySearchTreesTest {
+
+  @ParameterizedTest
+  @MethodSource("funs")
+  void n1(P96UniqueBinarySearchTrees fun) {
+    assertThat(fun.numTrees(1)).isEqualTo(1);
+  }
+
+  @ParameterizedTest
+  @MethodSource("funs")
+  void n2(P96UniqueBinarySearchTrees fun) {
+    assertThat(fun.numTrees(2)).isEqualTo(2);
+  }
+
+  @ParameterizedTest
+  @MethodSource("funs")
+  void n3(P96UniqueBinarySearchTrees fun) {
+    assertThat(fun.numTrees(3)).isEqualTo(5);
+  }
+
+  @ParameterizedTest
+  @MethodSource("funs")
+  void n4(P96UniqueBinarySearchTrees fun) {
+    assertThat(fun.numTrees(4)).isEqualTo(14);
+  }
+
+  @ParameterizedTest
+  @MethodSource("funs")
+  void n5(P96UniqueBinarySearchTrees fun) {
+    assertThat(fun.numTrees(5)).isEqualTo(42);
+  }
+
+  static Stream<P96UniqueBinarySearchTrees> funs() {
+    return Stream.of(
+        new P96UniqueBinarySearchTrees.TopDownRecursive(),
+        new P96UniqueBinarySearchTrees.BottomUpIterative());
+  }
+}
