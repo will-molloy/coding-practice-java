77158aaf5a88c7494effc949af72d3117edbb34a
Will Molloy <willjoemolloy@gmail.com>
2020-03-03 20:40:20 +1300

    improve leetcode P34FindFirstAndLastPositionOfElementInSortedArray (medium)
    - use binary search to find start/end of the range rather than linear
    scan
    - now O(log n) worst case rather than O(n)
    
    GitOrigin-RevId: 7ecb66b8585edbc9eb69cae3e170df108a7168f8

--- leetcode/src/main/java/wilmol/leetcode/problemset/algorithms/medium/P34FindFirstAndLastPositionOfElementInSortedArray.java
+++ leetcode/src/main/java/wilmol/leetcode/problemset/algorithms/medium/P34FindFirstAndLastPositionOfElementInSortedArray.java
@@ -6,39 +6,43 @@ package wilmol.leetcode.problemset.algorithms.medium;
  * <p><a
  * href=https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array>https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array</a>
  *
- * <p>Runtime: O(log n) (O(n) worst case)
+ * <p>Runtime: O(log n)
+ *
+ * <p>Space: O(1)
+ *
+ * <p>Key: sorted sequence, so O(log n) binary search can be used
  */
 class P34FindFirstAndLastPositionOfElementInSortedArray {
 
   public int[] searchRange(int[] nums, int target) {
-    // normal binary search to find target
-    // then scan left/right for first/last occurrence of target
+    int head = firstPosition(nums, target);
+    int last = firstPosition(nums, target + 1) - 1;
+    if (head <= last) {
+      // valid range
+      return new int[] {head, last};
+    } else {
+      // target not found
+      return new int[] {-1, -1};
+    }
+  }
+
+  // modified binary search which finds the first instance of the given target
+  private int firstPosition(int[] nums, int target) {
+    // default, when target > nums.last, first position is beyond the array
+    int firstPos = nums.length;
 
     int low = 0;
     int high = nums.length - 1;
+
     while (low <= high) {
       int mid = (low + high) >>> 1;
-
-      if (nums[mid] < target) {
-        // target on right
-        low = mid + 1;
-      } else if (nums[mid] > target) {
-        // target on left
+      if (nums[mid] >= target) {
+        firstPos = mid;
         high = mid - 1;
-      } else {
-        // target found
-        int first = mid;
-        int last = mid;
-        while (first >= 0 && nums[first] == target) {
-          first -= 1;
-        }
-        while (last < nums.length && nums[last] == target) {
-          last += 1;
-        }
-        return new int[] {first + 1, last - 1};
+      } else { // nums[mid] < target
+        low = mid + 1;
       }
     }
-    // target not found
-    return new int[] {-1, -1};
+    return firstPos;
   }
 }
