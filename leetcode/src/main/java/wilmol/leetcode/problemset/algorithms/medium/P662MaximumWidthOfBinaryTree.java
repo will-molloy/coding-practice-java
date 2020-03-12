95d76b6557afd9b3135505905cf01927aa7c65c0
Will Molloy <willjoemolloy@gmail.com>
2020-03-12 21:38:23 +1300

    leetcode P662MaximumWidthOfBinaryTree (medium)
    GitOrigin-RevId: 96ae0d46a970e5d5d91be3b1683480bf2e0128c2

---
+++ leetcode/src/main/java/wilmol/leetcode/problemset/algorithms/medium/P662MaximumWidthOfBinaryTree.java
@@ -0,0 +1,62 @@
+package wilmol.leetcode.problemset.algorithms.medium;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+import wilmol.leetcode.common.BinaryTreeNode;
+
+/**
+ * Created by wilmol on 2020-03-12.
+ *
+ * <p><a
+ * href=https://leetcode.com/problems/maximum-width-of-binary-tree>https://leetcode.com/problems/maximum-width-of-binary-tree</a>
+ *
+ * <p>Runtime: O(n)
+ *
+ * <p>Space: O(w) = O(n)
+ *
+ * <p>Key: labelling nodes, in this case [0, n)
+ */
+class P662MaximumWidthOfBinaryTree {
+
+  public int widthOfBinaryTree(BinaryTreeNode root) {
+    if (root == null) {
+      return 0;
+    }
+
+    // get biggest level, bfs
+    Queue<NodeWrapper> queue = new ArrayDeque<>();
+    queue.add(new NodeWrapper(root, 0));
+    int maxWidth = 1;
+
+    while (!queue.isEmpty()) {
+      // process next level
+      int levelSize = queue.size();
+      int firstInLevel = -1;
+      for (int i = 0; i < levelSize; i++) {
+        NodeWrapper node = queue.remove();
+        if (node.node != null) {
+          queue.add(new NodeWrapper(node.node.left, node.index * 2));
+          queue.add(new NodeWrapper(node.node.right, node.index * 2 + 1));
+          if (firstInLevel == -1) {
+            firstInLevel = node.index;
+          }
+          maxWidth = Math.max(maxWidth, node.index - firstInLevel + 1);
+        }
+      }
+    }
+    return maxWidth;
+  }
+
+  /**
+   * Wraps a node with its 'index' (i.e. index if the binary tree were serialised into a 1d array).
+   */
+  private static final class NodeWrapper {
+    private final BinaryTreeNode node;
+    private final int index;
+
+    private NodeWrapper(BinaryTreeNode node, int index) {
+      this.node = node;
+      this.index = index;
+    }
+  }
+}
