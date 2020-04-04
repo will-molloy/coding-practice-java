abbd25f2a5d8254627095dcc92c94fea91260e75
Will Molloy <willjoemolloy@gmail.com>
2020-04-04 16:21:42 +1300

    leetcode P1347MinimumNumberOfStepsToMakeTwoStringsAnagram (medium)
    GitOrigin-RevId: c3055f26c66acda152b7d33bb638b678f8e94f2c

---
+++ leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/easy/P1347MinimumNumberOfStepsToMakeTwoStringsAnagramTest.java
@@ -0,0 +1,37 @@
+package wilmol.leetcode.problemset.algorithms.easy;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+/** Created by wilmol on 2020-04-04. */
+class P1347MinimumNumberOfStepsToMakeTwoStringsAnagramTest {
+
+  private final P1347MinimumNumberOfStepsToMakeTwoStringsAnagram fun =
+      new P1347MinimumNumberOfStepsToMakeTwoStringsAnagram();
+
+  @Test
+  void example1() {
+    assertThat(fun.minSteps("bab", "aba")).isEqualTo(1);
+  }
+
+  @Test
+  void example2() {
+    assertThat(fun.minSteps("leetcode", "practice")).isEqualTo(5);
+  }
+
+  @Test
+  void example3() {
+    assertThat(fun.minSteps("anagram", "mangaar")).isEqualTo(0);
+  }
+
+  @Test
+  void example4() {
+    assertThat(fun.minSteps("xxyyzz", "xxyyzz")).isEqualTo(0);
+  }
+
+  @Test
+  void example5() {
+    assertThat(fun.minSteps("friend", "family")).isEqualTo(4);
+  }
+}
