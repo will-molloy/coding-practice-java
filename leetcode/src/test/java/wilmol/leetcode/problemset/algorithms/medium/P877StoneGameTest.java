50db118b00bb9f97d094ffa0df34e2e772d23a37
Will Molloy <willjoemolloy@gmail.com>
2020-04-04 16:56:47 +1300

    leetcode P877StoneGame (medium)
    Flawed problem.
    
    GitOrigin-RevId: 12daa635d5f9fdfba96663e776459d6c145c8c14

---
+++ leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/medium/P877StoneGameTest.java
@@ -0,0 +1,16 @@
+package wilmol.leetcode.problemset.algorithms.medium;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+/** Created by wilmol on 2020-04-04. */
+class P877StoneGameTest {
+
+  private final P877StoneGame fun = new P877StoneGame();
+
+  @Test
+  void example1() {
+    assertThat(fun.stoneGame(new int[] {5, 3, 4, 5})).isTrue();
+  }
+}
