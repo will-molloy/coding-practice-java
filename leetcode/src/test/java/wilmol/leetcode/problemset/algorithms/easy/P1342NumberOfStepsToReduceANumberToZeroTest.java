ea85ebd35b720b0a5fdf29a8072fa9c6d6a1b5be
Will Molloy <willjoemolloy@gmail.com>
2020-04-04 16:34:57 +1300

    leetcode P1342NumberOfStepsToReduceANumberToZero (easy)
    GitOrigin-RevId: 6cb11fdad7f50d657c741ad19a86e6ac7693276a

---
+++ leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/easy/P1342NumberOfStepsToReduceANumberToZeroTest.java
@@ -0,0 +1,32 @@
+package wilmol.leetcode.problemset.algorithms.easy;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+/** Created by wilmol on 2020-04-04. */
+class P1342NumberOfStepsToReduceANumberToZeroTest {
+
+  private final P1342NumberOfStepsToReduceANumberToZero fun =
+      new P1342NumberOfStepsToReduceANumberToZero();
+
+  @Test
+  void example1() {
+    assertThat(fun.numberOfSteps(14)).isEqualTo(6);
+  }
+
+  @Test
+  void example2() {
+    assertThat(fun.numberOfSteps(8)).isEqualTo(4);
+  }
+
+  @Test
+  void example3() {
+    assertThat(fun.numberOfSteps(123)).isEqualTo(12);
+  }
+
+  @Test
+  void alreadyZero() {
+    assertThat(fun.numberOfSteps(0)).isEqualTo(0);
+  }
+}
