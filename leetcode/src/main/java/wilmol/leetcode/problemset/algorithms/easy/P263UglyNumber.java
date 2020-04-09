2652e49b65de8403b692332794a6b082337f762a
Will Molloy <willjoemolloy@gmail.com>
2020-04-09 22:59:17 +1200

    leetcode P263UglyNumber (easy)
    GitOrigin-RevId: eeea0196495eb78ab0f8ca4f9bce821a5311f2d0

---
+++ leetcode/src/main/java/wilmol/leetcode/problemset/algorithms/easy/P263UglyNumber.java
@@ -0,0 +1,31 @@
+package wilmol.leetcode.problemset.algorithms.easy;
+
+/**
+ * Created by wilmol on 2020-04-09.
+ *
+ * <p><a
+ * href=https://leetcode.com/problems/ugly-number>https://leetcode.com/problems/ugly-number</a>
+ *
+ * <p>Runtime: O(n)
+ *
+ * <p>Space: O(1)
+ *
+ * <p>Key: prime factorisation: while divisible by factor {@code i}, repeatedly divide by it before
+ * moving onto next ({@code i = [2..n]})
+ */
+class P263UglyNumber {
+
+  public boolean isUgly(int num) {
+    if (num == 0) {
+      return false;
+    }
+    int[] factors = {2, 3, 5};
+    for (int factor : factors) {
+      while (num % factor == 0) {
+        num /= factor;
+      }
+    }
+    // if num was not reduced to 1 by the prime factors [2, 3, 5] then it has other prime factors
+    return num == 1;
+  }
+}
---
+++ leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/easy/P263UglyNumberTest.java
@@ -0,0 +1,36 @@
+package wilmol.leetcode.problemset.algorithms.easy;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+/** Created by wilmol on 2020-04-09. */
+class P263UglyNumberTest {
+
+  private final P263UglyNumber fun = new P263UglyNumber();
+
+  @Test
+  void example1() {
+    assertThat(fun.isUgly(6)).isTrue();
+  }
+
+  @Test
+  void example2() {
+    assertThat(fun.isUgly(8)).isTrue();
+  }
+
+  @Test
+  void example3() {
+    assertThat(fun.isUgly(14)).isFalse();
+  }
+
+  @Test
+  void zero() {
+    assertThat(fun.isUgly(0)).isFalse();
+  }
+
+  @Test
+  void one() {
+    assertThat(fun.isUgly(1)).isTrue();
+  }
+}
