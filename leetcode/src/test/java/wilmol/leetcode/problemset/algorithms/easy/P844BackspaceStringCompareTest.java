9464d21501e545f8b13b801f7935634babfab57d
Will Molloy <willjoemolloy@gmail.com>
2020-04-10 16:05:42 +1200

    leetcode P844BackspaceStringCompare (easy)
    GitOrigin-RevId: 679218f2c07cd49b5ef8d894ee6a2e6c8ef44a7f

---
+++ leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/easy/P844BackspaceStringCompareTest.java
@@ -0,0 +1,51 @@
+package wilmol.leetcode.problemset.algorithms.easy;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+/** Created by wilmol on 2020-04-10. */
+class P844BackspaceStringCompareTest {
+
+  private final P844BackspaceStringCompare fun = new P844BackspaceStringCompare();
+
+  @Test
+  void example1() {
+    assertThat(fun.backspaceCompare("ab#c", "ad#c")).isTrue();
+  }
+
+  @Test
+  void example2() {
+    assertThat(fun.backspaceCompare("ab##", "c#d#")).isTrue();
+  }
+
+  @Test
+  void example3() {
+    assertThat(fun.backspaceCompare("a##c", "#a#c")).isTrue();
+  }
+
+  @Test
+  void example4() {
+    assertThat(fun.backspaceCompare("a#c", "b")).isFalse();
+  }
+
+  @Test
+  void manyBackspaceAtEnd() {
+    assertThat(fun.backspaceCompare("a###", "b###")).isTrue();
+  }
+
+  @Test
+  void manyBackspaceAtStart() {
+    assertThat(fun.backspaceCompare("###a", "###b")).isFalse();
+  }
+
+  @Test
+  void oneIsErasedOtherIsnt() {
+    assertThat(fun.backspaceCompare("a#", "b")).isFalse();
+  }
+
+  @Test
+  void dontBackspaceBackspaceCharacter() {
+    assertThat(fun.backspaceCompare("bxj##tw", "bxo#j##tw")).isTrue();
+  }
+}
