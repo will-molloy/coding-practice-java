8654b675ec9cb41f2304ecbf7b3625fad59f12cc
Will Molloy <willjoemolloy@gmail.com>
2020-04-14 00:01:47 +1200

    leetcode P525ContiguousArray (medium)
    GitOrigin-RevId: 0014b137f2fe60a5800090d2c0174584546f5fb9

---
+++ leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/medium/P525ContiguousArrayTest.java
@@ -0,0 +1,51 @@
+package wilmol.leetcode.problemset.algorithms.medium;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+/** Created by wilmol on 2020-04-13. */
+class P525ContiguousArrayTest {
+
+  private final P525ContiguousArray fun = new P525ContiguousArray();
+
+  @Test
+  void example1() {
+    assertThat(fun.findMaxLength(new int[] {0, 1})).isEqualTo(2);
+  }
+
+  @Test
+  void example2() {
+    assertThat(fun.findMaxLength(new int[] {0, 1, 0})).isEqualTo(2);
+  }
+
+  @Test
+  void n10Length10() {
+    assertThat(fun.findMaxLength(new int[] {0, 1, 0, 1, 0, 1, 0, 1, 0, 1})).isEqualTo(10);
+  }
+
+  @Test
+  void n10Length10Alternative() {
+    assertThat(fun.findMaxLength(new int[] {0, 0, 0, 0, 0, 1, 1, 1, 1, 1})).isEqualTo(10);
+  }
+
+  @Test
+  void n10Length8() {
+    assertThat(fun.findMaxLength(new int[] {0, 1, 0, 1, 0, 1, 0, 1, 0, 0})).isEqualTo(8);
+  }
+
+  @Test
+  void n0() {
+    assertThat(fun.findMaxLength(new int[0])).isEqualTo(0);
+  }
+
+  @Test
+  void n10Length0() {
+    assertThat(fun.findMaxLength(new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0})).isEqualTo(0);
+  }
+
+  @Test
+  void maxLengthExcludesHeadAndLast() {
+    assertThat(fun.findMaxLength(new int[] {0, 0, 1, 0, 1, 0, 0, 0})).isEqualTo(4);
+  }
+}
