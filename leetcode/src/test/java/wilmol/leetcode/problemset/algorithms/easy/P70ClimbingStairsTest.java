a21a0eabf8d25cb2eaf13779c6700d6faa1a35f1
Will Molloy <willjoemolloy@gmail.com>
2020-04-13 17:05:14 +1200

    [leetcode] Add bottom up linear space solution to P70ClimbingStairs
    GitOrigin-RevId: e39703ec797181764c063aee284b35a1abd138c9

--- leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/easy/P70ClimbingStairsTest.java
+++ leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/easy/P70ClimbingStairsTest.java
@@ -12,25 +12,38 @@ class P70ClimbingStairsTest {
 
   @ParameterizedTest
   @MethodSource("funs")
-  void example1(P70ClimbingStairs fun) {
+  void n0(P70ClimbingStairs fun) {
+    assertThat(fun.climbStairs(0)).isEqualTo(1);
+  }
+
+  @ParameterizedTest
+  @MethodSource("funs")
+  void n1(P70ClimbingStairs fun) {
+    assertThat(fun.climbStairs(1)).isEqualTo(1);
+  }
+
+  @ParameterizedTest
+  @MethodSource("funs")
+  void n2(P70ClimbingStairs fun) {
     assertThat(fun.climbStairs(2)).isEqualTo(2);
   }
 
   @ParameterizedTest
   @MethodSource("funs")
-  void example2(P70ClimbingStairs fun) {
+  void n3(P70ClimbingStairs fun) {
     assertThat(fun.climbStairs(3)).isEqualTo(3);
   }
 
   @ParameterizedTest
   @MethodSource("funs")
-  void nOf10(P70ClimbingStairs fun) {
+  void n10(P70ClimbingStairs fun) {
     assertThat(fun.climbStairs(10)).isEqualTo(89);
   }
 
   static Stream<Arguments> funs() {
     return Stream.of(
         Arguments.of(new P70ClimbingStairs.RecursiveTopDown()),
-        Arguments.of(new P70ClimbingStairs.IterativeBottomUp()));
+        Arguments.of(new P70ClimbingStairs.IterativeBottomUp()),
+        Arguments.of(new P70ClimbingStairs.IterativeBottomUpConstantSpace()));
   }
 }
