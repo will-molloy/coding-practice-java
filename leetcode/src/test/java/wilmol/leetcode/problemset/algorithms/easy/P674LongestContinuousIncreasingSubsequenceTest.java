e8ca15f2dd4cf54f6b878b1cea80cee5abda6be5
Will Molloy <willjoemolloy@gmail.com>
2020-04-14 16:09:06 +1200

    leetcode P674LongestContinuousIncreasingSubsequence (easy)
    GitOrigin-RevId: a6d7c7b8cfcc3d07fc934e849668187576baf399

---
+++ leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/easy/P674LongestContinuousIncreasingSubsequenceTest.java
@@ -0,0 +1,27 @@
+package wilmol.leetcode.problemset.algorithms.easy;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+/** Created by wilmol on 2020-04-14. */
+class P674LongestContinuousIncreasingSubsequenceTest {
+
+  private final P674LongestContinuousIncreasingSubsequence fun =
+      new P674LongestContinuousIncreasingSubsequence();
+
+  @Test
+  void example1() {
+    assertThat(fun.findLengthOfLcis(new int[] {1, 3, 5, 4, 7})).isEqualTo(3);
+  }
+
+  @Test
+  void example2() {
+    assertThat(fun.findLengthOfLcis(new int[] {2, 2, 2, 2, 2})).isEqualTo(1);
+  }
+
+  @Test
+  void emptyArray() {
+    assertThat(fun.findLengthOfLcis(new int[0])).isEqualTo(0);
+  }
+}
