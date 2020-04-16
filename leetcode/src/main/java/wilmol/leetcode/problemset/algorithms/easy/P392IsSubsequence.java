35dac7907015a89049768141320280f8a08bddff
Will Molloy <willjoemolloy@gmail.com>
2020-04-17 00:54:43 +1200

    leetcode P392IsSubsequence (easy)
    GitOrigin-RevId: 05ae85d8feb693950627bc40ccf2d83d71cfd636

---
+++ leetcode/src/main/java/wilmol/leetcode/problemset/algorithms/easy/P392IsSubsequence.java
@@ -0,0 +1,28 @@
+package wilmol.leetcode.problemset.algorithms.easy;
+
+/**
+ * Created by wilmol on 2020-04-17.
+ *
+ * <p><a
+ * href=https://leetcode.com/problems/is-subsequence>https://leetcode.com/problems/is-subsequence</a>
+ *
+ * <p>Runtime: O(t)
+ *
+ * <p>Space: O(1)
+ *
+ * <p>Key: separate pointer for s and t.
+ */
+class P392IsSubsequence {
+
+  public boolean isSubsequence(String s, String t) {
+    int sI = 0;
+    int tI = 0;
+    while (sI < s.length() && tI < t.length()) {
+      if (s.charAt(sI) == t.charAt(tI)) {
+        sI++;
+      }
+      tI++;
+    }
+    return sI == s.length();
+  }
+}
