92cabdc09a01e9bdc6e0bb816554d622a34042c5
Will Molloy <willjoemolloy@gmail.com>
2020-04-18 19:43:46 +1200

    leetcode P581ShortestUnsortedContinuousSubarray (easy)
    Not so easy.
    
    GitOrigin-RevId: 532c1c23e2d4e100f7500ce9519caaf80fa34c81

--- leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/medium/P134GasStationTest.java
+++ leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/medium/P134GasStationTest.java
@@ -10,8 +10,13 @@ class P134GasStationTest {
   private final P134GasStation p134 = new P134GasStation();
 
   @Test
-  void example() {
+  void example1() {
     assertThat(p134.canCompleteCircuit(new int[] {1, 2, 3, 4, 5}, new int[] {3, 4, 5, 1, 2}))
         .isEqualTo(3);
+  }
+
+  @Test
+  void example2() {
+    assertThat(p134.canCompleteCircuit(new int[] {2, 3, 4}, new int[] {3, 4, 3})).isEqualTo(-1);
   }
 }
