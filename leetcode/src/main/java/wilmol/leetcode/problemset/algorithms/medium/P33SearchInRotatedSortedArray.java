5c0edb792407d5bcb564560688c6403f4abefe4f
Will Molloy <willjoemolloy@gmail.com>
2020-04-20 19:45:18 +1200

    leetcode improve P33SearchInRotatedSortedArray (medium)
    With learnings from Errichto's video.
    
    This improves the runtime from O(2*log(n)) to O(log(n)) and IMO is more
    elegant and intuitive.
    
    GitOrigin-RevId: 2754cda4826b949cb2c356491c3b251d6a2d9213

--- leetcode/src/main/java/wilmol/leetcode/problemset/algorithms/medium/P33SearchInRotatedSortedArray.java
+++ leetcode/src/main/java/wilmol/leetcode/problemset/algorithms/medium/P33SearchInRotatedSortedArray.java
@@ -4,59 +4,66 @@ package wilmol.leetcode.problemset.algorithms.medium;
  * Created by wilmol on 2019-08-13.
  *
  * <p><a
- * href=https://leetcode.com/problems/search-in-rotated-sorted-array>https://leetcode.com/problems/search-in-rotated-sorted-array</a>
+ * href=https://leetcode.com/problems/search-in-rotated-sorted-array>https://leetcode.com/problems/search-in-rotated-sorted-array/</a>
  *
  * <p>Runtime: O(log n)
  *
+ * <p>Space: O(1)
+ *
+ * <p>Key: The rotation partitions the sorted sequence into 2 sorted partitions. E.g. <3,4,5,0,1,2>
+ * = <3,4,5> ++ <0,1,2>. (If no rotation then second partition is empty.)
+ *
  * @see P153FindMinimumInRotatedSortedArray
  * @see P81SearchInRotatedSortedArray2
+ * @see <a href=https://youtu.be/84a8fQp5hJA>Errichto's video</a>
  */
 class P33SearchInRotatedSortedArray {
 
   public int search(int[] nums, int target) {
-    int rotation = getLeftRotation(nums);
-    return rotatedSearch(nums, target, rotation);
-  }
-
-  private int getLeftRotation(int[] nums) {
-    int left = 0;
-    int right = nums.length - 1;
-
-    // find index of smallest value (using binary search)
-    // this is the number of places rotated (to the left) since un-rotated it would have index 0
-    while (left < right) {
-      int mid = (left + right) >>> 1;
-
-      if (nums[mid] > nums[right]) {
-        // rotated case, smaller values are on right side
-        left = mid + 1;
-      } else {
-        // normal case, smaller values are on left side
-        right = mid;
-      }
+    int n = nums.length;
+    if (n == 0) {
+      return -1;
     }
-    // index of smallest value is in both left and right
-    return left;
-  }
 
-  private int rotatedSearch(int[] nums, int target, int leftRotation) {
-    int left = 0;
-    int right = nums.length - 1;
+    int l = 0;
+    int r = n - 1;
+    int head = nums[0];
 
-    // usual binary search, accounting for rotation
-    while (left <= right) {
-      int mid = (left + right) >>> 1;
-      int realMid = (mid + leftRotation) % nums.length;
+    // searching range [l, r] hence need to include l == r
+    while (l <= r) {
+      int mid = l + (r - l) / 2;
+      int value = nums[mid];
 
-      if (nums[realMid] > target) {
-        // target is on left side
-        right = mid - 1;
-      } else if (nums[realMid] < target) {
-        // target is on right side
-        left = mid + 1;
-      } else {
+      if (value == target) {
         // target found
-        return realMid;
+        return mid;
+      }
+
+      // true if current value is in the left sorted partition
+      boolean currentValueLeft = value >= head;
+      // true if target is in the left sorted partition
+      boolean targetLeft = target >= head;
+
+      if (currentValueLeft == targetLeft) {
+        // current value and target are in the same sorted partition
+        // can perform standard binary search
+        if (value > target) {
+          // search left
+          r = mid - 1;
+        } else {
+          // search right
+          l = mid + 1;
+        }
+      } else {
+        // current value and target are in different sorted partitions
+        // need to search the partition that contains target
+        if (targetLeft) {
+          // search left
+          r = mid - 1;
+        } else {
+          // search right
+          l = mid + 1;
+        }
       }
     }
     return -1;
