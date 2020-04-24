4923756c66f389e6737d6cb374355fd471bbe7e6
Will Molloy <willjoemolloy@gmail.com>
2020-04-24 19:11:11 +1200

    leetcode P221MaximalSquare (medium)
    GitOrigin-RevId: 5931a75900bdae8c214e53f82a2a8a48bceebb37

---
+++ leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/medium/P221MaximalSquareTest.java
@@ -0,0 +1,24 @@
+package wilmol.leetcode.problemset.algorithms.medium;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+/** Created by wilmol on 2020-04-24. */
+class P221MaximalSquareTest {
+
+  private final P221MaximalSquare p221 = new P221MaximalSquare();
+
+  @Test
+  void example() {
+    assertThat(
+            p221.maximalSquare(
+                new char[][] {
+                  {'1', '0', '1', '0', '0'},
+                  {'1', '0', '1', '1', '1'},
+                  {'1', '1', '1', '1', '1'},
+                  {'1', '0', '0', '1', '0'}
+                }))
+        .isEqualTo(4);
+  }
+}
