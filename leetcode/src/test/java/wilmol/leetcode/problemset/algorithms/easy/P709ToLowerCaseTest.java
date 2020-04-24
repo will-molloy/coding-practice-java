3a8b07933e05e6809d734e26dd59a353e146f8e3
Will Molloy <willjoemolloy@gmail.com>
2020-04-24 22:56:23 +1200

    leetcode P709ToLowerCase (easy)
    GitOrigin-RevId: fed8557dae03278787ac775b72526328cccfa40f

---
+++ leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/easy/P709ToLowerCaseTest.java
@@ -0,0 +1,26 @@
+package wilmol.leetcode.problemset.algorithms.easy;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+/** Created by wilmol on 2020-04-24. */
+class P709ToLowerCaseTest {
+
+  private final P709ToLowerCase p709 = new P709ToLowerCase();
+
+  @Test
+  void example1() {
+    assertThat(p709.toLowerCase("Hello")).isEqualTo("hello");
+  }
+
+  @Test
+  void example2() {
+    assertThat(p709.toLowerCase("here")).isEqualTo("here");
+  }
+
+  @Test
+  void example3() {
+    assertThat(p709.toLowerCase("LOVELY")).isEqualTo("lovely");
+  }
+}
