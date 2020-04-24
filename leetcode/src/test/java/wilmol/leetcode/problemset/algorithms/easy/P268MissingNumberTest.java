107c142dc277bf102b5ae1350053b4604fc0b3e2
Will Molloy <willjoemolloy@gmail.com>
2020-04-24 23:50:24 +1200

    leetcode P268MissingNumber (easy)
    GitOrigin-RevId: a6e28b4cb6cbe32da1d4ada87b9c9158abeffa09

---
+++ leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/easy/P268MissingNumberTest.java
@@ -0,0 +1,26 @@
+package wilmol.leetcode.problemset.algorithms.easy;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+/** Created by wilmol on 2020-04-24. */
+class P268MissingNumberTest {
+
+  private final P268MissingNumber p268 = new P268MissingNumber();
+
+  @Test
+  void example1() {
+    assertThat(p268.missingNumber(new int[] {3, 0, 1})).isEqualTo(2);
+  }
+
+  @Test
+  void example2() {
+    assertThat(p268.missingNumber(new int[] {9, 6, 4, 2, 3, 5, 7, 0, 1})).isEqualTo(8);
+  }
+
+  @Test
+  void equalsZero() {
+    assertThat(p268.missingNumber(new int[] {1, 2, 3})).isEqualTo(0);
+  }
+}
