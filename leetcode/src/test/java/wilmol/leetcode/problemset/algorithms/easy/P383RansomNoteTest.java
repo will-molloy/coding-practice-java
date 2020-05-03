4f45aa88af12b0a1a13155af745d2f5ac550ff22
Will Molloy <willjoemolloy@gmail.com>
2020-05-03 19:11:23 +1200

    leetcode P383RansomNote (easy)
    From May 2020 challenge.
    
    GitOrigin-RevId: a377499d2e24d0189222833ad91fa72f3090a3c4

---
+++ leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/easy/P383RansomNoteTest.java
@@ -0,0 +1,26 @@
+package wilmol.leetcode.problemset.algorithms.easy;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+/** Created by wilmol on 2020-05-03. */
+class P383RansomNoteTest {
+
+  private final P383RansomNote p383 = new P383RansomNote();
+
+  @Test
+  void example1() {
+    assertThat(p383.canConstruct("a", "b")).isFalse();
+  }
+
+  @Test
+  void example2() {
+    assertThat(p383.canConstruct("aa", "ab")).isFalse();
+  }
+
+  @Test
+  void example3() {
+    assertThat(p383.canConstruct("aa", "aab")).isTrue();
+  }
+}
