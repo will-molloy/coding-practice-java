15df9d75e4a0981115a7ed42352a908ad6ba83bc
Will Molloy <willjoemolloy@gmail.com>
2020-05-04 00:39:49 +1200

    leetcode P784LetterCasePermutation (easy)
    GitOrigin-RevId: 989ab88eb17612ba625966f25813ea358e543028

---
+++ leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/easy/P784LetterCasePermutationTest.java
@@ -0,0 +1,32 @@
+package wilmol.leetcode.problemset.algorithms.easy;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+/** Created by wilmol on 2020-05-04. */
+class P784LetterCasePermutationTest {
+
+  private final P784LetterCasePermutation p784 = new P784LetterCasePermutation();
+
+  @Test
+  void example1() {
+    assertThat(p784.letterCasePermutation("a1b2")).containsExactly("a1b2", "a1B2", "A1b2", "A1B2");
+  }
+
+  @Test
+  void example2() {
+    assertThat(p784.letterCasePermutation("3z4")).containsExactly("3z4", "3Z4");
+  }
+
+  @Test
+  void example3() {
+    assertThat(p784.letterCasePermutation("12345")).containsExactly("12345");
+  }
+
+  @Test
+  void abc() {
+    assertThat(p784.letterCasePermutation("abc"))
+        .containsExactly("abc", "abC", "aBc", "aBC", "Abc", "AbC", "ABc", "ABC");
+  }
+}
