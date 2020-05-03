486c02487c3ce10e92766cfd84ca43c7354a5a32
Will Molloy <willjoemolloy@gmail.com>
2020-05-03 19:20:33 +1200

    leetcode P1436DestinationCity (easy)
    From Weekly Contest 187.
    
    GitOrigin-RevId: 8da95da5e4175b9c82719e488bf30c76d5f817b3

---
+++ leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/easy/P1436DestinationCityTest.java
@@ -0,0 +1,39 @@
+package wilmol.leetcode.problemset.algorithms.easy;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.common.collect.ImmutableList;
+import org.junit.jupiter.api.Test;
+
+/** Created by wilmol on 2020-05-03. */
+class P1436DestinationCityTest {
+
+  private final P1436DestinationCity p1436 = new P1436DestinationCity();
+
+  @Test
+  void example1() {
+    assertThat(
+            p1436.destCity(
+                ImmutableList.of(
+                    ImmutableList.of("London", "New York"),
+                    ImmutableList.of("New York", "Lima"),
+                    ImmutableList.of("Lima", "Sao Paulo"))))
+        .isEqualTo("Sao Paulo");
+  }
+
+  @Test
+  void example2() {
+    assertThat(
+            p1436.destCity(
+                ImmutableList.of(
+                    ImmutableList.of("B", "C"),
+                    ImmutableList.of("D", "B"),
+                    ImmutableList.of("C", "A"))))
+        .isEqualTo("A");
+  }
+
+  @Test
+  void example3() {
+    assertThat(p1436.destCity(ImmutableList.of(ImmutableList.of("A", "Z")))).isEqualTo("Z");
+  }
+}
