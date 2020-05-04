fe73e59bb1823ed152bdb293b0221893175bd1c5
Will Molloy <willjoemolloy@gmail.com>
2020-05-04 23:17:51 +1200

    leetcode P453MinimumMovesToEqualArrayElements (easy)
    GitOrigin-RevId: 77644fdf0cfda021debe7515d3178bcccd3ffd00

---
+++ leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/easy/P453MinimumMovesToEqualArrayElementsTest.java
@@ -0,0 +1,22 @@
+package wilmol.leetcode.problemset.algorithms.easy;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+/** Created by wilmol on 2020-05-04. */
+class P453MinimumMovesToEqualArrayElementsTest {
+
+  private final P453MinimumMovesToEqualArrayElements p453 =
+      new P453MinimumMovesToEqualArrayElements();
+
+  @Test
+  void example() {
+    assertThat(p453.minMoves(new int[] {1, 2, 3})).isEqualTo(3);
+  }
+
+  @Test
+  void largeDifference() {
+    assertThat(p453.minMoves(new int[] {1, Integer.MAX_VALUE})).isEqualTo(Integer.MAX_VALUE - 1);
+  }
+}
