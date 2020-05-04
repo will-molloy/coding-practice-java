06d2d66ae08aba067439eca653a6317b739fb2f7
Will Molloy <willjoemolloy@gmail.com>
2020-05-04 22:57:52 +1200

    leetcode P456132Pattern (medium)
    Better solutions are possible.
    
    GitOrigin-RevId: 6be3e7cb531cf4fabdfae05a8f212a8e4c2408b3

---
+++ leetcode/src/main/java/wilmol/leetcode/problemset/algorithms/medium/P456132Pattern.java
@@ -0,0 +1,35 @@
+package wilmol.leetcode.problemset.algorithms.medium;
+
+/**
+ * Created by wilmol on 2020-05-04.
+ *
+ * <p><a
+ * href=https://leetcode.com/problems/132-pattern>https://leetcode.com/problems/132-pattern/</a>
+ *
+ * <p>Runtime: O(n<sup>2</sup>) TODO can do O(n log(n)) or even O(n)
+ *
+ * <p>Extra space: O(1)
+ *
+ * <p>Key: Tracking lower bound ({@code minSoFar}).
+ */
+class P456132Pattern {
+
+  public boolean find132pattern(int[] a) {
+    int n = a.length;
+    if (n < 3) {
+      return false;
+    }
+
+    // track a[i] as minSoFar so cut down from cubic to quadratic time
+    int minSoFar = Integer.MAX_VALUE;
+    for (int j = 0; j < n - 1; j++) {
+      minSoFar = Math.min(minSoFar, a[j]);
+      for (int k = j + 1; k < n; k++) {
+        if (minSoFar < a[k] && a[k] < a[j]) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+}
