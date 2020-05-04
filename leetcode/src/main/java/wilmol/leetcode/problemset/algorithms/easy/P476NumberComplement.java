0261d63bc5b9c8cff76bd18149392ece01d807dc
Will Molloy <willjoemolloy@gmail.com>
2020-05-04 19:54:59 +1200

    leetcode P476NumberComplement (easy)
    From May 2020 Challenge.
    
    GitOrigin-RevId: 2a11b1dfa15b0c925beb0dda8e57c1d8831a13da

---
+++ leetcode/src/main/java/wilmol/leetcode/problemset/algorithms/easy/P476NumberComplement.java
@@ -0,0 +1,31 @@
+package wilmol.leetcode.problemset.algorithms.easy;
+
+/**
+ * Created by wilmol on 2020-05-04.
+ *
+ * <p><a href=https://leetcode.com/problems/number-complement>https://leetcode.com/problems/number-complement/</a>
+ *
+ * <p>Runtime: O(bitLength(num))
+ *
+ * <p>Extra space: O(1)
+ *
+ * <p>Key: Converting decimal to binary; you get the bits (from the remainders) in reverse order.
+ */
+class P476NumberComplement {
+
+  public int findComplement(int num) {
+    int res = 0;
+    int exp = 0;
+    while (num > 0){
+      int bit = num % 2;
+      // flip
+      bit = bit == 1 ? 0 : 1;
+
+      res += bit << exp;
+
+      num /= 2;
+      exp++;
+    }
+    return res;
+  }
+}
