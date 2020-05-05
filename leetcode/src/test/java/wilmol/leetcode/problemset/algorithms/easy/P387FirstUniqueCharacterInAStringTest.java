c67b970ca1792384e0f4c3e9835c3025d442c7c2
Will Molloy <willjoemolloy@gmail.com>
2020-05-05 19:20:11 +1200

    leetcode P387FirstUniqueCharacterInAString (easy)
    From May 2020 Challenge.
    
    GitOrigin-RevId: 2cfa2beb1bbefd967e21e55b6366d6caa097656f

---
+++ leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/easy/P387FirstUniqueCharacterInAStringTest.java
@@ -0,0 +1,21 @@
+package wilmol.leetcode.problemset.algorithms.easy;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+/** Created by wilmol on 2020-05-05. */
+class P387FirstUniqueCharacterInAStringTest {
+
+  private final P387FirstUniqueCharacterInAString p387 = new P387FirstUniqueCharacterInAString();
+
+  @Test
+  void example1() {
+    assertThat(p387.firstUniqChar("leetcode")).isEqualTo(0);
+  }
+
+  @Test
+  void example2() {
+    assertThat(p387.firstUniqChar("loveleetcode")).isEqualTo(2);
+  }
+}
