0e89873f7e65f76a9603b6967638cebc218bd926
Will Molloy <willjoemolloy@gmail.com>
2020-05-10 16:29:34 +1200

    leetcode P1442CountTripletsThatCanFormTwoArraysOfEqualXor (medium)
    From Weekly Contest 188.
    
    GitOrigin-RevId: 04fdb0da96ab5106f755ec7d27d9e3e562a05d49

---
+++ leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/medium/P1442CountTripletsThatCanFormTwoArraysOfEqualXorTest.java
@@ -0,0 +1,37 @@
+package wilmol.leetcode.problemset.algorithms.medium;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+/** Created by wilmol on 2020-05-10. */
+class P1442CountTripletsThatCanFormTwoArraysOfEqualXorTest {
+
+  private final P1442CountTripletsThatCanFormTwoArraysOfEqualXor p1442 =
+      new P1442CountTripletsThatCanFormTwoArraysOfEqualXor();
+
+  @Test
+  void example1() {
+    assertThat(p1442.countTriplets(new int[] {2, 3, 1, 6, 7})).isEqualTo(4);
+  }
+
+  @Test
+  void example2() {
+    assertThat(p1442.countTriplets(new int[] {1, 1, 1, 1, 1})).isEqualTo(10);
+  }
+
+  @Test
+  void example3() {
+    assertThat(p1442.countTriplets(new int[] {2, 3})).isEqualTo(0);
+  }
+
+  @Test
+  void example4() {
+    assertThat(p1442.countTriplets(new int[] {1, 3, 5, 7, 9})).isEqualTo(3);
+  }
+
+  @Test
+  void example5() {
+    assertThat(p1442.countTriplets(new int[] {7, 11, 12, 9, 5, 2, 7, 17, 22})).isEqualTo(8);
+  }
+}
