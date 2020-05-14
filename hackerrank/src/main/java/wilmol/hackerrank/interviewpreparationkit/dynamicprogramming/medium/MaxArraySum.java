0beef47442cbbc38fac63873e77706bf90669eb9
Will Molloy <willjoemolloy@gmail.com>
2020-05-15 11:12:30 +1200

    hackerrank redo ipk dp MaxArraySum
    GitOrigin-RevId: b9890d4a9bf4fffcde6fb18a4efc8d5e5a893798

--- hackerrank/src/main/java/wilmol/hackerrank/interviewpreparationkit/dynamicprogramming/medium/MaxArraySum.java
+++ hackerrank/src/main/java/wilmol/hackerrank/interviewpreparationkit/dynamicprogramming/medium/MaxArraySum.java
@@ -2,41 +2,42 @@ package wilmol.hackerrank.interviewpreparationkit.dynamicprogramming.medium;
 
 import static java.lang.Math.max;
 
-import java.util.List;
-
 /**
- * Created by Will on 26/03/2019
+ * Created by wilmol on 2020-05-15.
  *
  * <p><a
  * href=https://www.hackerrank.com/challenges/max-array-sum/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dynamic-programming>https://www.hackerrank.com/challenges/max-array-sum/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dynamic-programming</a>
  *
- * <p>Find the max subset sum where elements are not adjacent to each other in the original array.
+ * <p>Runtime: O(n)
  *
- * <p>DP with 1D array.
+ * <p>Extra space: O(n)
  *
- * <p>Runtime: O(n) time O(1) space
+ * <p>Key: {@code dp[i]} represents answer for {@code arr[0, i]}.
  */
 final class MaxArraySum {
 
-  private MaxArraySum() {}
+  static int maxSubsetSum(int... arr) {
+    int n = arr.length;
 
-  static int maxSubsetSum(List<Integer> list) {
-    return maxSubsetSum(list.stream().mapToInt(Integer::intValue).toArray());
-  }
-
-  static int maxSubsetSum(int[] arr) {
-    // keep track of the maximum at every point (i.e. the sub problems)
-    for (int i = 1; i < arr.length; i++) {
-      // max at a[0] is a[0] (1st base case)
-      if (i == 1) {
-        // 2nd base case (i - 2 is still IOOB)
-        arr[i] = max(arr[0], arr[1]);
-      } else {
-        // recursive case
-        // max(prev max, this element, this element + prev prev max)
-        arr[i] = max(max(arr[i - 1], arr[i]), arr[i] + arr[i - 2]);
-      }
+    if (n == 1) {
+      return max(0, arr[0]);
+    } else if (n == 2) {
+      return max(max(0, arr[0]), arr[1]);
     }
-    return arr[arr.length - 1];
+
+    // dp[i] = answer for arr[0, i]
+    int[] dp = new int[n];
+    dp[0] = max(0, arr[0]);
+    dp[1] = max(dp[0], arr[1]);
+
+    // dp[i] = max(dp[i - 1], dp[i - 2], arr[i] + dp[i - 2])
+    // i.e. try extend with arr[i] or take previous best
+    for (int i = 2; i < n; i++) {
+      dp[i] = max(max(dp[i - 1], dp[i - 2]), arr[i] + dp[i - 2]);
+    }
+
+    return dp[n - 1];
   }
+
+  private MaxArraySum() {}
 }
