622f3b7559655fb15c4cd0d8e5449bee7c2a7fd5
Will Molloy <willjoemolloy@gmail.com>
2020-05-14 17:11:39 +1200

    leetcode P485MaxConsecutiveOnes (easy)
    GitOrigin-RevId: 2adba6d2088cce9ed93d5ca317bb9c4282aed690

---
+++ leetcode/src/main/java/wilmol/leetcode/problemset/algorithms/easy/P485MaxConsecutiveOnes.java
@@ -0,0 +1,31 @@
+package wilmol.leetcode.problemset.algorithms.easy;
+
+/**
+ * Created by wilmol on 2020-05-14.
+ *
+ * <p><a
+ * href=https://leetcode.com/problems/max-consecutive-ones>https://leetcode.com/problems/max-consecutive-ones/</a>
+ *
+ * <p>Runtime: O(n)
+ *
+ * <p>Extra space: O(1)
+ *
+ * <p>Key: The runs of 1s are disjoint so can reset {@code current} and update {@code max} when they
+ * end.
+ */
+class P485MaxConsecutiveOnes {
+
+  public int findMaxConsecutiveOnes(int[] nums) {
+    int max = 0;
+    int current = 0;
+    for (int i : nums) {
+      if (i == 0) {
+        max = Math.max(max, current);
+        current = 0;
+      } else {
+        current++;
+      }
+    }
+    return Math.max(max, current);
+  }
+}
