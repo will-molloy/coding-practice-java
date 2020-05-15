6cad600e9017fc136401f6aa64efa275af95187a
Will Molloy <willjoemolloy@gmail.com>
2020-05-16 00:16:38 +1200

    leetcode P918MaximumSumCircularSubarray (medium)
    Problem description is not very clear. Need to look at hint #2 to
    understand the wrap around case.
    
    May 2020 Challenge.
    
    GitOrigin-RevId: 64f10f75c8df4a42f071ab8ecdaff56fb1e8278a

---
+++ leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/medium/P918MaximumSumCircularSubarrayTest.java
@@ -0,0 +1,41 @@
+package wilmol.leetcode.problemset.algorithms.medium;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+/** Created by wilmol on 2020-05-16. */
+class P918MaximumSumCircularSubarrayTest {
+
+  private final P918MaximumSumCircularSubarray p918 = new P918MaximumSumCircularSubarray();
+
+  @Test
+  void example1() {
+    assertThat(p918.maxSubarraySumCircular(new int[] {1, -2, 3, -2})).isEqualTo(3);
+  }
+
+  @Test
+  void example2() {
+    assertThat(p918.maxSubarraySumCircular(new int[] {5, -3, 5})).isEqualTo(10);
+  }
+
+  @Test
+  void example3() {
+    assertThat(p918.maxSubarraySumCircular(new int[] {3, -1, 2, -1})).isEqualTo(4);
+  }
+
+  @Test
+  void example4() {
+    assertThat(p918.maxSubarraySumCircular(new int[] {3, -2, 2, -3})).isEqualTo(3);
+  }
+
+  @Test
+  void example5() {
+    assertThat(p918.maxSubarraySumCircular(new int[] {-2, -3, -1})).isEqualTo(-1);
+  }
+
+  @Test
+  void wrapsAroundOnZero() {
+    assertThat(p918.maxSubarraySumCircular(new int[] {2, -2, 2, 7, 8, 0})).isEqualTo(19);
+  }
+}
