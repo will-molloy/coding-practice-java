078c68f8c2f1703d07aa6eb1b4d7ea4dad2223cd
Will Molloy <willjoemolloy@gmail.com>
2020-05-17 16:20:42 +1200

    leetcode P1450NumberOfStudentsDoingHomeworkAtAGivenTime (easy)
    Weekly Contest 189.
    
    GitOrigin-RevId: 2d5a10cc505322137874a6e37745888e9534c135

---
+++ leetcode/src/main/java/wilmol/leetcode/problemset/algorithms/easy/P1450NumberOfStudentsDoingHomeworkAtAGivenTime.java
@@ -0,0 +1,28 @@
+package wilmol.leetcode.problemset.algorithms.easy;
+
+/**
+ * Created by wilmol on 2020-05-17.
+ *
+ * <p><a
+ * href=https://leetcode.com/problems/number-of-students-doing-homework-at-a-given-time>https://leetcode.com/problems/number-of-students-doing-homework-at-a-given-time/</a>
+ *
+ * <p>Runtime: O(n)
+ *
+ * <p>Extra space: O(1)
+ *
+ * <p>Key: Count how many meet the condition, the elements don't influence each other.
+ */
+class P1450NumberOfStudentsDoingHomeworkAtAGivenTime {
+
+  public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+    int n = startTime.length;
+
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+      if (queryTime >= startTime[i] && queryTime <= endTime[i]) {
+        count++;
+      }
+    }
+    return count;
+  }
+}
