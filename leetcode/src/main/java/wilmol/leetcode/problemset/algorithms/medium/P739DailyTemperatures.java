7ca4b56a8ab05c426ce456e04393f04921a718ee
Will Molloy <willjoemolloy@gmail.com>
2020-05-19 23:44:45 +1200

    leetcode alternative stack solution for P739DailyTemperatures
    GitOrigin-RevId: 9fdbbb71ba9d60468a892894209232700312da8e

--- leetcode/src/main/java/wilmol/leetcode/problemset/algorithms/medium/P739DailyTemperatures.java
+++ leetcode/src/main/java/wilmol/leetcode/problemset/algorithms/medium/P739DailyTemperatures.java
@@ -1,6 +1,8 @@
 package wilmol.leetcode.problemset.algorithms.medium;
 
+import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.Deque;
 
 /**
  * Created by wilmol on 2020-05-19.
@@ -8,45 +10,84 @@ import java.util.Arrays;
  * <p><a
  * href=https://leetcode.com/problems/daily-temperatures>https://leetcode.com/problems/daily-temperatures/</a>
  *
- * <p>Runtime: O(n) (inner loop is constant since temps are in fixed range [30, 100])
- *
- * <p>Extra space: O(1)
- *
- * <p>Key: Iterate in reverse so can store day of each future temperature.
+ * <p>Key: Iterate in reverse so can store day of each future temp.
  *
  * @see P901OnlineStockSpan
  */
-class P739DailyTemperatures {
+interface P739DailyTemperatures {
 
-  // TODO smarter solution with stack
-  //  see https://leetcode.com/problems/daily-temperatures/solution/
+  int[] dailyTemperatures(int[] temps);
 
-  public int[] dailyTemperatures(int[] temps) {
-    int n = temps.length;
+  /**
+   * Solution which uses a map to track earliest day of warmer temp.
+   *
+   * <p>Runtime: O(n) (inner loop is O(1) since temps are in fixed range [30, 100])
+   *
+   * <p>Extra space: O(1) (fixed sized array/map)
+   */
+  class Map implements P739DailyTemperatures {
 
-    int[] result = new int[n];
-    Arrays.fill(result, Integer.MAX_VALUE);
+    @Override
+    public int[] dailyTemperatures(int[] temps) {
+      int n = temps.length;
 
-    // holds earliest day that a temperature occurs (map)
-    int[] earliestDayForTemp = new int[71];
-    Arrays.fill(earliestDayForTemp, Integer.MAX_VALUE);
+      int[] result = new int[n];
+      Arrays.fill(result, Integer.MAX_VALUE);
 
-    // iterate in reverse because result depends on future days
-    for (int day = n - 1; day >= 0; day--) {
-      int temp = temps[day];
+      // holds earliest day that a temp occurs (map)
+      int[] earliestDayForTemp = new int[71];
+      Arrays.fill(earliestDayForTemp, Integer.MAX_VALUE);
 
-      // check if future day is recorded for larger temps
-      for (int largerTemp = temp + 1; largerTemp <= 100; largerTemp++) {
-        if (earliestDayForTemp[largerTemp - 30] == Integer.MAX_VALUE) {
-          continue;
+      // iterate in reverse because result depends on future days
+      for (int day = n - 1; day >= 0; day--) {
+        int temp = temps[day];
+
+        // check if future day is recorded for warmer temps
+        for (int warmerTemp = temp + 1; warmerTemp <= 100; warmerTemp++) {
+          if (earliestDayForTemp[warmerTemp - 30] == Integer.MAX_VALUE) {
+            continue;
+          }
+          // want the earliest day for a warmer temp
+          result[day] = Math.min(result[day], earliestDayForTemp[warmerTemp - 30] - day);
         }
-        // want the earliest day for a larger temp
-        result[day] = Math.min(result[day], earliestDayForTemp[largerTemp - 30] - day);
+        result[day] = result[day] == Integer.MAX_VALUE ? 0 : result[day];
+        earliestDayForTemp[temp - 30] = day;
       }
-      result[day] = result[day] == Integer.MAX_VALUE ? 0 : result[day];
-      earliestDayForTemp[temp - 30] = day;
-    }
 
-    return result;
+      return result;
+    }
+  }
+
+  /**
+   * Solution which uses a stack to track day of warmer temp.
+   *
+   * <p>Runtime: O(n) (amortised analysis; only pop an element at most once, so inner loop is O(1))
+   *
+   * <p>Extra space: O(n)
+   */
+  class Stack implements P739DailyTemperatures {
+
+    @Override
+    public int[] dailyTemperatures(int[] temps) {
+      int n = temps.length;
+
+      int[] result = new int[n];
+
+      // store index of warmer temp
+      Deque<Integer> stack = new ArrayDeque<>();
+
+      // iterate in reverse because result depends on future days
+      for (int day = n - 1; day >= 0; day--) {
+        while (!stack.isEmpty() && temps[day] >= temps[stack.peek()]) {
+          // remove smaller temps as earlier days won't depend on them
+          // (avoids need to scan the list each iteration)
+          stack.pop();
+        }
+        result[day] = stack.isEmpty() ? 0 : stack.peek() - day;
+        stack.push(day);
+      }
+
+      return result;
+    }
   }
 }
--- leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/medium/P739DailyTemperaturesTest.java
+++ leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/medium/P739DailyTemperaturesTest.java
@@ -2,26 +2,32 @@ package wilmol.leetcode.problemset.algorithms.medium;
 
 import static com.google.common.truth.Truth.assertThat;
 
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 /** Created by wilmol on 2020-05-19. */
 class P739DailyTemperaturesTest {
 
-  private final P739DailyTemperatures p739 = new P739DailyTemperatures();
-
-  @Test
-  void example() {
+  @ParameterizedTest
+  @MethodSource("p739")
+  void example(P739DailyTemperatures p739) {
     assertThat(p739.dailyTemperatures(new int[] {73, 74, 75, 71, 69, 72, 76, 73}))
         .asList()
         .containsExactly(1, 1, 4, 2, 1, 1, 0, 0)
         .inOrder();
   }
 
-  @Test
-  void tempMustBeLargerNotEqual() {
+  @ParameterizedTest
+  @MethodSource("p739")
+  void tempMustBeLargerNotEqual(P739DailyTemperatures p739) {
     assertThat(p739.dailyTemperatures(new int[] {89, 62, 70, 58, 47, 47, 46, 76, 100, 70}))
         .asList()
         .containsExactly(8, 1, 5, 4, 3, 2, 1, 1, 0, 0)
         .inOrder();
+  }
+
+  static Stream<P739DailyTemperatures> p739() {
+    return Stream.of(new P739DailyTemperatures.Map(), new P739DailyTemperatures.Stack());
   }
 }
