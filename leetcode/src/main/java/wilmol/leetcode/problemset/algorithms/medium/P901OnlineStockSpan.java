4122d36af9ad6a1c78d5d4fc4cf83d414861d97d
Will Molloy <willjoemolloy@gmail.com>
2020-05-19 22:50:26 +1200

    leetcode P901OnlineStockSpan (medium)
    May 2020 Challenge.
    
    GitOrigin-RevId: 6a93e6498fa9fa86bd2faf00951daae50f974567

---
+++ leetcode/src/main/java/wilmol/leetcode/problemset/algorithms/medium/P901OnlineStockSpan.java
@@ -0,0 +1,43 @@
+package wilmol.leetcode.problemset.algorithms.medium;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * Created by wilmol on 2020-05-19.
+ *
+ * <p><a
+ * href=https://leetcode.com/problems/online-stock-span>https://leetcode.com/problems/online-stock-span/</a>
+ *
+ * <p>Runtime: O(n) (amortised cost of {@code next} is O(1))
+ *
+ * <p>Extra space: O(n)
+ *
+ * <p>Key: Stack because we want to access most recently added items first.
+ */
+class P901OnlineStockSpan {
+
+  // holds (price, span)
+  private final Deque<int[]> stack = new ArrayDeque<>();
+
+  public int next(int price) {
+    // initially 1 to include itself
+    int span = 1;
+
+    // compared to scanning the list each time
+    // we collapse previously smaller prices into the current price
+    // correct because future prices will see the current price first
+    // i.e.
+    // - if smaller: they wont look at the previous prices
+    // - if larger: they'll end up adding what this price added up (repeated work)
+    while (!stack.isEmpty()) {
+      if (stack.peek()[0] <= price) {
+        span += stack.pop()[1];
+      } else {
+        break;
+      }
+    }
+    stack.push(new int[] {price, span});
+    return span;
+  }
+}
