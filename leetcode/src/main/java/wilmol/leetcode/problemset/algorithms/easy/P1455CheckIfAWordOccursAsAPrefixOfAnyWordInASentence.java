529f979b88580fd3feee4f81c8f915ed6911f679
Will Molloy <willjoemolloy@gmail.com>
2020-05-24 16:26:52 +1200

    leetcode P1455CheckIfAWordOccursAsAPrefixOfAnyWordInASentence (easy)
    Weekly Contesnt 190.
    
    GitOrigin-RevId: 5bc8a13cf9032f1b349cb150a75f934f783b9833

---
+++ leetcode/src/main/java/wilmol/leetcode/problemset/algorithms/easy/P1455CheckIfAWordOccursAsAPrefixOfAnyWordInASentence.java
@@ -0,0 +1,27 @@
+package wilmol.leetcode.problemset.algorithms.easy;
+
+/**
+ * Created by wilmol on 2020-05-24.
+ *
+ * <p><a
+ * href=https://leetcode.com/problems/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence>https://leetcode.com/problems/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/</a>
+ *
+ * <p>Runtime: O(n)
+ *
+ * <p>Extra space: O(n)
+ *
+ * <p>Key: {@link String#startsWith} method.
+ */
+class P1455CheckIfAWordOccursAsAPrefixOfAnyWordInASentence {
+
+  public int isPrefixOfWord(String sentence, String searchWord) {
+
+    String[] words = sentence.split(" ");
+    for (int i = 0; i < words.length; i++) {
+      if (words[i].startsWith(searchWord)) {
+        return i + 1;
+      }
+    }
+    return -1;
+  }
+}
