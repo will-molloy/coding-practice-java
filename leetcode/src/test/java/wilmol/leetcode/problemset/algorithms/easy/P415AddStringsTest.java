7a6ec427d2d3f20ed13fe202baa33dc361536819
Will Molloy <willjoemolloy@gmail.com>
2020-05-25 15:40:04 +1200

    leetcode P415AddStrings (easy)
    GitOrigin-RevId: 110f8947dc22e6e0f7e9e19f9c332a79ecedf5ae

---
+++ leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/easy/P415AddStringsTest.java
@@ -0,0 +1,41 @@
+package wilmol.leetcode.problemset.algorithms.easy;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+/** Created by wilmol on 2020-05-25. */
+class P415AddStringsTest {
+
+  private final P415AddStrings p415 = new P415AddStrings();
+
+  @Test
+  void addDigits() {
+    assertThat(p415.addStrings("1", "1")).isEqualTo("2");
+  }
+
+  @Test
+  void addDigitsWithCarry() {
+    assertThat(p415.addStrings("1", "9")).isEqualTo("10");
+  }
+
+  @Test
+  void addSeveralDigits() {
+    assertThat(p415.addStrings("123", "123")).isEqualTo("246");
+  }
+
+  @Test
+  void addSeveralDigitsWithCarry() {
+    assertThat(p415.addStrings("9999", "9999")).isEqualTo("19998");
+  }
+
+  @Test
+  void addNumbersOfDifferentLength() {
+    assertThat(p415.addStrings("9999", "0")).isEqualTo("9999");
+  }
+
+  @Test
+  void addNumbersOfDifferentLengthWithCarry() {
+    assertThat(p415.addStrings("9999", "1")).isEqualTo("10000");
+  }
+}
