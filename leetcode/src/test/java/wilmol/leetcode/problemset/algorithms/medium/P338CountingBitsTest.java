1190ffb01641f90e2687b6bc312aa02ae32806cc
Will Molloy <willjoemolloy@gmail.com>
2020-05-27 15:19:46 +1200

    leetcode P338CountingBits (medium)
    GitOrigin-RevId: 8c9176f7a8485fda8efe2f1e9492f7932b0cb1f8

---
+++ leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/medium/P338CountingBitsTest.java
@@ -0,0 +1,21 @@
+package wilmol.leetcode.problemset.algorithms.medium;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+/** Created by wilmol on 2020-05-27. */
+class P338CountingBitsTest {
+
+  private final P338CountingBits p338 = new P338CountingBits();
+
+  @Test
+  void example1() {
+    assertThat(p338.countBits(2)).asList().containsExactly(0, 1, 1).inOrder();
+  }
+
+  @Test
+  void example2() {
+    assertThat(p338.countBits(5)).asList().containsExactly(0, 1, 1, 2, 1, 2).inOrder();
+  }
+}
