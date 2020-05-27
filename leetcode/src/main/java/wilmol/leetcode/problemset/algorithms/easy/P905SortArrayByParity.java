22b3faa56c425ed3137839537a32857914329e69
Will Molloy <willjoemolloy@gmail.com>
2020-05-27 16:56:45 +1200

    leetcode P905SortArrayByParity (easy)
    GitOrigin-RevId: 979a7364486b9c2dd7004ecb859f69fd4e0cc75f

---
+++ leetcode/src/main/java/wilmol/leetcode/problemset/algorithms/easy/P905SortArrayByParity.java
@@ -0,0 +1,74 @@
+package wilmol.leetcode.problemset.algorithms.easy;
+
+/**
+ * Created by wilmol on 2020-05-27.
+ *
+ * <p><a
+ * href=https://leetcode.com/problems/sort-array-by-parity>https://leetcode.com/problems/sort-array-by-parity/</a>
+ */
+interface P905SortArrayByParity {
+
+  int[] sortArrayByParity(int[] a);
+
+  /**
+   * Linear solution.
+   *
+   * <p>Runtime: O(n)
+   *
+   * <p>Extra space: O(n)
+   *
+   * <p>Key: Filter for evens then odds, more efficient than sorting.
+   */
+  class Linear implements P905SortArrayByParity {
+
+    @Override
+    public int[] sortArrayByParity(int[] a) {
+      int n = a.length;
+      int[] answer = new int[n];
+      int i = 0;
+      for (int value : a) {
+        if (value % 2 == 0) {
+          answer[i++] = value;
+        }
+      }
+      for (int value : a) {
+        if (value % 2 != 0) {
+          answer[i++] = value;
+        }
+      }
+      return answer;
+    }
+  }
+
+  /**
+   * Linear in place solution.
+   *
+   * <p>Runtime: O(n)
+   *
+   * <p>Extra space: O(1)
+   *
+   * <p>Key: Swap even values to the front.
+   */
+  class LinearInPlace implements P905SortArrayByParity {
+
+    @Override
+    public int[] sortArrayByParity(int[] a) {
+      int n = a.length;
+      int i = 0;
+      for (int j = 0; j < n; j++) {
+        if (a[j] % 2 == 0) {
+          // swap even value to the front
+          swap(a, i, j);
+          i++;
+        }
+      }
+      return a;
+    }
+
+    private void swap(int[] a, int i, int j) {
+      int temp = a[i];
+      a[i] = a[j];
+      a[j] = temp;
+    }
+  }
+}
---
+++ leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/easy/P905SortArrayByParityTest.java
@@ -0,0 +1,24 @@
+package wilmol.leetcode.problemset.algorithms.easy;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+/** Created by wilmol on 2020-05-27. */
+class P905SortArrayByParityTest {
+
+  @ParameterizedTest
+  @MethodSource("p905")
+  void example(P905SortArrayByParity p905) {
+    assertThat(p905.sortArrayByParity(new int[] {3, 1, 2, 4}))
+        .asList()
+        .containsExactly(2, 4, 3, 1)
+        .inOrder();
+  }
+
+  static Stream<P905SortArrayByParity> p905() {
+    return Stream.of(new P905SortArrayByParity.Linear(), new P905SortArrayByParity.LinearInPlace());
+  }
+}
