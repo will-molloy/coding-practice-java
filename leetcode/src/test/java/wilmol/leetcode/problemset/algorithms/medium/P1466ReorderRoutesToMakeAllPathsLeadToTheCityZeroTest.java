473d0902b251e2059a3363054ba891df6189b404
Will Molloy <willjoemolloy@gmail.com>
2020-05-31 20:30:52 +1200

    leetcode P1466ReorderRoutesToMakeAllPathsLeadToTheCityZero (medium)
    Weekly Contest 191.
    
    GitOrigin-RevId: 55958cb41857db76d540035f5d2f0983dff7f809

---
+++ leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/medium/P1466ReorderRoutesToMakeAllPathsLeadToTheCityZeroTest.java
@@ -0,0 +1,28 @@
+package wilmol.leetcode.problemset.algorithms.medium;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+/** Created by wilmol on 2020-05-31. */
+class P1466ReorderRoutesToMakeAllPathsLeadToTheCityZeroTest {
+
+  private final P1466ReorderRoutesToMakeAllPathsLeadToTheCityZero p1466 =
+      new P1466ReorderRoutesToMakeAllPathsLeadToTheCityZero();
+
+  @ParameterizedTest
+  @MethodSource
+  void examples(int n, int[][] connections, int expected) {
+    assertThat(p1466.minReorder(n, connections)).isEqualTo(expected);
+  }
+
+  static Stream<Arguments> examples() {
+    return Stream.of(
+        Arguments.of(6, new int[][] {{0, 1}, {1, 3}, {2, 3}, {4, 0}, {4, 5}}, 3),
+        Arguments.of(5, new int[][] {{1, 0}, {1, 2}, {3, 2}, {3, 4}}, 2),
+        Arguments.of(3, new int[][] {{1, 0}, {2, 0}}, 0));
+  }
+}
