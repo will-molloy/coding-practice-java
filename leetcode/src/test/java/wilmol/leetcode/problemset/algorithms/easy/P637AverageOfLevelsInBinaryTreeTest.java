9a14bfd4f4766e8379823904d8cfa99dafcc9448
Will Molloy <willjoemolloy@gmail.com>
2020-06-02 23:45:11 +1200

    leetcode P637AverageOfLevelsInBinaryTree (easy)
    GitOrigin-RevId: 4db96063f1a21842eb63c278af51ecd26a7a70ca

---
+++ leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/easy/P637AverageOfLevelsInBinaryTreeTest.java
@@ -0,0 +1,31 @@
+package wilmol.leetcode.problemset.algorithms.easy;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.Test;
+import wilmol.leetcode.common.BinaryTreeNode;
+
+/** Created by wilmol on 2020-06-02. */
+class P637AverageOfLevelsInBinaryTreeTest {
+
+  private final P637AverageOfLevelsInBinaryTree p637 = new P637AverageOfLevelsInBinaryTree();
+
+  @Test
+  void example1() {
+    assertThat(p637.averageOfLevels(BinaryTreeNode.fromLevelOrder(3, 9, 20, null, null, 15, 7)))
+        .containsExactly(3d, 14.5d, 11d)
+        .inOrder();
+  }
+
+  @Test
+  void integerOverflow() {
+    assertThat(
+            p637.averageOfLevels(
+                BinaryTreeNode.fromLevelOrder(
+                    Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE)))
+        .containsExactly(
+            Integer.valueOf(Integer.MAX_VALUE).doubleValue(),
+            Integer.valueOf(Integer.MAX_VALUE).doubleValue())
+        .inOrder();
+  }
+}
