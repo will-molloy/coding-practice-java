87a5001041a0a362cf3f7df8fd2fb4ed6e2d057a
Will Molloy <willjoemolloy@gmail.com>
2020-06-03 00:41:49 +1200

    leetcode P133CloneGraph (medium)
    GitOrigin-RevId: 3ebe2bcace2a8c91bdefd52d2beba6af9f58c7bc

---
+++ leetcode/src/main/java/wilmol/leetcode/problemset/algorithms/medium/P133CloneGraph.java
@@ -0,0 +1,58 @@
+package wilmol.leetcode.problemset.algorithms.medium;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by wilmol on 2020-06-02.
+ *
+ * <p><a
+ * href=https://leetcode.com/problems/clone-graph>https://leetcode.com/problems/clone-graph/</a>
+ *
+ * <p>Runtime: O(n)
+ *
+ * <p>Extra space: O(n)
+ *
+ * <p>Key: Only create the copied nodes once. Used a {@code HashMap} to achieve this.
+ */
+class P133CloneGraph {
+
+  public Node cloneGraph(Node node) {
+    if (node == null) {
+      return null;
+    }
+    return dfs(node, new HashMap<>());
+  }
+
+  private Node dfs(Node node, Map<Integer, Node> nodeCopies) {
+    Node copy = nodeCopies.get(node.val);
+    if (copy == null) {
+      copy = new Node(node.val);
+      nodeCopies.put(node.val, copy);
+      for (Node adjNode : node.neighbors) {
+        copy.neighbors.add(dfs(adjNode, nodeCopies));
+      }
+    }
+    return copy;
+  }
+
+  static class Node {
+    private final int val;
+    private final List<Node> neighbors;
+
+    Node(int val) {
+      this.val = val;
+      this.neighbors = new ArrayList<>();
+    }
+
+    int val() {
+      return val;
+    }
+
+    List<Node> neighbors() {
+      return neighbors;
+    }
+  }
+}
---
+++ leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/medium/P133CloneGraphTest.java
@@ -0,0 +1,99 @@
+package wilmol.leetcode.problemset.algorithms.medium;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.Test;
+import wilmol.leetcode.problemset.algorithms.medium.P133CloneGraph.Node;
+
+/** Created by wilmol on 2020-06-03. */
+class P133CloneGraphTest {
+
+  private final P133CloneGraph p133 = new P133CloneGraph();
+
+  @Test
+  void example1() {
+    // Given
+    Node one = new Node(1);
+    Node two = new Node(2);
+    Node three = new Node(3);
+    Node four = new Node(4);
+
+    one.neighbors().add(two);
+    one.neighbors().add(four);
+
+    two.neighbors().add(one);
+    two.neighbors().add(three);
+
+    three.neighbors().add(two);
+    three.neighbors().add(four);
+
+    four.neighbors().add(one);
+    four.neighbors().add(three);
+
+    // When
+    Node oneCopy = p133.cloneGraph(one);
+    Node twoCopy = oneCopy.neighbors().get(0);
+    Node threeCopy = twoCopy.neighbors().get(1);
+    Node fourCopy = oneCopy.neighbors().get(1);
+
+    // Then
+    assertThat(oneCopy).isNotSameInstanceAs(one);
+    assertThat(twoCopy).isNotSameInstanceAs(two);
+    assertThat(threeCopy).isNotSameInstanceAs(three);
+    assertThat(fourCopy).isNotSameInstanceAs(four);
+
+    assertThat(oneCopy.val()).isEqualTo(1);
+    assertThat(twoCopy.val()).isEqualTo(2);
+    assertThat(threeCopy.val()).isEqualTo(3);
+    assertThat(fourCopy.val()).isEqualTo(4);
+
+    assertThat(oneCopy.neighbors()).containsExactly(twoCopy, fourCopy);
+    assertThat(twoCopy.neighbors()).containsExactly(oneCopy, threeCopy);
+    assertThat(threeCopy.neighbors()).containsExactly(twoCopy, fourCopy);
+    assertThat(fourCopy.neighbors()).containsExactly(oneCopy, threeCopy);
+  }
+
+  @Test
+  void example2() {
+    // Given
+    Node one = new Node(1);
+
+    // When
+    Node oneCopy = p133.cloneGraph(one);
+
+    // Then
+    assertThat(oneCopy).isNotSameInstanceAs(one);
+
+    assertThat(oneCopy.val()).isEqualTo(1);
+
+    assertThat(oneCopy.neighbors()).isEmpty();
+  }
+
+  @Test
+  void example3() {
+    assertThat(p133.cloneGraph(null)).isNull();
+  }
+
+  @Test
+  void example4() {
+    // Given
+    Node one = new Node(1);
+    Node two = new Node(2);
+    one.neighbors().add(two);
+    two.neighbors().add(one);
+
+    // When
+    Node oneCopy = p133.cloneGraph(one);
+    Node twoCopy = oneCopy.neighbors().get(0);
+
+    // Then
+    assertThat(oneCopy).isNotSameInstanceAs(one);
+    assertThat(twoCopy).isNotSameInstanceAs(two);
+
+    assertThat(oneCopy.val()).isEqualTo(1);
+    assertThat(twoCopy.val()).isEqualTo(2);
+
+    assertThat(oneCopy.neighbors()).containsExactly(twoCopy);
+    assertThat(twoCopy.neighbors()).containsExactly(oneCopy);
+  }
+}
