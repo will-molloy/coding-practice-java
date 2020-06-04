af3c816e34f53e317a66817e311ec1d0428e1ec3
Will Molloy <willjoemolloy@gmail.com>
2020-06-05 00:16:59 +1200

    leetcode P1002FindCommonCharacters (easy)
    GitOrigin-RevId: a9329fe63da4ee26d8da7445e1952c50540a0627

---
+++ leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/easy/P1002FindCommonCharactersTest.java
@@ -0,0 +1,37 @@
+package wilmol.leetcode.problemset.algorithms.easy;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.common.collect.ImmutableList;
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import wilmol.testlib.TestLib;
+
+/** Created by wilmol on 2020-06-04. */
+class P1002FindCommonCharactersTest {
+
+  @ParameterizedTest
+  @MethodSource
+  void examples(P1002FindCommonCharacters p1002, String[] a, List<String> expected) {
+    assertThat(p1002.commonChars(a)).containsExactlyElementsIn(expected);
+  }
+
+  static Stream<Arguments> examples() {
+    return TestLib.cartesianProduct(p1002Solutions(), examplesInputsAndOutputs());
+  }
+
+  private static List<Arguments> p1002Solutions() {
+    return ImmutableList.of(
+        Arguments.of(new P1002FindCommonCharacters.ArraysAndLoops()),
+        Arguments.of(new P1002FindCommonCharacters.MapsAndStreams()));
+  }
+
+  private static List<Arguments> examplesInputsAndOutputs() {
+    return ImmutableList.of(
+        Arguments.of(new String[] {"bella", "label", "roller"}, ImmutableList.of("e", "l", "l")),
+        Arguments.of(new String[] {"cool", "lock", "cook"}, ImmutableList.of("c", "o")));
+  }
+}
