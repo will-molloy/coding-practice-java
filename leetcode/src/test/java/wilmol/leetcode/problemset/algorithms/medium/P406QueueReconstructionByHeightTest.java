02e02e5ac608df6d6d1e3ce2a3c6336c2bf36b1e
Will Molloy <willjoemolloy@gmail.com>
2020-06-07 12:54:01 +1200

    leetcode P406QueueReconstructionByHeight (medium)
    GitOrigin-RevId: 0094a027a97002a455224b710179a29c72138cb7

---
+++ leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/medium/P406QueueReconstructionByHeightTest.java
@@ -0,0 +1,17 @@
+package wilmol.leetcode.problemset.algorithms.medium;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+/** Created by wilmol on 2020-06-07. */
+class P406QueueReconstructionByHeightTest {
+
+  private final P406QueueReconstructionByHeight p406 = new P406QueueReconstructionByHeight();
+
+  @Test
+  void example() {
+    assertThat(p406.reconstructQueue(new int[][] {{7, 0}, {4, 4}, {7, 1}, {5, 0}, {6, 1}, {5, 2}}))
+        .isEqualTo(new int[][] {{5, 0}, {7, 0}, {5, 2}, {6, 1}, {4, 4}, {7, 1}});
+  }
+}
