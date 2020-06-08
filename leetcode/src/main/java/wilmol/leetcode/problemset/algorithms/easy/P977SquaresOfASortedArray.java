3a57f8f6c10aad08e8fb7e2e9555a4232114da57
Will Molloy <willjoemolloy@gmail.com>
2020-06-08 22:24:50 +1200

    leetcode P977SquaresOfASortedArray (easy)
    GitOrigin-RevId: 2988659b6a72af8e2fdbe89e2d3df497a472223f

---
+++ leetcode/src/main/java/wilmol/leetcode/problemset/algorithms/easy/P977SquaresOfASortedArray.java
@@ -0,0 +1,54 @@
+package wilmol.leetcode.problemset.algorithms.easy;
+
+/**
+ * Created by wilmol on 2020-06-08.
+ *
+ * <p><a
+ * href=https://leetcode.com/problems/squares-of-a-sorted-array>https://leetcode.com/problems/squares-of-a-sorted-array/</a>
+ *
+ * <p>Runtime: O(n)
+ *
+ * <p>Extra space: O(1)
+ *
+ * <p>Key: Array is partitioned into negative half and positive half; negative half squares are
+ * arranged in descending order while positive half squares are arranged in ascending order. So
+ * having two pointers (one for each half) is optimal.
+ */
+class P977SquaresOfASortedArray {
+
+  public int[] sortedSquares(int[] a) {
+    int n = a.length;
+
+    // two pointers, one for positive numbers, one for negative numbers
+    int positiveI = 0;
+    // find where negative becomes positive
+    while (positiveI < n && a[positiveI] < 0) {
+      positiveI++;
+    }
+    int negativeI = positiveI - 1;
+
+    int i = 0;
+    int[] result = new int[n];
+    // since positive half squares are arranged in ascending order, increase positive index pointer
+    // since negative half squares are arranged in descending order, decrease negative index pointer
+    while (positiveI < n && negativeI >= 0) {
+      if (a[negativeI] * a[negativeI] < a[positiveI] * a[positiveI]) {
+        result[i++] = a[negativeI] * a[negativeI];
+        negativeI--;
+      } else {
+        result[i++] = a[positiveI] * a[positiveI];
+        positiveI++;
+      }
+    }
+    while (positiveI < n) {
+      result[i++] = a[positiveI] * a[positiveI];
+      positiveI++;
+    }
+    while (negativeI >= 0) {
+      result[i++] = a[negativeI] * a[negativeI];
+      negativeI--;
+    }
+
+    return result;
+  }
+}
---
+++ leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/easy/P977SquaresOfASortedArrayTest.java
@@ -0,0 +1,34 @@
+package wilmol.leetcode.problemset.algorithms.easy;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.common.collect.ImmutableList;
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+/** Created by wilmol on 2020-06-08. */
+class P977SquaresOfASortedArrayTest {
+
+  private final P977SquaresOfASortedArray p977 = new P977SquaresOfASortedArray();
+
+  @ParameterizedTest
+  @MethodSource
+  void examples(int[] a, List<Integer> expected) {
+    assertThat(p977.sortedSquares(a)).asList().containsExactlyElementsIn(expected).inOrder();
+  }
+
+  static Stream<Arguments> examples() {
+    return Stream.of(
+        Arguments.of(new int[] {-4, -1, 0, 3, 10}, ImmutableList.of(0, 1, 9, 16, 100)),
+        Arguments.of(new int[] {-7, -3, 2, 3, 11}, ImmutableList.of(4, 9, 9, 49, 121)));
+  }
+
+  @Test
+  void needIndexCheckWhenFindingStartOfPositiveRegion() {
+    assertThat(p977.sortedSquares(new int[] {-1})).asList().containsExactly(1);
+  }
+}
