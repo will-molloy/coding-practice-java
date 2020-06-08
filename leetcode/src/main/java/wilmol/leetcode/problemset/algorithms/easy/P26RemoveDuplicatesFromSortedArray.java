7c1233cebbf2f9f4030860a58a7ab69ecaa66194
Will Molloy <willjoemolloy@gmail.com>
2020-06-09 10:19:23 +1200

    leetcode P26RemoveDuplicatesFromSortedArray (easy) add notes
    GitOrigin-RevId: 15f0e9d28ab0853b3717243be6083a7f0b2abb47

--- leetcode/src/main/java/wilmol/leetcode/problemset/algorithms/easy/P26RemoveDuplicatesFromSortedArray.java
+++ leetcode/src/main/java/wilmol/leetcode/problemset/algorithms/easy/P26RemoveDuplicatesFromSortedArray.java
@@ -6,22 +6,25 @@ package wilmol.leetcode.problemset.algorithms.easy;
  * <p><a
  * href=https://leetcode.com/problems/remove-duplicates-from-sorted-array>https://leetcode.com/problems/remove-duplicates-from-sorted-array</a>
  *
- * <p>Runtime: O(n), O(1) extra memory
+ * <p>Runtime: O(n)
+ *
+ * <p>Extra space: O(1)
+ *
+ * <p>Key: Track length of 'new' array (but write in-place); only write new numbers.
  *
  * @see wilmol.leetcode.problemset.algorithms.medium.P80RemoveDuplicateFromSortedArray2
  */
 class P26RemoveDuplicatesFromSortedArray {
 
   public int removeDuplicates(int[] nums) {
-    if (nums.length <= 1) {
-      return nums.length;
-    }
+    int n = nums.length;
 
+    // initially keep the first element
     int length = 1;
-    for (int i = 1; i < nums.length; i++) {
-      if (nums[i] > nums[length - 1]) {
-        nums[length] = nums[i];
-        length += 1;
+    for (int i = 1; i < n; i++) {
+      if (nums[i] > nums[i - 1]) {
+        // only write new numbers (i.e. those at the start of duplicate runs)
+        nums[length++] = nums[i];
       }
     }
     return length;
