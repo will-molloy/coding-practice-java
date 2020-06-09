4693cbd14f6fd1a7c53944f9677a5069c486db72
Will Molloy <willjoemolloy@gmail.com>
2020-06-09 14:17:48 +1200

    leetcode P941ValidMountainArray (easy)
    GitOrigin-RevId: 0eb478d36d07eeaeb8c7ddeaca19e22e7a5b87e7

---
+++ leetcode/src/main/java/wilmol/leetcode/problemset/algorithms/easy/P941ValidMountainArray.java
@@ -0,0 +1,43 @@
+package wilmol.leetcode.problemset.algorithms.easy;
+
+/**
+ * Created by wilmol on 2020-06-09.
+ *
+ * <p><a
+ * href=https://leetcode.com/problems/valid-mountain-array>https://leetcode.com/problems/valid-mountain-array/</a>
+ *
+ * <p>Runtime: O(n) (one pass)
+ *
+ * <p>Extra space: O(1)
+ *
+ * <p>Key: Separate loops for climb and descent.
+ */
+class P941ValidMountainArray {
+
+  public boolean validMountainArray(int[] a) {
+    int n = a.length;
+    if (n < 3) {
+      return false;
+    }
+
+    // climb mountain until it stops increasing
+    int i = 0;
+    while (i + 1 < n && a[i] < a[i + 1]) {
+      i++;
+    }
+
+    // peak can't be first or last
+    if (i == 0 || i == n - 1) {
+      return false;
+    }
+
+    // ensure descent is strictly decreasing
+    while (i + 1 < n) {
+      if (a[i] <= a[i + 1]) {
+        return false;
+      }
+      i++;
+    }
+    return true;
+  }
+}
