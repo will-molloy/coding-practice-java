ee84087806434fd1559c85ae06f959aa952476d9
Will Molloy <willjoemolloy@gmail.com>
2020-06-09 13:39:15 +1200

    leetcode P1346CheckIfNAndItsDoubleExists (easy)
    GitOrigin-RevId: e65cf0a5882fbe0677ed5210691e803caeafaae2

---
+++ leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/easy/P1346CheckIfNAndItsDoubleExistsTest.java
@@ -0,0 +1,33 @@
+package wilmol.leetcode.problemset.algorithms.easy;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+/** Created by wilmol on 2020-06-09. */
+class P1346CheckIfNAndItsDoubleExistsTest {
+
+  private final P1346CheckIfNAndItsDoubleExists p1346 = new P1346CheckIfNAndItsDoubleExists();
+
+  @ParameterizedTest
+  @MethodSource
+  void examples(int[] arr, boolean expected) {
+    assertThat(p1346.checkIfExist(arr)).isEqualTo(expected);
+  }
+
+  static Stream<Arguments> examples() {
+    return Stream.of(
+        Arguments.of(new int[] {10, 2, 5, 3}, true),
+        Arguments.of(new int[] {7, 1, 14, 11}, true),
+        Arguments.of(new int[] {3, 1, 7, 11}, false));
+  }
+
+  @Test
+  void indexMustBeDifferent() {
+    assertThat(p1346.checkIfExist(new int[] {0})).isFalse();
+  }
+}
