e52fefa1484c2af14bc4469fcf18ecc177022fd9
Will Molloy <willjoemolloy@gmail.com>
2020-06-09 17:31:14 +1200

    leetcode P206ReverseLinkedList (easy) recursive solution
    GitOrigin-RevId: 13c93fdb09cf4d0c2c72aefca6dde4764c478578

--- leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/easy/P206ReverseLinkedListTest.java
+++ leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/easy/P206ReverseLinkedListTest.java
@@ -2,26 +2,33 @@ package wilmol.leetcode.problemset.algorithms.easy;
 
 import static com.google.common.truth.Truth.assertThat;
 
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import wilmol.leetcode.common.ListNode;
 
 /** Created by wilmol on 2020-02-08. */
 class P206ReverseLinkedListTest {
 
-  private final P206ReverseLinkedList func = new P206ReverseLinkedList();
-
-  @Test
-  void example() {
-    assertThat(func.reverseList(ListNode.of(1, 2, 3, 4, 5))).isEqualTo(ListNode.of(5, 4, 3, 2, 1));
+  @ParameterizedTest
+  @MethodSource("p206")
+  void example(P206ReverseLinkedList p206) {
+    assertThat(p206.reverseList(ListNode.of(1, 2, 3, 4, 5))).isEqualTo(ListNode.of(5, 4, 3, 2, 1));
   }
 
-  @Test
-  void emptyList() {
-    assertThat(func.reverseList(null)).isEqualTo(null);
+  @ParameterizedTest
+  @MethodSource("p206")
+  void emptyList(P206ReverseLinkedList p206) {
+    assertThat(p206.reverseList(null)).isEqualTo(null);
   }
 
-  @Test
-  void singletonList() {
-    assertThat(func.reverseList(ListNode.of(1))).isEqualTo(ListNode.of(1));
+  @ParameterizedTest
+  @MethodSource("p206")
+  void singletonList(P206ReverseLinkedList p206) {
+    assertThat(p206.reverseList(ListNode.of(1))).isEqualTo(ListNode.of(1));
+  }
+
+  static Stream<P206ReverseLinkedList> p206() {
+    return Stream.of(new P206ReverseLinkedList.Iterative(), new P206ReverseLinkedList.Recursive());
   }
 }
