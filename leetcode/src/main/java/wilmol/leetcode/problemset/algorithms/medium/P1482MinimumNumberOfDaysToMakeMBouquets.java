221a8a00c67abde2931ce1a4b996dd9d8c0d3c7e
Will Molloy <willjoemolloy@gmail.com>
2020-06-14 17:12:09 +1200

    leetcode P1482MinimumNumberOfDaysToMakeMBouquets (medium)
    Weekly Contest 193.
    
    GitOrigin-RevId: c7df421dae615ae9abfdffdff93f99e5f32caf23

---
+++ leetcode/src/main/java/wilmol/leetcode/problemset/algorithms/medium/P1482MinimumNumberOfDaysToMakeMBouquets.java
@@ -0,0 +1,64 @@
+package wilmol.leetcode.problemset.algorithms.medium;
+
+import java.util.Arrays;
+
+/**
+ * Created by wilmol on 2020-06-14.
+ *
+ * <p><a
+ * href=https://leetcode.com/problems/minimum-number-of-days-to-make-m-bouquets>https://leetcode.com/problems/minimum-number-of-days-to-make-m-bouquets/</a>
+ *
+ * <p>Runtime: O(n * log(max(bloomDays)))
+ *
+ * <p>Extra space: O(1)
+ *
+ * <p>Key: Binary search over the day range.
+ */
+class P1482MinimumNumberOfDaysToMakeMBouquets {
+
+  public int minDays(int[] bloomDays, int m, int k) {
+    int n = bloomDays.length;
+    if (n < m * k) {
+      return -1;
+    }
+
+    int l = 1;
+    int r = Arrays.stream(bloomDays).max().getAsInt();
+    int min = -1;
+    while (l <= r) {
+      int mid = l + (r - l) / 2;
+      if (isOk(bloomDays, m, k, mid)) {
+        // search earlier day (to see if smaller day is ok)
+        r = mid - 1;
+        min = mid;
+      } else {
+        // search later day (when more flowers are born)
+        l = mid + 1;
+      }
+    }
+    return min;
+  }
+
+  private boolean isOk(int[] bloomDays, int m, int k, int day) {
+    // flower count
+    int count = 0;
+    for (int bloomDay : bloomDays) {
+      if (bloomDay <= day) {
+        count++;
+      } else {
+        // reset count as flowers must be adjacent
+        count = 0;
+      }
+      if (count == k) {
+        // picked k flowers, so need 1 less bouquet
+        m--;
+        if (m == 0) {
+          // picked m bouquets, 'day' is ok
+          return true;
+        }
+        count = 0;
+      }
+    }
+    return false;
+  }
+}
