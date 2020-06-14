6abbb1c87b99ad7d6c9455e8aeecbed02c9bee28
Will Molloy <willjoemolloy@gmail.com>
2020-06-14 16:59:31 +1200

    leetcode P1481LeastNumberOfUniqueIntegersAfterKRemovals (medium)
    Weekly Contest 193.
    
    GitOrigin-RevId: f4c5694f04757a9f3621f1fcbf30d6b86542554d

---
+++ leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/medium/P1481LeastNumberOfUniqueIntegersAfterKRemovalsTest.java
@@ -0,0 +1,27 @@
+package wilmol.leetcode.problemset.algorithms.medium;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+/** Created by wilmol on 2020-06-14. */
+class P1481LeastNumberOfUniqueIntegersAfterKRemovalsTest {
+
+  private final P1481LeastNumberOfUniqueIntegersAfterKRemovals p1481 =
+      new P1481LeastNumberOfUniqueIntegersAfterKRemovals();
+
+  @ParameterizedTest
+  @MethodSource
+  void examples(int[] arr, int k, int expected) {
+    assertThat(p1481.findLeastNumOfUniqueInts(arr, k)).isEqualTo(expected);
+  }
+
+  static Stream<Arguments> examples() {
+    return Stream.of(
+        Arguments.of(new int[] {5, 5, 4}, 1, 1),
+        Arguments.of(new int[] {4, 3, 1, 1, 3, 3, 2}, 3, 2));
+  }
+}
