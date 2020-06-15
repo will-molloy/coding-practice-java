776abbb6d1ca8b218e6ab61b636117e2d74cbece
Will Molloy <willjoemolloy@gmail.com>
2020-06-15 19:40:28 +1200

    leetcode P21MergeTwoSortedLists (easy)
    Recursive solution.
    
    GitOrigin-RevId: 44bacd528ad1898a4a2a5689bd949a385931a164

--- leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/easy/P21MergeTwoSortedListsTest.java
+++ leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/easy/P21MergeTwoSortedListsTest.java
@@ -2,34 +2,43 @@ package wilmol.leetcode.problemset.algorithms.easy;
 
 import static com.google.common.truth.Truth.assertThat;
 
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import wilmol.leetcode.common.ListNode;
 
 /** Created by Will on 2019-07-23 at 20:29. */
 class P21MergeTwoSortedListsTest {
 
-  private final P21MergeTwoSortedLists function = new P21MergeTwoSortedLists();
-
-  @Test
-  void example() {
+  @ParameterizedTest
+  @MethodSource("functions")
+  void example(P21MergeTwoSortedLists function) {
     assertThat(function.mergeTwoLists(ListNode.of(1, 2, 4), ListNode.of(1, 3, 4)))
         .isEqualTo(ListNode.of(1, 1, 2, 3, 4, 4));
   }
 
-  @Test
-  void singleList() {
+  @ParameterizedTest
+  @MethodSource("functions")
+  void singleList(P21MergeTwoSortedLists function) {
     assertThat(function.mergeTwoLists(ListNode.of(1, 2, 4), null)).isEqualTo(ListNode.of(1, 2, 4));
     assertThat(function.mergeTwoLists(null, ListNode.of(1, 3, 4))).isEqualTo(ListNode.of(1, 3, 4));
   }
 
-  @Test
-  void listsDontOverlap() {
+  @ParameterizedTest
+  @MethodSource("functions")
+  void listsDontOverlap(P21MergeTwoSortedLists function) {
     assertThat(function.mergeTwoLists(ListNode.of(1, 2, 3), ListNode.of(7, 8, 9)))
         .isEqualTo(ListNode.of(1, 2, 3, 7, 8, 9));
   }
 
-  @Test
-  void bothEmpty() {
-    assertThat(function.mergeTwoLists(null, null)).isEqualTo(null);
+  @ParameterizedTest
+  @MethodSource("functions")
+  void bothEmpty(P21MergeTwoSortedLists function) {
+    assertThat(function.mergeTwoLists(null, null)).isNull();
+  }
+
+  static Stream<P21MergeTwoSortedLists> functions() {
+    return Stream.of(
+        new P21MergeTwoSortedLists.Iterative(), new P21MergeTwoSortedLists.Recursive());
   }
 }
