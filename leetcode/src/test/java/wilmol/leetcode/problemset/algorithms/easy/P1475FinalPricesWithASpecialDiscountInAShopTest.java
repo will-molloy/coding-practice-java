cdb90a02b0ba77130ffb699f718df1d7103ef57a
Will Molloy <willjoemolloy@gmail.com>
2020-06-16 23:28:37 +1200

    leetcode P1475FinalPricesWithASpecialDiscountInAShop (easy)
    'Easy' solution used. O(n) 'medium' solution is possible.
    
    GitOrigin-RevId: d4a4fe55783f683a11dbe86b6529dfe9f793e573

---
+++ leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/easy/P1475FinalPricesWithASpecialDiscountInAShopTest.java
@@ -0,0 +1,30 @@
+package wilmol.leetcode.problemset.algorithms.easy;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.common.collect.ImmutableList;
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+/** Created by wilmol on 2020-06-16. */
+class P1475FinalPricesWithASpecialDiscountInAShopTest {
+
+  private final P1475FinalPricesWithASpecialDiscountInAShop p1475 =
+      new P1475FinalPricesWithASpecialDiscountInAShop();
+
+  @ParameterizedTest
+  @MethodSource
+  void examples(int[] prices, List<Integer> expected) {
+    assertThat(p1475.finalPrices(prices)).asList().containsExactlyElementsIn(expected).inOrder();
+  }
+
+  static Stream<Arguments> examples() {
+    return Stream.of(
+        Arguments.of(new int[] {8, 4, 6, 2, 3}, ImmutableList.of(4, 2, 4, 2, 3)),
+        Arguments.of(new int[] {1, 2, 3, 4, 5}, ImmutableList.of(1, 2, 3, 4, 5)),
+        Arguments.of(new int[] {10, 1, 1, 6}, ImmutableList.of(9, 0, 1, 6)));
+  }
+}
