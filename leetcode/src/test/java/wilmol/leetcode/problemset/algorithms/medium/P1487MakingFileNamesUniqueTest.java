b10e6b1f30477a258a34367ef45f6ea272d858ba
Will Molloy <willjoemolloy@gmail.com>
2020-06-21 19:55:39 +1200

    leetcode P1487MakingFileNamesUnique (medium)
    Weekly Contest 194.
    
    GitOrigin-RevId: 8b61f37b5da1de93d0fe31ad53fee77fa93663b6

---
+++ leetcode/src/test/java/wilmol/leetcode/problemset/algorithms/medium/P1487MakingFileNamesUniqueTest.java
@@ -0,0 +1,69 @@
+package wilmol.leetcode.problemset.algorithms.medium;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+/** Created by wilmol on 2020-06-21. */
+class P1487MakingFileNamesUniqueTest {
+
+  private final P1487MakingFileNamesUnique p1487 = new P1487MakingFileNamesUnique();
+
+  @Test
+  void example1NoSuffixingNeeded() {
+    assertThat(p1487.getFolderNames(new String[] {"pes", "fifa", "gta", "pes(2019)"}))
+        .asList()
+        .containsExactly("pes", "fifa", "gta", "pes(2019)")
+        .inOrder();
+  }
+
+  @Test
+  void example2SuffixProcedureCreatesReservedNameSoSuffixAgain() {
+    assertThat(p1487.getFolderNames(new String[] {"gta", "gta(1)", "gta", "avalon"}))
+        .asList()
+        .containsExactly("gta", "gta(1)", "gta(2)", "avalon")
+        .inOrder();
+  }
+
+  @Test
+  void example3RepeatSuffixProcedureForFirstUnreservedName() {
+    assertThat(
+            p1487.getFolderNames(
+                new String[] {"onepiece", "onepiece(1)", "onepiece(2)", "onepiece(3)", "onepiece"}))
+        .asList()
+        .containsExactly("onepiece", "onepiece(1)", "onepiece(2)", "onepiece(3)", "onepiece(4)")
+        .inOrder();
+  }
+
+  @Test
+  void example4SuffixInOrder() {
+    assertThat(p1487.getFolderNames(new String[] {"wano", "wano", "wano", "wano"}))
+        .asList()
+        .containsExactly("wano", "wano(1)", "wano(2)", "wano(3)")
+        .inOrder();
+  }
+
+  @Test
+  void example5SuffixTwice() {
+    assertThat(p1487.getFolderNames(new String[] {"kaido", "kaido(1)", "kaido", "kaido(1)"}))
+        .asList()
+        .containsExactly("kaido", "kaido(1)", "kaido(2)", "kaido(1)(1)")
+        .inOrder();
+  }
+
+  @Test
+  void mustTakeEarliestSuffix() {
+    assertThat(p1487.getFolderNames(new String[] {"ji", "ji(4)", "ji"}))
+        .asList()
+        .containsExactly("ji", "ji(4)", "ji(1)")
+        .inOrder();
+  }
+
+  @Test
+  void suffixTwiceBecauseNameBecomesReservedDuringTheProcedure() {
+    assertThat(p1487.getFolderNames(new String[] {"j", "j(4)", "j", "j(1)"}))
+        .asList()
+        .containsExactly("j", "j(4)", "j(1)", "j(1)(1)")
+        .inOrder();
+  }
+}
