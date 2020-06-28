72e823bf8aa2d7e62b0155ef6e2193c403fd44f5
Will Molloy <willjoemolloy@gmail.com>
2020-06-28 12:42:43 +1200

    leetcode refactor P279PerfectSquares (medium)
    Clean up comments a bit.
    
    GitOrigin-RevId: cdf089f1d6563977d7a8103eedc61d391cf417f9

--- leetcode/src/main/java/com/wilmol/leetcode/problemset/algorithms/medium/P279PerfectSquares.java
+++ leetcode/src/main/java/com/wilmol/leetcode/problemset/algorithms/medium/P279PerfectSquares.java
@@ -9,13 +9,12 @@ import java.util.List;
  * <p><a
  * href=https://leetcode.com/problems/perfect-squares>https://leetcode.com/problems/perfect-squares/</a>
  *
- * <p>Difficulty: Medium.
- *
  * <p>Runtime: O(n<sup>3/2</sup>)
  *
  * <p>Space: O(n<sup>3/2</sup>)
  *
- * <p>Key: Using dp the problem is the same as {@link P322CoinChange}.
+ * <p>Key: Using dp the problem is the same as {@link P322CoinChange}; first generate squares with
+ * array list.
  *
  * @see P322CoinChange
  */
@@ -29,23 +28,23 @@ class P279PerfectSquares {
       squares.add(i * i);
     }
 
-    // then do dp
-    // dp[i][j] = considering ith numbers in the set to sum to j
+    // dp[i][j] = considering squares[0, i] and min required to sum to j
     int[][] dp = new int[squares.size()][n + 1];
 
+    // base case
+    // numSquares(x) = x when only using 1 squared
     for (int j = 1; j <= n; j++) {
-      // case where only '1' is considered
       dp[0][j] = j;
     }
 
-    // dp[i][j] = min(dp[i - 1][j], dp[i][j - last in set] + 1)
-    // i.e. take old answer (which considered smaller set of squares available) or use new square
-    // available
+    // recursive case
+    // use new square: dp[i][j - new square] + 1
+    // dont use new square: dp[i - 1][j]
+    // take min of these choices
     for (int i = 1; i < squares.size(); i++) {
       for (int j = 1; j <= n; j++) {
-        int lastInSet = squares.get(i);
-        if (lastInSet <= j) {
-          dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - lastInSet] + 1);
+        if (squares.get(i) <= j) {
+          dp[i][j] = Math.min(dp[i][j - squares.get(i)] + 1, dp[i - 1][j]);
         } else {
           dp[i][j] = dp[i - 1][j];
         }
