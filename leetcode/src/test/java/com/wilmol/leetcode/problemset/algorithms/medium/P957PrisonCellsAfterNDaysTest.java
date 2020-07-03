9cfae3727d4fb1b6ef034a83153c822cf10d71bc
Will Molloy <willjoemolloy@gmail.com>
2020-07-03 23:49:37 +1200

    leetcode P957PrisonCellsAfterNDays (medium)
    July 2020 Challenge.
    
    GitOrigin-RevId: 3e38822b14278f9b295f5d467973b58ce924744c

---
+++ leetcode/src/test/java/com/wilmol/leetcode/problemset/algorithms/medium/P957PrisonCellsAfterNDaysTest.java
@@ -0,0 +1,55 @@
+package com.wilmol.leetcode.problemset.algorithms.medium;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * P957PrisonCellsAfterNDaysTest.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P957PrisonCellsAfterNDaysTest {
+
+  private final P957PrisonCellsAfterNDays p957 = new P957PrisonCellsAfterNDays();
+
+  @Test
+  void example1DoesntRepeat() {
+    assertThat(p957.prisonAfterNDays(new int[] {0, 1, 0, 1, 1, 0, 0, 1}, 7))
+        .asList()
+        .containsExactly(0, 0, 1, 1, 0, 0, 0, 0)
+        .inOrder();
+  }
+
+  @Test
+  void example2MaxNRepeats() {
+    assertThat(p957.prisonAfterNDays(new int[] {1, 0, 0, 1, 0, 0, 1, 0}, (int) 1e9))
+        .asList()
+        .containsExactly(0, 0, 1, 1, 1, 1, 1, 0)
+        .inOrder();
+  }
+
+  @Test
+  void repeatsOnMultipleOfN() {
+    assertThat(p957.prisonAfterNDays(new int[] {1, 1, 0, 1, 1, 0, 1, 1}, 6))
+        .asList()
+        .containsExactly(0, 0, 1, 0, 0, 1, 0, 0)
+        .inOrder();
+  }
+
+  @Test
+  void repeatsOnN() {
+    assertThat(p957.prisonAfterNDays(new int[] {1, 1, 0, 1, 1, 0, 1, 1}, 2))
+        .asList()
+        .containsExactly(0, 0, 1, 0, 0, 1, 0, 0)
+        .inOrder();
+  }
+
+  @Test
+  void oneIteration() {
+    assertThat(p957.prisonAfterNDays(new int[] {1, 1, 0, 1, 1, 0, 1, 1}, 1))
+        .asList()
+        .containsExactly(0, 0, 1, 0, 0, 1, 0, 0)
+        .inOrder();
+  }
+}
