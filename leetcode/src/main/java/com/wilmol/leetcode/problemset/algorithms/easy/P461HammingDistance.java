9951121b8968ba2b49558e85ae9538a429ef0bc0
Will Molloy <willjoemolloy@gmail.com>
2020-07-05 20:38:06 +1200

    leetcode P461HammingDistance (easy)
    July 2020 Challenge.
    
    GitOrigin-RevId: 05ac51c6671b2c3b6c449f6a8b285da5149a003c

---
+++ leetcode/src/main/java/com/wilmol/leetcode/problemset/algorithms/easy/P461HammingDistance.java
@@ -0,0 +1,41 @@
+package com.wilmol.leetcode.problemset.algorithms.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+/**
+ * <a
+ * href=https://leetcode.com/problems/hamming-distance>https://leetcode.com/problems/hamming-distance/</a>
+ *
+ * <p>Runtime: O(1)
+ *
+ * <p>Extra memory: O(1)
+ *
+ * <p>Key: Convert to fixed length binary then count differing bits. Xor sum simplifies the
+ * differing bits part.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P461HammingDistance {
+
+  public static final int MAX_BIT_LEN = 31;
+
+  public int hammingDistance(int x, int y) {
+    List<Integer> x2 = toReverseBinary(x);
+    List<Integer> y2 = toReverseBinary(y);
+    return IntStream.range(0, MAX_BIT_LEN).map(i -> x2.get(i) ^ y2.get(i)).sum();
+  }
+
+  private List<Integer> toReverseBinary(int i) {
+    List<Integer> list = new ArrayList<>();
+    while (i > 0) {
+      list.add(i % 2);
+      i /= 2;
+    }
+    while (list.size() < MAX_BIT_LEN) {
+      list.add(0);
+    }
+    return list;
+  }
+}
