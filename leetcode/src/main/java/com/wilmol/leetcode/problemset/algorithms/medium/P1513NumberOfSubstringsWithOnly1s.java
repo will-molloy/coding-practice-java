cdf40bc7db77cc9cae488a89aebcb13de8b1d724
Will Molloy <willjoemolloy@gmail.com>
2020-07-12 17:42:43 +1200

    leetcode P1513NumberOfSubstringsWithOnly1s (medium)
    Weekly Contest 197.
    
    GitOrigin-RevId: 4f01cc3e06924cfd117927664b2b50ae627839a3

---
+++ leetcode/src/main/java/com/wilmol/leetcode/problemset/algorithms/medium/P1513NumberOfSubstringsWithOnly1s.java
@@ -0,0 +1,35 @@
+package com.wilmol.leetcode.problemset.algorithms.medium;
+
+/**
+ * <a
+ * href=https://leetcode.com/problems/number-of-substrings-with-only-1s>https://leetcode.com/problems/number-of-substrings-with-only-1s/</a>
+ *
+ * <p>Runtime: O(n)
+ *
+ * <p>Extra space: O(n)
+ *
+ * <p>Key: Hard part is the mod, need to use {@code long} when computing sum otherwise it overflows.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P1513NumberOfSubstringsWithOnly1s {
+
+  private static final int MOD = (int) 1e9 + 7;
+
+  public int numSub(String s) {
+    long count = 0;
+
+    // get all runs of 1s
+    String[] runs = s.split("0");
+    for (String run : runs) {
+      // number of substrings is sum [1, n]
+      count += sum(run.length()) % MOD;
+    }
+    return (int) count;
+  }
+
+  private long sum(long n) {
+    // O(1)
+    return (n * (n + 1)) / 2;
+  }
+}
