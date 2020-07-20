481866aabd2da182198b41fa3401585d2326d13b
Will Molloy <willjoemolloy@gmail.com>
2020-07-20 19:43:29 +1200

    leetcode P203RemoveLinkedListElements (easy)
    GitOrigin-RevId: 9dcbdfe0e10f9846a8cc81587917b67e5601fb04

---
+++ leetcode/src/main/java/com/wilmol/leetcode/problemset/algorithms/easy/P203RemoveLinkedListElements.java
@@ -0,0 +1,37 @@
+package com.wilmol.leetcode.problemset.algorithms.easy;
+
+import com.wilmol.leetcode.common.ListNode;
+
+/**
+ * <a
+ * href=https://leetcode.com/problems/remove-linked-list-elements>https://leetcode.com/problems/remove-linked-list-elements/</a>
+ *
+ * <p>Runtime: O(n)
+ *
+ * <p>Extra memory: O(1)
+ *
+ * <p>Key: Dummy node to handle case head needs to be removed.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P203RemoveLinkedListElements {
+
+  public ListNode removeElements(ListNode node, int val) {
+    ListNode dummy = new ListNode(-1);
+    dummy.next = node;
+    ListNode prev = dummy;
+
+    while (node != null) {
+      if (node.val == val) {
+        // remove current node, keep prev same, haven't tested next node yet
+        prev.next = node.next;
+      } else {
+        // go next
+        prev = node;
+      }
+      // go next
+      node = node.next;
+    }
+    return dummy.next;
+  }
+}
