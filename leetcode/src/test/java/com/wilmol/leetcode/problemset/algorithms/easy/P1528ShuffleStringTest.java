c2193c44e623f53512ae7d1c9635120cdf7182e5
Will Molloy <willjoemolloy@gmail.com>
2020-07-26 17:20:39 +1200

    leetcode P1528ShuffleString (easy)
    Weekly Contest 199.
    
    GitOrigin-RevId: b9d3010db1982f9a80fff540c66ac7f84ae0c864

---
+++ leetcode/src/test/java/com/wilmol/leetcode/problemset/algorithms/easy/P1528ShuffleStringTest.java
@@ -0,0 +1,33 @@
+package com.wilmol.leetcode.problemset.algorithms.easy;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+/**
+ * P1528ShuffleStringTest.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P1528ShuffleStringTest {
+
+  private final P1528ShuffleString p1528 = new P1528ShuffleString();
+
+  @ParameterizedTest
+  @MethodSource
+  void examples(String s, int[] indices, String expected) {
+    assertThat(p1528.restoreString(s, indices)).isEqualTo(expected);
+  }
+
+  static Stream<Arguments> examples() {
+    return Stream.of(
+        Arguments.of("codeleet", new int[] {4, 5, 6, 7, 0, 2, 1, 3}, "leetcode"),
+        Arguments.of("abc", new int[] {0, 1, 2}, "abc"),
+        Arguments.of("aiohn", new int[] {3, 1, 4, 2, 0}, "nihao"),
+        Arguments.of("aaiougrt", new int[] {4, 0, 2, 6, 7, 3, 1, 5}, "arigatou"),
+        Arguments.of("art", new int[] {1, 0, 2}, "rat"));
+  }
+}
