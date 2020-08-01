d545d6c4eeb2bc355d2a1806cb221af30cd148c5
Will Molloy <willjoemolloy@gmail.com>
2020-08-01 19:10:38 +1200

    leetcode P520DetectCapital (easy)
    August 2020 Challenge.
    
    GitOrigin-RevId: 4d61cb12242e4efed432ba6fb5a1843fbb687d30

---
+++ leetcode/src/test/java/com/wilmol/leetcode/problemset/algorithms/easy/P520DetectCapitalTest.java
@@ -0,0 +1,35 @@
+package com.wilmol.leetcode.problemset.algorithms.easy;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * P520DetectCapitalTest.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P520DetectCapitalTest {
+
+  private final P520DetectCapital p520 = new P520DetectCapital();
+
+  @Test
+  void allUpper() {
+    assertThat(p520.detectCapitalUse("USA")).isTrue();
+  }
+
+  @Test
+  void allLower() {
+    assertThat(p520.detectCapitalUse("leetcode")).isTrue();
+  }
+
+  @Test
+  void firstUpper() {
+    assertThat(p520.detectCapitalUse("Google")).isTrue();
+  }
+
+  @Test
+  void wrongUse() {
+    assertThat(p520.detectCapitalUse("FlaG")).isFalse();
+  }
+}
