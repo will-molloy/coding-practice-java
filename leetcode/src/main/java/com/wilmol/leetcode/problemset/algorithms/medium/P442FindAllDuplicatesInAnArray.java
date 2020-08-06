bf8f4e0b61b190009977c6662aa90a5abb00930b
Will Molloy <willjoemolloy@gmail.com>
2020-08-06 22:33:02 +1200

    leetcode P442FindAllDuplicatesInAnArray (medium)
    August 2020 Challenge.
    
    GitOrigin-RevId: 68dba9a22d46a979dac7b538d5cbad6fd7e84e5f

---
+++ leetcode/src/main/java/com/wilmol/leetcode/problemset/algorithms/medium/P442FindAllDuplicatesInAnArray.java
@@ -0,0 +1,41 @@
+package com.wilmol.leetcode.problemset.algorithms.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <a
+ * href=https://leetcode.com/problems/find-all-duplicates-in-an-array>https://leetcode.com/problems/find-all-duplicates-in-an-array/</a>
+ *
+ * <p>Runtime: O(n)
+ *
+ * <p>Extra memory: O(1)
+ *
+ * <p>Key: Mark {@code num} seen by making {@code nums[num]} negative. Works since {@code num} is in
+ * [1, n], so each has a bucket.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P442FindAllDuplicatesInAnArray {
+
+  public List<Integer> findDuplicates(int[] nums) {
+    List<Integer> result = new ArrayList<>();
+
+    for (int i = 0; i < nums.length; i++) {
+      // abs since other num may have made it negative
+      int num = Math.abs(nums[i]);
+      // num is in [1, n] so need 1-indexed
+      int index = num - 1;
+
+      if (nums[index] < 0) {
+        // if already negative, must be dupe
+        result.add(num);
+      } else {
+        // make negative to mark as seen
+        nums[index] = -nums[index];
+      }
+    }
+
+    return result;
+  }
+}
