4e2e88cbe9b947d2714e84b72bf8e38ae3f545bc
Will Molloy <willjoemolloy@gmail.com>
2020-08-14 00:16:56 +1200

    leetcode P1286IteratorForCombination (medium)
    August 2020 Challenge.
    
    GitOrigin-RevId: eec29d6fe9f782854046327c0056340bf8095bdd

---
+++ leetcode/src/test/java/com/wilmol/leetcode/problemset/algorithms/medium/P1286IteratorForCombinationTest.java
@@ -0,0 +1,36 @@
+package com.wilmol.leetcode.problemset.algorithms.medium;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.wilmol.leetcode.problemset.algorithms.medium.P1286IteratorForCombination.CombinationIterator;
+import org.junit.jupiter.api.Test;
+
+/**
+ * P1286IteratorForCombinationTest.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P1286IteratorForCombinationTest {
+
+  @Test
+  void example() {
+    CombinationIterator iterator = new CombinationIterator("abc", 2);
+
+    assertThat(iterator.hasNext()).isTrue();
+    assertThat(iterator.next()).isEqualTo("ab");
+    assertThat(iterator.hasNext()).isTrue();
+    assertThat(iterator.next()).isEqualTo("ac");
+    assertThat(iterator.hasNext()).isTrue();
+    assertThat(iterator.next()).isEqualTo("bc");
+    assertThat(iterator.hasNext()).isFalse();
+  }
+
+  @Test
+  void charactersLengthEqualsCombinationLength() {
+    CombinationIterator iterator = new CombinationIterator("abc", 3);
+
+    assertThat(iterator.hasNext()).isTrue();
+    assertThat(iterator.next()).isEqualTo("abc");
+    assertThat(iterator.hasNext()).isFalse();
+  }
+}
