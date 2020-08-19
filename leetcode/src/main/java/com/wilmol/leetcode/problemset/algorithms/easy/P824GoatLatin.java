4b75165de7e9cc61879d3b92edae1e44d9ff217f
Will Molloy <willjoemolloy@gmail.com>
2020-08-19 19:22:43 +1200

    leetcode P824GoatLatin (easy)
    August 2020 Challenge.
    
    Speed more time on complexity than problem lol.
    
    GitOrigin-RevId: da5e6cba32d8de36b87ec4ac2b1cc7fca40f18b8

---
+++ leetcode/src/main/java/com/wilmol/leetcode/problemset/algorithms/easy/P824GoatLatin.java
@@ -0,0 +1,42 @@
+package com.wilmol.leetcode.problemset.algorithms.easy;
+
+import java.util.Arrays;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+/**
+ * <a href=https://leetcode.com/problems/goat-latin>https://leetcode.com/problems/goat-latin/</a>
+ *
+ * <p>Runtime: average O(n), worst O(n<sup>2</sup>), let average word length = w, then there are k =
+ * n/w words, complexity is O(n + k<sup>2</sup>), worst case k = n (when w = 1), just see <a
+ * href=https://leetcode.com/problems/goat-latin/solution>https://leetcode.com/problems/goat-latin/solution/</a>
+ *
+ * <p>Extra memory: O(1) (including return value, average O(n), worst O(n<sup>2</sup>), same as time
+ * complexity)
+ *
+ * <p>Key: Transform words one by one.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P824GoatLatin {
+
+  public String toGoatLatin(String s) {
+    Pattern vowel = Pattern.compile("[aeiou].*", Pattern.CASE_INSENSITIVE);
+    String[] suffix = {"a"};
+
+    return Arrays.stream(s.split(" "))
+        .map(
+            word -> {
+              String newWord;
+              if (vowel.matcher(word).matches()) {
+                newWord = word;
+              } else {
+                newWord = word.substring(1) + word.substring(0, 1);
+              }
+              newWord += "ma" + suffix[0];
+              suffix[0] += "a";
+              return newWord;
+            })
+        .collect(Collectors.joining(" "));
+  }
+}
