f5bc43687aa52f7c843c93cbb7ff593283d80432
Will Molloy <willjoemolloy@gmail.com>
2020-08-20 19:58:19 +1200

    leetcode P143ReorderList (medium)
    August 2020 Challenge.
    
    GitOrigin-RevId: b57a173e702c266843df10e1d33ef8b40d9553c8

---
+++ leetcode/src/test/java/com/wilmol/leetcode/problemset/algorithms/medium/P143ReorderListTest.java
@@ -0,0 +1,45 @@
+package com.wilmol.leetcode.problemset.algorithms.medium;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.wilmol.leetcode.common.ListNode;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+/**
+ * P143ReorderListTest.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P143ReorderListTest {
+
+  private final P143ReorderList p143 = new P143ReorderList();
+
+  @ParameterizedTest
+  @MethodSource
+  void examples(ListNode head, ListNode expected) {
+    p143.reorderList(head);
+    assertThat(head).isEqualTo(expected);
+  }
+
+  static Stream<Arguments> examples() {
+    return Stream.of(
+        Arguments.of(ListNode.of(1, 2, 3, 4), ListNode.of(1, 4, 2, 3)),
+        Arguments.of(ListNode.of(1, 2, 3, 4, 5), ListNode.of(1, 5, 2, 4, 3)));
+  }
+
+  @Test
+  void emptyList() {
+    p143.reorderList(null);
+  }
+
+  @Test
+  void singletonList() {
+    ListNode head = ListNode.of(1);
+    p143.reorderList(head);
+    assertThat(head).isEqualTo(ListNode.of(1));
+  }
+}
