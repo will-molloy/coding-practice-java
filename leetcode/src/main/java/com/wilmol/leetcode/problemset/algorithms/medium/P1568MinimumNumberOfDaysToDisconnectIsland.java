a9e67a96f15346e58573853c090f8e0c7b545980
Will Molloy <willjoemolloy@gmail.com>
2020-08-30 23:10:36 +1200

    leetcode P1568MinimumNumberOfDaysToDisconnectIsland (medium)
    Weekly Contest 204.
    
    Didn't get the trick that the result is <= 2 so instead implemented
    exponential time backtracking algorithm (O(900^n)?) which got TLE.
    
    Should have stopped to think about question more. Try examples on paper
    first for example.
    
    GitOrigin-RevId: ad90d3583882e9d059c101ee4dad96eeafc83814

---
+++ leetcode/src/main/java/com/wilmol/leetcode/problemset/algorithms/medium/P1568MinimumNumberOfDaysToDisconnectIsland.java
@@ -0,0 +1,75 @@
+package com.wilmol.leetcode.problemset.algorithms.medium;
+
+/**
+ * <a
+ * href=https://leetcode.com/problems/minimum-number-of-days-to-disconnect-island>https://leetcode.com/problems/minimum-number-of-days-to-disconnect-island/</a>
+ *
+ * <p>Runtime: O((rows * cols)<sup>2</sup>)
+ *
+ * <p>Extra memory: O(1)
+ *
+ * <p>Key: Result is always <= 2, since can isolate any corner block with at most two removals.
+ * Therefore only need to test case where result is 0 (test initially) and 1 (try remove each 1).
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P1568MinimumNumberOfDaysToDisconnectIsland {
+
+  public int minDays(int[][] grid) {
+    if (numIslands(grid) != 1) {
+      return 0;
+    }
+
+    for (int row = 0; row < grid.length; row++) {
+      for (int col = 0; col < grid[0].length; col++) {
+        if (grid[row][col] == 1) {
+          grid[row][col] = 0;
+          if (numIslands(grid) != 1) {
+            return 1;
+          }
+          grid[row][col] = 1;
+        }
+      }
+    }
+
+    return 2;
+  }
+
+  // O(rows * cols)
+  private int numIslands(int[][] grid) {
+    int count = 0;
+
+    // count islands
+    for (int row = 0; row < grid.length; row++) {
+      for (int col = 0; col < grid[0].length; col++) {
+        if (grid[row][col] == 1) {
+          count++;
+          dfsNumIslands(grid, row, col);
+        }
+      }
+    }
+
+    // restore islands
+    for (int row = 0; row < grid.length; row++) {
+      for (int col = 0; col < grid[0].length; col++) {
+        if (grid[row][col] == 2) {
+          grid[row][col] = 1;
+        }
+      }
+    }
+
+    return count;
+  }
+
+  // clears an island so it isn't double counted
+  private void dfsNumIslands(int[][] grid, int row, int col) {
+    if (row >= 0 && row < grid.length && col >= 0 && col < grid[0].length && grid[row][col] == 1) {
+      // don't make 0, otherwise can't restore island
+      grid[row][col] = 2;
+      dfsNumIslands(grid, row + 1, col);
+      dfsNumIslands(grid, row - 1, col);
+      dfsNumIslands(grid, row, col + 1);
+      dfsNumIslands(grid, row, col - 1);
+    }
+  }
+}
