026378080e6024ab3cf12321a3d1a594f0705b7c
Will Molloy <willjoemolloy@gmail.com>
2020-09-03 00:47:08 +1200

    leetcode P1304FindNUniqueIntegersSumUpToZero (easy)
    GitOrigin-RevId: 0ccdcc9bdd288be5f36c02dbcd446be2ce1d6312

---
+++ leetcode/src/test/java/com/wilmol/leetcode/problemset/algorithms/easy/P1304FindNUniqueIntegersSumUpToZeroTest.java
@@ -0,0 +1,31 @@
+package com.wilmol.leetcode.problemset.algorithms.easy;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * P1304FindNUniqueIntegersSumUpToZeroTest.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P1304FindNUniqueIntegersSumUpToZeroTest {
+
+  private final P1304FindNUniqueIntegersSumUpToZero p1304 =
+      new P1304FindNUniqueIntegersSumUpToZero();
+
+  @Test
+  void even() {
+    assertThat(p1304.sumZero(2)).asList().containsExactly(-1, 1).inOrder();
+  }
+
+  @Test
+  void odd() {
+    assertThat(p1304.sumZero(3)).asList().containsExactly(-1, 0, 1).inOrder();
+  }
+
+  @Test
+  void one() {
+    assertThat(p1304.sumZero(1)).asList().containsExactly(0);
+  }
+}
