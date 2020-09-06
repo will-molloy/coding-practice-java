464a03f7329458ee3ac43d913c1edc25ffa30fa6
Will Molloy <willjoemolloy@gmail.com>
2020-09-06 17:03:35 +1200

    leetcode P1576ReplaceAllQuestionMarksToAvoidConsecutiveRepeatingCharacters (easy)
    Weekly Contest 205.
    
    GitOrigin-RevId: f02b1930b0537491863563ed86233e29a23caee9

---
+++ leetcode/src/main/java/com/wilmol/leetcode/problemset/algorithms/easy/P1576ReplaceAllQuestionMarksToAvoidConsecutiveRepeatingCharacters.java
@@ -0,0 +1,31 @@
+package com.wilmol.leetcode.problemset.algorithms.easy;
+
+/**
+ * <a
+ * href=https://leetcode.com/problems/replace-all-s-to-avoid-consecutive-repeating-characters>https://leetcode.com/problems/replace-all-s-to-avoid-consecutive-repeating-characters/</a>
+ *
+ * <p>Runtime: O(n)
+ *
+ * <p>Extra memory: O(n)
+ *
+ * <p>Key: Brute force; find first valid replacement [a, z].
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P1576ReplaceAllQuestionMarksToAvoidConsecutiveRepeatingCharacters {
+
+  public String modifyString(String s) {
+    char[] chars = s.toCharArray();
+    for (int i = 0; i < chars.length; i++) {
+      if (chars[i] == '?') {
+        for (char c = 'a'; c <= 'z'; c++) {
+          if ((i == 0 || chars[i - 1] != c) && (i == chars.length - 1 || chars[i + 1] != c)) {
+            chars[i] = c;
+            break;
+          }
+        }
+      }
+    }
+    return new String(chars);
+  }
+}
