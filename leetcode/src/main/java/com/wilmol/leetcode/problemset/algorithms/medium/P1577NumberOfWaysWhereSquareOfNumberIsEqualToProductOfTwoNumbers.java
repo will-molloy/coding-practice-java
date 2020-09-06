c5e48d6ee503c2df406765a75eb80e94aa0757fa
Will Molloy <willjoemolloy@gmail.com>
2020-09-06 17:16:45 +1200

    leetcode P1577NumberOfWaysWhereSquareOfNumberIsEqualToProductOfTwoNumbers (medium)
    Weekly Contest 205.
    
    GitOrigin-RevId: 5ae91873d2ab794cc11cc5523fa51457037ee19b

---
+++ leetcode/src/main/java/com/wilmol/leetcode/problemset/algorithms/medium/P1577NumberOfWaysWhereSquareOfNumberIsEqualToProductOfTwoNumbers.java
@@ -0,0 +1,44 @@
+package com.wilmol.leetcode.problemset.algorithms.medium;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <a
+ * href=https://leetcode.com/problems/number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers>https://leetcode.com/problems/number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers/</a>
+ *
+ * <p>Runtime: O(n1<sup>2</sup> + n2<sup>2</sup>)
+ *
+ * <p>Extra memory: O(n1 + n2)
+ *
+ * <p>Key: Precompute square counts.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P1577NumberOfWaysWhereSquareOfNumberIsEqualToProductOfTwoNumbers {
+
+  // TODO can do O(n1 + n2) time
+
+  public int numTriplets(int[] nums1, int[] nums2) {
+    int count1 = count(nums1, nums2);
+    int count2 = count(nums2, nums1);
+    return count1 + count2;
+  }
+
+  private int count(int[] nums1, int[] nums2) {
+    Map<Long, Integer> squareCounts = new HashMap<>();
+    for (int num : nums1) {
+      long square = (long) num * num;
+      squareCounts.merge(square, 1, Integer::sum);
+    }
+
+    int count = 0;
+    for (int j = 0; j < nums2.length; j++) {
+      for (int k = j + 1; k < nums2.length; k++) {
+        long prod = (long) nums2[j] * nums2[k];
+        count += squareCounts.getOrDefault(prod, 0);
+      }
+    }
+    return count;
+  }
+}
---
+++ leetcode/src/test/java/com/wilmol/leetcode/problemset/algorithms/medium/P1577NumberOfWaysWhereSquareOfNumberIsEqualToProductOfTwoNumbersTest.java
@@ -0,0 +1,33 @@
+package com.wilmol.leetcode.problemset.algorithms.medium;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+/**
+ * P1577NumberOfWaysWhereSquareOfNumberIsEqualToProductOfTwoNumbersTest.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P1577NumberOfWaysWhereSquareOfNumberIsEqualToProductOfTwoNumbersTest {
+
+  private final P1577NumberOfWaysWhereSquareOfNumberIsEqualToProductOfTwoNumbers p1577 =
+      new P1577NumberOfWaysWhereSquareOfNumberIsEqualToProductOfTwoNumbers();
+
+  @ParameterizedTest
+  @MethodSource
+  void examples(int[] nums1, int[] nums2, int expected) {
+    assertThat(p1577.numTriplets(nums1, nums2)).isEqualTo(expected);
+  }
+
+  static Stream<Arguments> examples() {
+    return Stream.of(
+        Arguments.of(new int[] {7, 4}, new int[] {5, 2, 8, 9}, 1),
+        Arguments.of(new int[] {1, 1}, new int[] {1, 1, 1}, 9),
+        Arguments.of(new int[] {7, 7, 8, 3}, new int[] {1, 2, 9, 7}, 2),
+        Arguments.of(new int[] {4, 7, 9, 11, 23}, new int[] {3, 5, 1024, 12, 18}, 0));
+  }
+}
