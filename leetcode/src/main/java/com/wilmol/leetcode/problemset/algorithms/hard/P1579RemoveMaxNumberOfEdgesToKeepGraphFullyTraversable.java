c738b45f5a94d01e332de87cfe97ddda09a38eb1
Will Molloy <willjoemolloy@gmail.com>
2020-09-08 17:50:25 +1200

    leetcode improve P1579RemoveMaxNumberOfEdgesToKeepGraphFullyTraversable
    Made O(n + e) by removing the sort, can simply add shared edges first.
    
    GitOrigin-RevId: 31d8ad7c5fd1b74d42ef4dd156c5babd85e615a9

--- leetcode/src/main/java/com/wilmol/leetcode/problemset/algorithms/hard/P1579RemoveMaxNumberOfEdgesToKeepGraphFullyTraversable.java
+++ leetcode/src/main/java/com/wilmol/leetcode/problemset/algorithms/hard/P1579RemoveMaxNumberOfEdgesToKeepGraphFullyTraversable.java
@@ -2,7 +2,6 @@ package com.wilmol.leetcode.problemset.algorithms.hard;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -12,7 +11,7 @@ import java.util.stream.IntStream;
  * <a
  * href=https://leetcode.com/problems/remove-max-number-of-edges-to-keep-graph-fully-traversable>https://leetcode.com/problems/remove-max-number-of-edges-to-keep-graph-fully-traversable/</a>
  *
- * <p>Runtime: O(n + e log e)
+ * <p>Runtime: O(n + e)
  *
  * <p>Extra memory: O(n + e)
  *
@@ -29,29 +28,31 @@ class P1579RemoveMaxNumberOfEdgesToKeepGraphFullyTraversable {
     // 3. difference of total num edges - combined MST = num edges can be removed
 
     // {cost, src, dst}
-    List<List<Integer>> aliceEdge = new ArrayList<>();
-    List<List<Integer>> bobEdge = new ArrayList<>();
+    List<List<Integer>> aliceEdges = new ArrayList<>();
+    List<List<Integer>> bobEdges = new ArrayList<>();
+    // add shared edges first so they're favoured in the MST
     for (int[] edge : edges) {
       if (edge[0] == 3) {
-        // favour combined edge in MST calculation by making its cost cheaper
-        aliceEdge.add(Arrays.asList(1, edge[1], edge[2]));
-        bobEdge.add(Arrays.asList(1, edge[1], edge[2]));
-      } else if (edge[0] == 1) {
-        aliceEdge.add(Arrays.asList(2, edge[1], edge[2]));
-      } else if (edge[0] == 2) {
+        aliceEdges.add(Arrays.asList(1, edge[1], edge[2]));
+        bobEdges.add(Arrays.asList(1, edge[1], edge[2]));
+      }
+    }
+    // add unshared edges
+    for (int[] edge : edges) {
+      if (edge[0] == 1) {
+        aliceEdges.add(Arrays.asList(2, edge[1], edge[2]));
+      } else {
         // use different cost to alice so combined mst considers them differently (i.e. in the
         // Set.addAll)
-        // wont have the unshared edges in same MST calculation so doesn't matter as long as >
-        // shared edge cost
-        bobEdge.add(Arrays.asList(3, edge[1], edge[2]));
+        bobEdges.add(Arrays.asList(3, edge[1], edge[2]));
       }
     }
 
-    Set<List<Integer>> aliceMst = mst(n, aliceEdge);
+    Set<List<Integer>> aliceMst = mst(n, aliceEdges);
     if (aliceMst.size() != n - 1) {
       return -1;
     }
-    Set<List<Integer>> bobMst = mst(n, bobEdge);
+    Set<List<Integer>> bobMst = mst(n, bobEdges);
     if (bobMst.size() != n - 1) {
       return -1;
     }
@@ -67,11 +68,10 @@ class P1579RemoveMaxNumberOfEdgesToKeepGraphFullyTraversable {
   private int[] parent;
 
   // kruskals algorithm
+  // assumed edges already sorted
   private Set<List<Integer>> mst(int n, List<List<Integer>> edges) {
     // make n sets
     parent = IntStream.rangeClosed(0, n).toArray();
-    // sort so try cheapest edges first
-    edges.sort(Comparator.comparing(list -> list.get(0)));
 
     Set<List<Integer>> mstEdges = new HashSet<>();
 
