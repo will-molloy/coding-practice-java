734432c9701794b59b9726c3428fd4ea2fb14106
Will Molloy <willjoemolloy@gmail.com>
2020-09-13 22:53:00 +1200

    leetcode P1582SpecialPositionsInABinaryMatrix (easy)
    Weekly Contest 206.
    
    GitOrigin-RevId: 656da67b6131cee7295c535ef684f95b160d8ef0

---
+++ leetcode/src/test/java/com/wilmol/leetcode/problemset/algorithms/easy/P1582SpecialPositionsInABinaryMatrixTest.java
@@ -0,0 +1,60 @@
+package com.wilmol.leetcode.problemset.algorithms.easy;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+/**
+ * P1582SpecialPositionsInABinaryMatrixTest.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P1582SpecialPositionsInABinaryMatrixTest {
+
+  private final P1582SpecialPositionsInABinaryMatrix p1582 =
+      new P1582SpecialPositionsInABinaryMatrix();
+
+  @ParameterizedTest
+  @MethodSource
+  void examples(int[][] mat, int expected) {
+    assertThat(p1582.numSpecial(mat)).isEqualTo(expected);
+  }
+
+  static Stream<Arguments> examples() {
+    return Stream.of(
+        Arguments.of(
+            new int[][] {
+              {1, 0, 0},
+              {0, 0, 1},
+              {1, 0, 0},
+            },
+            1),
+        Arguments.of(
+            new int[][] {
+              {1, 0, 0},
+              {0, 1, 0},
+              {0, 0, 1},
+            },
+            3),
+        Arguments.of(
+            new int[][] {
+              {0, 0, 0, 1},
+              {1, 0, 0, 0},
+              {0, 1, 1, 0},
+              {0, 0, 0, 0}
+            },
+            2),
+        Arguments.of(
+            new int[][] {
+              {0, 0, 0, 0, 0},
+              {1, 0, 0, 0, 0},
+              {0, 1, 0, 0, 0},
+              {0, 0, 1, 0, 0},
+              {0, 0, 0, 1, 1},
+            },
+            3));
+  }
+}
