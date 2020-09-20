5432731f5a8fe9a7f6a0eb809e7baea1af40ae9d
Will Molloy <willjoemolloy@gmail.com>
2020-09-20 17:06:54 +1200

    leetcode P1593SplitAStringIntoTheMaxNumberOfUniqueSubstrings (medium)
    Weekly Contest 207.
    
    GitOrigin-RevId: 624d232c6f890eeda96ba6f8a68204f33601c777

---
+++ leetcode/src/main/java/com/wilmol/leetcode/problemset/algorithms/medium/P1593SplitAStringIntoTheMaxNumberOfUniqueSubstrings.java
@@ -0,0 +1,41 @@
+package com.wilmol.leetcode.problemset.algorithms.medium;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * <a
+ * href=https://leetcode.com/problems/split-a-string-into-the-max-number-of-unique-substrings>https://leetcode.com/problems/split-a-string-into-the-max-number-of-unique-substrings/</a>
+ *
+ * <p>Runtime: O(2<sup>n</sup>)
+ *
+ * <p>Extra memory: O(n)
+ *
+ * <p>Key: Recursively try all the combinations (DFS backtracking).
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P1593SplitAStringIntoTheMaxNumberOfUniqueSubstrings {
+
+  private int max;
+
+  public int maxUniqueSplit(String s) {
+    max = 1;
+    dfs(new HashSet<>(), s);
+    return max;
+  }
+
+  private void dfs(Set<String> subs, String tail) {
+    if (tail.isEmpty()) {
+      max = Math.max(max, subs.size());
+    }
+    for (int i = 1; i <= tail.length(); i++) {
+      String head = tail.substring(0, i);
+      if (subs.add(head)) {
+        dfs(subs, tail.substring(i));
+        // backtrack
+        subs.remove(head);
+      }
+    }
+  }
+}
---
+++ leetcode/src/test/java/com/wilmol/leetcode/problemset/algorithms/medium/P1593SplitAStringIntoTheMaxNumberOfUniqueSubstringsTest.java
@@ -0,0 +1,29 @@
+package com.wilmol.leetcode.problemset.algorithms.medium;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+/**
+ * P1593SplitAStringIntoTheMaxNumberOfUniqueSubstringsTest.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P1593SplitAStringIntoTheMaxNumberOfUniqueSubstringsTest {
+
+  private final P1593SplitAStringIntoTheMaxNumberOfUniqueSubstrings p1593 =
+      new P1593SplitAStringIntoTheMaxNumberOfUniqueSubstrings();
+
+  @ParameterizedTest
+  @MethodSource
+  void examples(String s, int expected) {
+    assertThat(p1593.maxUniqueSplit(s)).isEqualTo(expected);
+  }
+
+  static Stream<Arguments> examples() {
+    return Stream.of(Arguments.of("ababccc", 5), Arguments.of("aba", 2), Arguments.of("aa", 1));
+  }
+}
