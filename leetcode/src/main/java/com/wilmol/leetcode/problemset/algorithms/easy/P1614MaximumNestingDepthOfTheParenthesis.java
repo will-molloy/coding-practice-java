af077657b04b145238339c11758545d78afee87b
Will Molloy <willjoemolloy@gmail.com>
2020-10-11 19:48:49 +1300

    leetcode P1614MaximumNestingDepthOfTheParenthesis (easy)
    Weekly Contest 210.
    
    GitOrigin-RevId: eb01a152fe7fc6cb59d3a7991717fcb37d2c7b27

---
+++ leetcode/src/main/java/com/wilmol/leetcode/problemset/algorithms/easy/P1614MaximumNestingDepthOfTheParenthesis.java
@@ -0,0 +1,30 @@
+package com.wilmol.leetcode.problemset.algorithms.easy;
+
+/**
+ * <a href=https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses>https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses/</a>
+ *
+ * <p>Runtime: O(n)
+ *
+ * <p>Extra memory: O(1)
+ *
+ * <p>Key: Track depth as you go.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P1614MaximumNestingDepthOfTheParenthesis {
+
+  public int maxDepth(String s) {
+    int depth = 0;
+    int max = 0;
+    for (char c : s.toCharArray()) {
+      if (c == '(') {
+        depth++;
+      }
+      if (c == ')') {
+        depth--;
+      }
+      max = Math.max(max, depth);
+    }
+    return max;
+  }
+}
