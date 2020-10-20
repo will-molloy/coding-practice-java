f2060547e67eadea44f33cfd02701f03b3d3236b
Will Molloy <willjoemolloy@gmail.com>
2020-10-20 17:44:48 +1300

    leetcode P1624LargestSubstringBetweenTwoEqualCharacters (easy)
    Weekly Contest 211.
    
    GitOrigin-RevId: 7d5edbe95f9515331c1ac4d742ffa2ce19f8092e

---
+++ leetcode/src/test/java/com/wilmol/leetcode/problemset/algorithms/easy/P1624LargestSubstringBetweenTwoEqualCharactersTest.java
@@ -0,0 +1,33 @@
+package com.wilmol.leetcode.problemset.algorithms.easy;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+/**
+ * P1624LargestSubstringBetweenTwoEqualCharactersTest.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P1624LargestSubstringBetweenTwoEqualCharactersTest {
+
+  private final P1624LargestSubstringBetweenTwoEqualCharacters p1624 =
+      new P1624LargestSubstringBetweenTwoEqualCharacters();
+
+  @ParameterizedTest
+  @MethodSource
+  void examples(String s, int expected) {
+    assertThat(p1624.maxLengthBetweenEqualCharacters(s)).isEqualTo(expected);
+  }
+
+  static Stream<Arguments> examples() {
+    return Stream.of(
+        Arguments.of("aa", 0),
+        Arguments.of("abca", 2),
+        Arguments.of("cbzxy", -1),
+        Arguments.of("cabbac", 4));
+  }
+}
