7f60dd7fc74b8b003d4afa126e22f779813c7ec1
Will Molloy <willjoemolloy@gmail.com>
2020-10-21 22:51:04 +1300

    leetcode P735AsteroidCollision (medium)
    October 2020 Challenge.
    
    GitOrigin-RevId: 1c951b8eba3562f71fbb0021e6bf7d4b7ba0c635

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ projecteuler, leetcode, hackerrank etc. using Java
 - [ ] [Project Euler](https://projecteuler.net/profile/wilmol.png)
   - [ ] [Problems Archives](project-euler/src/main/java/com/wilmol/projecteuler/problems) (65/714)
 - [ ] [LeetCode](https://leetcode.com/wilmol/)
-  - [ ] [Algorithms](leetcode/src/main/java/com/wilmol/leetcode/problemset/algorithms) (511/1482)
+  - [ ] [Algorithms](leetcode/src/main/java/com/wilmol/leetcode/problemset/algorithms) (512/1482)
   - [ ] [Concurrency](leetcode/src/main/java/com/wilmol/leetcode/problemset/concurrency) (5/9)
 - [ ] [HackerRank](https://www.hackerrank.com/WilMol)
   - [ ] [Interview Preparation Kit](hackerrank/src/main/java/com/wilmol/hackerrank/interviewpreparationkit) (40/69)
---
+++ leetcode/src/main/java/com/wilmol/leetcode/problemset/algorithms/medium/P735AsteroidCollision.java
@@ -0,0 +1,55 @@
+package com.wilmol.leetcode.problemset.algorithms.medium;
+
+import java.util.ArrayDeque;
+
+/**
+ * <a
+ * href=https://leetcode.com/problems/asteroid-collision>https://leetcode.com/problems/asteroid-collision/</a>
+ *
+ * <p>Runtime: O(n) (push/pop each asteroid at most once)
+ *
+ * <p>Extra memory: O(n)
+ *
+ * <p>Key: Consider what happens when adding a new asteroid to the right of a sequence of stable
+ * asteroids, a stack is natural.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P735AsteroidCollision {
+
+  public int[] asteroidCollision(int[] a) {
+    ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+    for (int next : a) {
+      if (next > 0) {
+        // next is moving right, it can't be destroyed nor destroy anything (in this iteration)
+        stack.push(next);
+        continue;
+      }
+
+      // next is moving left
+
+      while (!stack.isEmpty() && stack.peek() > 0 && stack.peek() < -next) {
+        // existing asteroids moving right are destroyed by next
+        stack.pop();
+      }
+
+      if (!stack.isEmpty() && stack.peek() > 0) {
+        if (stack.peek() == -next) {
+          // both next and existing asteroid moving right are destroyed
+          stack.pop();
+        }
+        // existing asteroid moving right destroys next
+      } else {
+        // no existing asteroid or existing asteroid moving left
+        stack.push(next);
+      }
+    }
+
+    int[] result = new int[stack.size()];
+    for (int i = stack.size() - 1; i >= 0; i--) {
+      result[i] = stack.pop();
+    }
+    return result;
+  }
+}
