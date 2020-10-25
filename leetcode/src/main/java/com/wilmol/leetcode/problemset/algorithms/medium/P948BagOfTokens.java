a4123fe332a989de4b2568a3336fc43364d102b0
Will Molloy <willjoemolloy@gmail.com>
2020-10-25 18:48:07 +1300

    leetcode P948BagOfTokens (medium)
    October 2020 Challenge.
    
    GitOrigin-RevId: 899e0c108af52e0ba5d35b7ce148131bada8b5d9

---
+++ leetcode/src/main/java/com/wilmol/leetcode/problemset/algorithms/medium/P948BagOfTokens.java
@@ -0,0 +1,54 @@
+package com.wilmol.leetcode.problemset.algorithms.medium;
+
+import java.util.Arrays;
+
+/**
+ * <a
+ * href=https://leetcode.com/problems/bag-of-tokens>https://leetcode.com/problems/bag-of-tokens/</a>
+ *
+ * <p>Runtime: O(n * log(n))
+ *
+ * <p>Extra memory: O(1)
+ *
+ * <p>Key: Spend power on smallest tokens, gain power on largest tokens. Prioritise spending power
+ * (as this gains score). (Greedy.)
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P948BagOfTokens {
+
+  public int bagOfTokensScore(int[] tokens, int p) {
+    int n = tokens.length;
+
+    Arrays.sort(tokens);
+
+    // tracks smallest tokens
+    int i = 0;
+    // tracks largest tokens
+    int j = n - 1;
+
+    int score = 0;
+    int maxScore = 0;
+
+    // loop until pointers meet (as we can only play each token at most once)
+    while (i <= j) {
+      // spend power if possible
+      if (p >= tokens[i]) {
+        p -= tokens[i];
+        score++;
+        i++;
+      } else {
+        if (score == 0) {
+          // no power to spend, and no way to gain power, exit
+          break;
+        }
+        p += tokens[j];
+        score--;
+        j--;
+      }
+      // need to track max score somehow, as we may end on gaining power (which lost score)
+      maxScore = Math.max(maxScore, score);
+    }
+    return maxScore;
+  }
+}
