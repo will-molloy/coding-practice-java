518941d350e627678dba47031b327ab711194ebd
Will Molloy <willjoemolloy@gmail.com>
2020-10-27 23:13:34 +1300

    leetcode P1629SlowestKey (easy)
    Weekly Contest 212.
    
    GitOrigin-RevId: b8cdfad0242f75c77b2bf8fae5e50159817dbf3c

---
+++ leetcode/src/main/java/com/wilmol/leetcode/problemset/algorithms/easy/P1629SlowestKey.java
@@ -0,0 +1,33 @@
+package com.wilmol.leetcode.problemset.algorithms.easy;
+
+/**
+ * <a href=https://leetcode.com/problems/slowest-key>https://leetcode.com/problems/slowest-key/</a>
+ *
+ * <p>Runtime: O(n)
+ *
+ * <p>Extra memory: O(1)
+ *
+ * <p>Key: Track max distance and char associated with that max distance.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P1629SlowestKey {
+
+  public char slowestKey(int[] releaseTimes, String keysPressed) {
+    int n = releaseTimes.length;
+
+    int maxDuration = releaseTimes[0];
+    char maxKey = keysPressed.charAt(0);
+
+    for (int i = 1; i < n; i++) {
+      int duration = releaseTimes[i] - releaseTimes[i - 1];
+      if (duration >= maxDuration) {
+        if (duration > maxDuration || keysPressed.charAt(i) > maxKey) {
+          maxKey = keysPressed.charAt(i);
+        }
+        maxDuration = duration;
+      }
+    }
+    return maxKey;
+  }
+}
---
+++ leetcode/src/test/java/com/wilmol/leetcode/problemset/algorithms/easy/P1629SlowestKeyTest.java
@@ -0,0 +1,30 @@
+package com.wilmol.leetcode.problemset.algorithms.easy;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+/**
+ * P1629SlowestKeyTest.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P1629SlowestKeyTest {
+
+  private final P1629SlowestKey p1629 = new P1629SlowestKey();
+
+  @ParameterizedTest
+  @MethodSource
+  void examples(int[] releaseTimes, String keysPressed, char expected) {
+    assertThat(p1629.slowestKey(releaseTimes, keysPressed)).isEqualTo(expected);
+  }
+
+  static Stream<Arguments> examples() {
+    return Stream.of(
+        Arguments.of(new int[] {9, 29, 49, 50}, "cbcd", 'c'),
+        Arguments.of(new int[] {12, 23, 36, 46, 62}, "spuda", 'a'));
+  }
+}
