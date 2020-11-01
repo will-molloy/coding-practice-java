8065828b4b7e357af9b3b7436c43a32d4e585d85
Will Molloy <willjoemolloy@gmail.com>
2020-11-01 19:22:47 +1300

    leetcode P99RecoverBinarySearchTree (hard)
    Medium solution used. Followup makes it hard.
    
    October 2020 Challenge finale.
    
    GitOrigin-RevId: b8f70191c6597f3820b7164b9fb8c35b08fed4f0

---
+++ leetcode/src/test/java/com/wilmol/leetcode/problemset/algorithms/hard/P99RecoverBinarySearchTreeTest.java
@@ -0,0 +1,32 @@
+package com.wilmol.leetcode.problemset.algorithms.hard;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.wilmol.leetcode.common.BinaryTreeNode;
+import org.junit.jupiter.api.Test;
+
+/**
+ * P99RecoverBinarySearchTreeTest.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P99RecoverBinarySearchTreeTest {
+
+  private final P99RecoverBinarySearchTree p99 = new P99RecoverBinarySearchTree();
+
+  @Test
+  void example1() {
+    // inorder: [3, 2, 1] -> (swap 3 and 1) -> [1, 2, 3]
+    BinaryTreeNode root = BinaryTreeNode.fromLevelOrder(1, 3, null, null, 2);
+    p99.recoverTree(root);
+    assertThat(root).isEqualTo(BinaryTreeNode.fromLevelOrder(3, 1, null, null, 2));
+  }
+
+  @Test
+  void example2() {
+    // inorder: [1, 3, 2, 4] -> (swap 3 and 2) -> [1, 2, 3, 4]
+    BinaryTreeNode root = BinaryTreeNode.fromLevelOrder(3, 1, 4, null, null, 2);
+    p99.recoverTree(root);
+    assertThat(root).isEqualTo(BinaryTreeNode.fromLevelOrder(2, 1, 4, null, null, 3));
+  }
+}
