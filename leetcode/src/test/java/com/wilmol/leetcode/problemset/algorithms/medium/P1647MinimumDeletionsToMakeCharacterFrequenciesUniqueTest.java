9ca5623b2cb7784653bce039ebc95eedb157358d
Will Molloy <willjoemolloy@gmail.com>
2020-11-08 20:06:45 +1300

    leetcode P1647MinimumDeletionsToMakeCharacterFrequenciesUnique (medium)
    Weekly Contest 214.
    
    GitOrigin-RevId: 08250eaae5a481634b68a4fb2b4353dead975030

---
+++ leetcode/src/test/java/com/wilmol/leetcode/problemset/algorithms/medium/P1647MinimumDeletionsToMakeCharacterFrequenciesUniqueTest.java
@@ -0,0 +1,42 @@
+package com.wilmol.leetcode.problemset.algorithms.medium;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+/**
+ * P1647MinimumDeletionsToMakeCharacterFrequenciesUniqueTest.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P1647MinimumDeletionsToMakeCharacterFrequenciesUniqueTest {
+
+  private final P1647MinimumDeletionsToMakeCharacterFrequenciesUnique p1647 =
+      new P1647MinimumDeletionsToMakeCharacterFrequenciesUnique();
+
+  @ParameterizedTest
+  @MethodSource
+  void examples(String s, int expected) {
+    assertThat(p1647.minDeletions(s)).isEqualTo(expected);
+  }
+
+  static Stream<Arguments> examples() {
+    return Stream.of(
+        Arguments.of("aab", 0), Arguments.of("aaabbbcc", 2), Arguments.of("ceabaacb", 2));
+  }
+
+  @Test
+  void tle1() {
+    assertThat(p1647.minDeletions("abcdefghijklmnopqrstuvwxwz")).isEqualTo(23);
+  }
+
+  @Test
+  void wa1() {
+    assertThat(p1647.minDeletions("odofingjfdncnecmfdekekchbbicbflkoanohamkcjljbfmahhbnkiangdal"))
+        .isEqualTo(39);
+  }
+}
