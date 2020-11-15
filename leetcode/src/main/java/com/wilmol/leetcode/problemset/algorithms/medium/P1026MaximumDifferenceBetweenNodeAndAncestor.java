9c3447ecb33b9f8e787f9fccd1d42d6fb05c7ecc
Will Molloy <willjoemolloy@gmail.com>
2020-11-15 14:49:51 +1300

    leetcode P1026MaximumDifferenceBetweenNodeAndAncestor (medium)
    GitOrigin-RevId: a3897fc4669d212875e6e85a90a5ca9cd9c3bbae

---
+++ leetcode/src/main/java/com/wilmol/leetcode/problemset/algorithms/medium/P1026MaximumDifferenceBetweenNodeAndAncestor.java
@@ -0,0 +1,37 @@
+package com.wilmol.leetcode.problemset.algorithms.medium;
+
+import com.wilmol.leetcode.common.BinaryTreeNode;
+
+/**
+ * <a
+ * href=https://leetcode.com/problems/maximum-difference-between-node-and-ancestor>https://leetcode.com/problems/maximum-difference-between-node-and-ancestor/</a>
+ *
+ * <p>Runtime: O(n)
+ *
+ * <p>Extra memory: O(h)
+ *
+ * <p>Key: Track min/max for each root to leaf path.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P1026MaximumDifferenceBetweenNodeAndAncestor {
+
+  private int best;
+
+  public int maxAncestorDiff(BinaryTreeNode root) {
+    best = Integer.MIN_VALUE;
+    dfs(Integer.MAX_VALUE, Integer.MIN_VALUE, root);
+    return best;
+  }
+
+  private void dfs(int minParent, int maxParent, BinaryTreeNode node) {
+    if (node == null) {
+      best = Math.max(best, maxParent - minParent);
+      return;
+    }
+    minParent = Math.min(minParent, node.val);
+    maxParent = Math.max(maxParent, node.val);
+    dfs(minParent, maxParent, node.left);
+    dfs(minParent, maxParent, node.right);
+  }
+}
