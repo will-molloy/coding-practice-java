27843f72b9f27987b7ab21b5149e8cfe7801a6b0
Will Molloy <willjoemolloy@gmail.com>
2020-11-15 20:22:51 +1300

    leetcode P1657DetermineIfTwoStringsAreClose (medium)
    Weekly Contest 215.
    
    GitOrigin-RevId: 993dbbc23ee386f9a6fcacd4dff147ff1aea81e6

---
+++ leetcode/src/main/java/com/wilmol/leetcode/problemset/algorithms/medium/P1657DetermineIfTwoStringsAreClose.java
@@ -0,0 +1,44 @@
+package com.wilmol.leetcode.problemset.algorithms.medium;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * <a
+ * href=https://leetcode.com/problems/determine-if-two-strings-are-close>https://leetcode.com/problems/determine-if-two-strings-are-close/</a>
+ *
+ * <p>Runtime: O(n)
+ *
+ * <p>Extra memory: O(1) - fixed number of keys (at most 26)
+ *
+ * <p>Key: Test key/value sets are equal (separately):
+ *
+ * <ul>
+ *   <li>Can use operation 1 to swap characters - position doesn't matter - can compare sets rather
+ *       than seqs
+ *   <li>Can use operation 2 to transform characters - aka swap character frequency values - as long
+ *       as the key/value sets are equal the values can be assigned to the keys such that the
+ *       mappings (key -> value) are equal
+ * </ul>
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P1657DetermineIfTwoStringsAreClose {
+
+  public boolean closeStrings(String word1, String word2) {
+    Map<Character, Integer> charFreqs1 = charFreqs(word1);
+    Map<Character, Integer> charFreqs2 = charFreqs(word2);
+
+    return charFreqs1.keySet().equals(charFreqs2.keySet())
+        && Set.copyOf(charFreqs1.values()).equals(Set.copyOf(charFreqs2.values()));
+  }
+
+  private Map<Character, Integer> charFreqs(String s) {
+    Map<Character, Integer> map = new HashMap<>();
+    for (char c : s.toCharArray()) {
+      map.merge(c, 1, Integer::sum);
+    }
+    return map;
+  }
+}
---
+++ leetcode/src/test/java/com/wilmol/leetcode/problemset/algorithms/medium/P1657DetermineIfTwoStringsAreCloseTest.java
@@ -0,0 +1,38 @@
+package com.wilmol.leetcode.problemset.algorithms.medium;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+/**
+ * P1657DetermineIfTwoStringsAreCloseTest.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P1657DetermineIfTwoStringsAreCloseTest {
+
+  private final P1657DetermineIfTwoStringsAreClose p1657 = new P1657DetermineIfTwoStringsAreClose();
+
+  @ParameterizedTest
+  @MethodSource
+  void examples(String word1, String word2, boolean expected) {
+    assertThat(p1657.closeStrings(word1, word2)).isEqualTo(expected);
+  }
+
+  static Stream<Arguments> examples() {
+    return Stream.of(
+        Arguments.of("abc", "bca", true),
+        Arguments.of("a", "aa", false),
+        Arguments.of("cabbba", "abbccc", true),
+        Arguments.of("cabbba", "aabbss", false));
+  }
+
+  @Test
+  void wa1() {
+    assertThat(p1657.closeStrings("uau", "ssx")).isFalse();
+  }
+}
