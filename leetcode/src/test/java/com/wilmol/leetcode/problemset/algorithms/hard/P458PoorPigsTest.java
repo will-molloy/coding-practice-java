155c46e330d42eda963a485c1869227045cbbf5f
Will Molloy <willjoemolloy@gmail.com>
2020-11-15 15:30:23 +1300

    leetcode P458PoorPigs (hard)
    More of a math problem.
    
    GitOrigin-RevId: 1ba5387debf8100e504f70893d6fc37a73585a33

---
+++ leetcode/src/test/java/com/wilmol/leetcode/problemset/algorithms/hard/P458PoorPigsTest.java
@@ -0,0 +1,25 @@
+package com.wilmol.leetcode.problemset.algorithms.hard;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * P458PoorPigsTest.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P458PoorPigsTest {
+
+  private final P458PoorPigs p458 = new P458PoorPigs();
+
+  @Test
+  void example() {
+    assertThat(p458.poorPigs(1000, 15, 60)).isEqualTo(5);
+  }
+
+  @Test
+  void exampleBut25Buckets() {
+    assertThat(p458.poorPigs(25, 15, 60)).isEqualTo(2);
+  }
+}
