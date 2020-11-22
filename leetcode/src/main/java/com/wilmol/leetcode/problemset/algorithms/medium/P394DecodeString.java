5d04c2a7277090e234f1e9d14477fcd4b758a4c7
Will Molloy <willjoemolloy@gmail.com>
2020-11-22 23:05:55 +1300

    leetcode P394DecodeString (medium)
    GitOrigin-RevId: fc53886c019c15c416656b0465d81c1cacf6f6f4

---
+++ leetcode/src/main/java/com/wilmol/leetcode/problemset/algorithms/medium/P394DecodeString.java
@@ -0,0 +1,44 @@
+package com.wilmol.leetcode.problemset.algorithms.medium;
+
+import java.util.ArrayDeque;
+
+/**
+ * <a
+ * href=https://leetcode.com/problems/decode-string>https://leetcode.com/problems/decode-string/</a>
+ *
+ * <p>Runtime: O(n)
+ *
+ * <p>Extra memory: O(n)
+ *
+ * <p>Key: 2 stacks - 1 for most recent repeat number, 1 to store result before processing bracket
+ * contents.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P394DecodeString {
+
+  public String decodeString(String s) {
+    String res = "";
+    int num = 0;
+
+    ArrayDeque<Integer> nums = new ArrayDeque<>();
+    ArrayDeque<String> strings = new ArrayDeque<>();
+
+    for (char c : s.toCharArray()) {
+      if (Character.isDigit(c)) {
+        num = num * 10 + c - '0';
+      } else if (c == '[') {
+        nums.push(num);
+        num = 0;
+        strings.push(res);
+        res = "";
+      } else if (c == ']') {
+        // strings holds contents before the open bracket, res holds contents after, so repeat res
+        res = strings.pop() + res.repeat(nums.pop());
+      } else {
+        res += c;
+      }
+    }
+    return res;
+  }
+}
