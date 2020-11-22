9fb977779ecb9a31f824985df5999816d266402f
Will Molloy <willjoemolloy@gmail.com>
2020-11-22 20:04:12 +1300

    leetcode P1664WaysToMakeAFairArray (medium)
    Weekly Contest 216.
    
    GitOrigin-RevId: 55351213ba909bdd96c73c91a6a9d3f9288de2e2

---
+++ leetcode/src/test/java/com/wilmol/leetcode/problemset/algorithms/medium/P1664WaysToMakeAFairArrayTest.java
@@ -0,0 +1,30 @@
+package com.wilmol.leetcode.problemset.algorithms.medium;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * P1664WaysToMakeAFairArrayTest.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P1664WaysToMakeAFairArrayTest {
+
+  private final P1664WaysToMakeAFairArray p1664 = new P1664WaysToMakeAFairArray();
+
+  @Test
+  void example1() {
+    assertThat(p1664.waysToMakeFair(new int[] {2, 1, 6, 4})).isEqualTo(1);
+  }
+
+  @Test
+  void example2() {
+    assertThat(p1664.waysToMakeFair(new int[] {1, 1, 1})).isEqualTo(3);
+  }
+
+  @Test
+  void example3() {
+    assertThat(p1664.waysToMakeFair(new int[] {1, 2, 3})).isEqualTo(0);
+  }
+}
