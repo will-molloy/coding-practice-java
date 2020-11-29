df6043139d3744fb634617d8f11eb0b29bde3a62
Will Molloy <willjoemolloy@gmail.com>
2020-11-29 23:45:55 +1300

    leetcode P395LongestSubstringWithAtLeastKRepeatingCharacters (medium)
    Not the best solution, but solutions tab is also O(n^2).
    
    Try discuss tab?
    
    GitOrigin-RevId: da471420264c5bb3c974cd8c61dd3223c1e9c193

---
+++ leetcode/src/test/java/com/wilmol/leetcode/problemset/algorithms/medium/P395LongestSubstringWithAtLeastKRepeatingCharactersTest.java
@@ -0,0 +1,29 @@
+package com.wilmol.leetcode.problemset.algorithms.medium;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+/**
+ * P395LongestSubstringWithAtLeastKRepeatingCharactersTest.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P395LongestSubstringWithAtLeastKRepeatingCharactersTest {
+
+  private final P395LongestSubstringWithAtLeastKRepeatingCharacters p395 =
+      new P395LongestSubstringWithAtLeastKRepeatingCharacters();
+
+  @ParameterizedTest
+  @MethodSource
+  void examples(String s, int k, int expected) {
+    assertThat(p395.longestSubstring(s, k)).isEqualTo(expected);
+  }
+
+  static Stream<Arguments> examples() {
+    return Stream.of(Arguments.of("aaabb", 3, 3), Arguments.of("ababbc", 2, 5));
+  }
+}
