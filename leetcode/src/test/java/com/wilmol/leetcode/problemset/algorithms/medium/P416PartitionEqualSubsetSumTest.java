59b893898def1c2eac34f20bbbcffbd810e689b6
Will Molloy <willjoemolloy@gmail.com>
2020-11-29 23:59:59 +1300

    leetcode P416PartitionEqualSubsetSum (medium)
    GitOrigin-RevId: 515b43d67c4e7efeb0a8f42f0823c4b1b2557f21

---
+++ leetcode/src/test/java/com/wilmol/leetcode/problemset/algorithms/medium/P416PartitionEqualSubsetSumTest.java
@@ -0,0 +1,29 @@
+package com.wilmol.leetcode.problemset.algorithms.medium;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+/**
+ * P416PartitionEqualSubsetSumTest.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P416PartitionEqualSubsetSumTest {
+
+  private final P416PartitionEqualSubsetSum p416 = new P416PartitionEqualSubsetSum();
+
+  @ParameterizedTest
+  @MethodSource
+  void examples(int[] nums, boolean expected) {
+    assertThat(p416.canPartition(nums)).isEqualTo(expected);
+  }
+
+  static Stream<Arguments> examples() {
+    return Stream.of(
+        Arguments.of(new int[] {1, 5, 11, 5}, true), Arguments.of(new int[] {1, 2, 3, 5}, false));
+  }
+}
