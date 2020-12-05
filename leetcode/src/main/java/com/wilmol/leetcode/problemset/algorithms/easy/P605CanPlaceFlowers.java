9555795961d2476ed6c7c87c41576c5aa1594dd5
Will Molloy <willjoemolloy@gmail.com>
2020-12-06 12:06:59 +1300

    leetcode P605CanPlaceFlowers (easy)
    GitOrigin-RevId: 7e033e11e3bb6221d48aeac0356862c529b65fa2

---
+++ leetcode/src/main/java/com/wilmol/leetcode/problemset/algorithms/easy/P605CanPlaceFlowers.java
@@ -0,0 +1,36 @@
+package com.wilmol.leetcode.problemset.algorithms.easy;
+
+/**
+ * <a
+ * href=https://leetcode.com/problems/can-place-flowers>https://leetcode.com/problems/can-place-flowers/</a>
+ *
+ * <p>Runtime: O(n)
+ *
+ * <p>Extra memory: O(1)
+ *
+ * <p>Key: Greedy. Choice each iteration is to place flowers or not. You should always place flowers
+ * if you can because not planting only frees up the next iteration and compared to planting in the
+ * next iteration, planting now blocks less of the array.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P605CanPlaceFlowers {
+
+  public boolean canPlaceFlowers(int[] flowerbed, int n) {
+    int len = flowerbed.length;
+
+    for (int i = 0; i < len; i++) {
+      if (flowerbed[i] == 1) {
+        continue;
+      }
+      boolean leftEmpty = i == 0 || flowerbed[i - 1] == 0;
+      boolean rightEmpty = i == len - 1 || flowerbed[i + 1] == 0;
+      if (leftEmpty && rightEmpty) {
+        // greedy, always plant if possible
+        flowerbed[i] = 1;
+        n--;
+      }
+    }
+    return n <= 0;
+  }
+}
