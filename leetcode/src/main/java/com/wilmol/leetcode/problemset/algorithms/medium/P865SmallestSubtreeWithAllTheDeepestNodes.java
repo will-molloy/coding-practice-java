d4f1a678fe10ca19ea31cf3863a4d0b4ebfaaffe
Will Molloy <willjoemolloy@gmail.com>
2020-12-13 12:56:31 +1300

    leetcode P865SmallestSubtreeWithAllTheDeepestNodes (medium)
    GitOrigin-RevId: 4a86e2a7a7d967b485d3c30b7f9f0d8b418e911c

---
+++ leetcode/src/main/java/com/wilmol/leetcode/problemset/algorithms/medium/P865SmallestSubtreeWithAllTheDeepestNodes.java
@@ -0,0 +1,51 @@
+package com.wilmol.leetcode.problemset.algorithms.medium;
+
+import com.wilmol.leetcode.common.BinaryTreeNode;
+
+/**
+ * <a
+ * href=https://leetcode.com/problems/smallest-subtree-with-all-the-deepest-nodes>https://leetcode.com/problems/smallest-subtree-with-all-the-deepest-nodes/</a>
+ *
+ * <p>Runtime: O(n)
+ *
+ * <p>Extra memory: O(h)
+ *
+ * <p>Key: Smallest subtree is the same as the lowest common ancestor. If heights are same return
+ * current node, else return node with larger height.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P865SmallestSubtreeWithAllTheDeepestNodes {
+
+  public BinaryTreeNode subtreeWithAllDeepest(BinaryTreeNode root) {
+    return dfs(root).node;
+  }
+
+  private TreeNodeLevel dfs(BinaryTreeNode node) {
+    if (node == null) {
+      return new TreeNodeLevel(null, 0);
+    }
+    TreeNodeLevel left = dfs(node.left);
+    TreeNodeLevel right = dfs(node.right);
+
+    // return subtree with largest height
+    // +1 to that height
+    if (left.level > right.level) {
+      return new TreeNodeLevel(left.node, left.level + 1);
+    } else if (right.level > left.level) {
+      return new TreeNodeLevel(right.node, right.level + 1);
+    } else {
+      return new TreeNodeLevel(node, left.level + 1);
+    }
+  }
+
+  private static class TreeNodeLevel {
+    BinaryTreeNode node;
+    int level;
+
+    TreeNodeLevel(BinaryTreeNode node, int level) {
+      this.node = node;
+      this.level = level;
+    }
+  }
+}
---
+++ leetcode/src/test/java/com/wilmol/leetcode/problemset/algorithms/medium/P865SmallestSubtreeWithAllTheDeepestNodesTest.java
@@ -0,0 +1,35 @@
+package com.wilmol.leetcode.problemset.algorithms.medium;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.wilmol.leetcode.common.BinaryTreeNode;
+import org.junit.jupiter.api.Test;
+
+/**
+ * P865SmallestSubtreeWithAllTheDeepestNodesTest.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P865SmallestSubtreeWithAllTheDeepestNodesTest {
+
+  private final P865SmallestSubtreeWithAllTheDeepestNodes p865 =
+      new P865SmallestSubtreeWithAllTheDeepestNodes();
+
+  @Test
+  void example1() {
+    BinaryTreeNode root = BinaryTreeNode.fromLevelOrder(3, 5, 1, 6, 2, 0, 8, null, null, 7, 4);
+    assertThat(p865.subtreeWithAllDeepest(root)).isSameInstanceAs(root.left.right);
+  }
+
+  @Test
+  void example2() {
+    BinaryTreeNode root = new BinaryTreeNode(1);
+    assertThat(p865.subtreeWithAllDeepest(root)).isSameInstanceAs(root);
+  }
+
+  @Test
+  void example3() {
+    BinaryTreeNode root = BinaryTreeNode.fromLevelOrder(0, 1, 3, null, 2);
+    assertThat(p865.subtreeWithAllDeepest(root)).isSameInstanceAs(root.left.right);
+  }
+}
