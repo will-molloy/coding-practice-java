2981fe147c6492f2fddb7f91b12ce68648fd928f
Will Molloy <willjoemolloy@gmail.com>
2021-01-06 15:22:30 +1300

    leetcode P1694ReformatPhoneNumber (easy)
    Weekly Contest 220.
    
    GitOrigin-RevId: e2bfa73abdb12752cc53c5ce24519169847d5288

---
+++ leetcode/src/main/java/com/wilmol/leetcode/problemset/algorithms/easy/P1694ReformatPhoneNumber.java
@@ -0,0 +1,33 @@
+package com.wilmol.leetcode.problemset.algorithms.easy;
+
+/**
+ * <a
+ * href=https://leetcode.com/problems/reformat-phone-number>https://leetcode.com/problems/reformat-phone-number/</a>
+ *
+ * <p>Runtime: O(n)
+ *
+ * <p>Extra memory: O(n)
+ *
+ * <p>Key: First get the digits, then build result by adding 3 digits until 4 are left and handling
+ * the 3 cases.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P1694ReformatPhoneNumber {
+
+  public String reformatNumber(String number) {
+    String digits = number.replaceAll("\\D", "");
+
+    String result = "";
+    int i;
+    for (i = 0; i < digits.length() - 4; i += 3) {
+      result += digits.substring(i, i + 3) + "-";
+    }
+    if (i == digits.length() - 4) {
+      result += digits.substring(i, i + 2) + "-";
+      i += 2;
+    }
+    result += digits.substring(i);
+    return result;
+  }
+}
