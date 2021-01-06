c29228f644fe6b5ed75fd3769acad305fa0cd794
Will Molloy <willjoemolloy@gmail.com>
2021-01-06 13:29:02 +1300

    leetcode P1010PairsOfSongsWithTotalDurationsDivisibleBy60 (medium)
    GitOrigin-RevId: af0ada9a11af328982234b77d94787609c4a01e5

---
+++ leetcode/src/main/java/com/wilmol/leetcode/problemset/algorithms/medium/P1010PairsOfSongsWithTotalDurationsDivisibleBy60.java
@@ -0,0 +1,30 @@
+package com.wilmol.leetcode.problemset.algorithms.medium;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <a
+ * href=https://leetcode.com/problems/pairs-of-songs-with-total-durations-divisible-by-60>https://leetcode.com/problems/pairs-of-songs-with-total-durations-divisible-by-60/</a>
+ *
+ * <p>Runtime: O(n)
+ *
+ * <p>Extra memory: O(n)
+ *
+ * <p>Key: Lookup remainders in hash table.
+ *
+ * @see com.wilmol.leetcode.problemset.algorithms.easy.P1TwoSum
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P1010PairsOfSongsWithTotalDurationsDivisibleBy60 {
+
+  public int numPairsDivisibleBy60(int[] time) {
+    int count = 0;
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int t : time) {
+      count += map.getOrDefault((60 - t % 60) % 60, 0);
+      map.merge(t % 60, 1, Integer::sum);
+    }
+    return count;
+  }
+}
