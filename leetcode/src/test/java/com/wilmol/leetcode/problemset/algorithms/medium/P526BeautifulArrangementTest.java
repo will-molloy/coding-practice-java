cb91bfce0b0ab33da743206af651f4dbda9d7b0f
Will Molloy <willjoemolloy@gmail.com>
2021-01-06 14:56:57 +1300

    leetcode P526BeautifulArrangement (medium)
    GitOrigin-RevId: a93c9b23468a1debce88f7aaa8f8c5e223eed8c7

---
+++ leetcode/src/test/java/com/wilmol/leetcode/problemset/algorithms/medium/P526BeautifulArrangementTest.java
@@ -0,0 +1,34 @@
+package com.wilmol.leetcode.problemset.algorithms.medium;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+/**
+ * P526BeautifulArrangementTest.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P526BeautifulArrangementTest {
+
+  private final P526BeautifulArrangement p526BeautifulArrangement = new P526BeautifulArrangement();
+
+  @ParameterizedTest
+  @MethodSource
+  void examples(int n, int expected) {
+    assertThat(p526BeautifulArrangement.countArrangement(n)).isEqualTo(expected);
+  }
+
+  static Stream<Arguments> examples() {
+    return Stream.of(Arguments.of(2, 2), Arguments.of(1, 1));
+  }
+
+  @Test
+  void maxN() {
+    assertThat(p526BeautifulArrangement.countArrangement(15)).isEqualTo(24679);
+  }
+}
