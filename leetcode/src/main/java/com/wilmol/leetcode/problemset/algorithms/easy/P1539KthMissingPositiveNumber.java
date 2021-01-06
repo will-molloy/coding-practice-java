efb2366b6d64098f09bf51dbdf35b754bab26466
Will Molloy <willjoemolloy@gmail.com>
2021-01-07 11:23:44 +1300

    leetcode P1539KthMissingPositiveNumber (easy)
    GitOrigin-RevId: 3b09b89b35eaac29424529fe56c45f152201742e

---
+++ leetcode/src/main/java/com/wilmol/leetcode/problemset/algorithms/easy/P1539KthMissingPositiveNumber.java
@@ -0,0 +1,34 @@
+package com.wilmol.leetcode.problemset.algorithms.easy;
+
+/**
+ * <a
+ * href=https://leetcode.com/problems/kth-missing-positive-number>https://leetcode.com/problems/kth-missing-positive-number/</a>
+ *
+ * <p>Runtime: O(n) TODO log(n) binary search is possible
+ *
+ * <p>Extra memory: O(1)
+ *
+ * <p>Key: Track next positive number while scanning the array.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P1539KthMissingPositiveNumber {
+
+  public int findKthPositive(int[] arr, int k) {
+    int next = 1;
+    int count = 0;
+    int i = 0;
+
+    while (true) {
+      while (i == arr.length || next != arr[i]) {
+        count++;
+        if (count == k) {
+          return next;
+        }
+        next++;
+      }
+      next++;
+      i++;
+    }
+  }
+}
