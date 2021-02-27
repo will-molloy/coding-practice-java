90ae9768c62dbfa98c92c71dec99005fc2ecb782
Will Molloy <willjoemolloy@gmail.com>
2021-02-27 19:43:45 +1300

    leetcode P991BrokenCalculator (medium)
    GitOrigin-RevId: aa35fa2c1f2d9af2dee3c1b1e23ade7164dd9570

---
+++ leetcode/src/test/java/com/wilmol/leetcode/problemset/algorithms/medium/P991BrokenCalculatorTest.java
@@ -0,0 +1,30 @@
+package com.wilmol.leetcode.problemset.algorithms.medium;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+/**
+ * P991BrokenCalculatorTest.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P991BrokenCalculatorTest {
+
+  @ParameterizedTest
+  @MethodSource
+  void examples(int x, int y, int expected) {
+    assertThat(new P991BrokenCalculator().brokenCalc(x, y)).isEqualTo(expected);
+  }
+
+  static Stream<Arguments> examples() {
+    return Stream.of(
+        Arguments.of(2, 3, 2),
+        Arguments.of(5, 8, 2),
+        Arguments.of(3, 10, 3),
+        Arguments.of(1024, 1, 1023));
+  }
+}
