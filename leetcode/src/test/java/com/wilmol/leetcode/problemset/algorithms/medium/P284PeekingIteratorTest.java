3be285a0b8bdc0e8bbb861d7e6bd22ef3d88d7a5
Will Molloy <willjoemolloy@gmail.com>
2021-02-28 15:32:07 +1300

    leetcode P284PeekingIterator (medium)
    GitOrigin-RevId: b9a6f55cc2e7f5af6f8fc80ffcbfbdaf33bfca79

---
+++ leetcode/src/test/java/com/wilmol/leetcode/problemset/algorithms/medium/P284PeekingIteratorTest.java
@@ -0,0 +1,26 @@
+package com.wilmol.leetcode.problemset.algorithms.medium;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+/**
+ * P284PeekingIteratorTest.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P284PeekingIteratorTest {
+
+  @Test
+  void example() {
+    P284PeekingIterator.PeekingIterator peekingIterator =
+        new P284PeekingIterator.PeekingIterator(List.of(1, 2, 3).iterator());
+
+    assertThat(peekingIterator.next()).isEqualTo(1);
+    assertThat(peekingIterator.peek()).isEqualTo(2);
+    assertThat(peekingIterator.next()).isEqualTo(2);
+    assertThat(peekingIterator.next()).isEqualTo(3);
+    assertThat(peekingIterator.hasNext()).isFalse();
+  }
+}
