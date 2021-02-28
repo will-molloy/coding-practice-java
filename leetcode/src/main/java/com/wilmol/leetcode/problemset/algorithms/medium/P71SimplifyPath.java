6e288e9a527e84178d984a5750024f7f930d6d26
Will Molloy <willjoemolloy@gmail.com>
2021-02-28 15:24:02 +1300

    leetcode P71SimplifyPath (medium)
    GitOrigin-RevId: 1022c5c03e5d983abb9eb6b51085ac33838173e0

---
+++ leetcode/src/main/java/com/wilmol/leetcode/problemset/algorithms/medium/P71SimplifyPath.java
@@ -0,0 +1,39 @@
+package com.wilmol.leetcode.problemset.algorithms.medium;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Set;
+
+/**
+ * <a
+ * href=https://leetcode.com/problems/simplify-path>https://leetcode.com/problems/simplify-path/</a>
+ *
+ * <p>Runtime: O(n)
+ *
+ * <p>Extra memory: O(n)
+ *
+ * <p>Key: Simulate with stack. Ignore "..", "." and "".
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P71SimplifyPath {
+
+  public String simplifyPath(String path) {
+    Deque<String> stack = new ArrayDeque<>();
+    Set<String> ignore = Set.of("..", ".", "");
+
+    for (String dir : path.split("/")) {
+      if (dir.equals("..") && !stack.isEmpty()) {
+        stack.pop();
+      } else if (!ignore.contains(dir)) {
+        stack.push(dir);
+      }
+    }
+
+    String s = "";
+    for (String dir : stack) {
+      s = "/" + dir + s;
+    }
+    return s.isEmpty() ? "/" : s;
+  }
+}
