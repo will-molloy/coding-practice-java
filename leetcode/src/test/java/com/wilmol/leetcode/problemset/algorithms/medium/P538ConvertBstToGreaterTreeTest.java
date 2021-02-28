0bff3e45f3c3e936e9ae2826a5bef37b933a6319
Will Molloy <willjoemolloy@gmail.com>
2021-02-28 16:20:07 +1300

    leetcode P538ConvertBstToGreaterTree (medium)
    GitOrigin-RevId: 9c2f4996a694ccdf51ff5271f442d8fbd570e5df

---
+++ leetcode/src/test/java/com/wilmol/leetcode/problemset/algorithms/medium/P538ConvertBstToGreaterTreeTest.java
@@ -0,0 +1,40 @@
+package com.wilmol.leetcode.problemset.algorithms.medium;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.wilmol.leetcode.common.BinaryTreeNode;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+/**
+ * P538ConvertBstToGreaterTreeTest.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P538ConvertBstToGreaterTreeTest {
+
+  private final P538ConvertBstToGreaterTree p538 = new P538ConvertBstToGreaterTree();
+
+  @ParameterizedTest
+  @MethodSource
+  void examples(BinaryTreeNode root, BinaryTreeNode expected) {
+    assertThat(p538.convertBst(root)).isEqualTo(expected);
+  }
+
+  static Stream<Arguments> examples() {
+    return Stream.of(
+        Arguments.of(
+            BinaryTreeNode.fromLevelOrder(
+                4, 1, 6, 0, 2, 5, 7, null, null, null, 3, null, null, null, 8),
+            BinaryTreeNode.fromLevelOrder(
+                30, 36, 21, 36, 35, 26, 15, null, null, null, 33, null, null, null, 8)),
+        Arguments.of(
+            BinaryTreeNode.fromLevelOrder(0, null, 1), BinaryTreeNode.fromLevelOrder(1, null, 1)),
+        Arguments.of(
+            BinaryTreeNode.fromLevelOrder(1, 0, 2), BinaryTreeNode.fromLevelOrder(3, 3, 2)),
+        Arguments.of(
+            BinaryTreeNode.fromLevelOrder(3, 2, 4, 1), BinaryTreeNode.fromLevelOrder(7, 9, 4, 10)));
+  }
+}
