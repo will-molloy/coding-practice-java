a863d65527ccb5ba03667de0cb00971c33d5cf70
Will Molloy <willjoemolloy@gmail.com>
2021-02-28 15:17:04 +1300

    leetcode P594LongestHarmoniousSubsequence (easy)
    GitOrigin-RevId: dd19c7fee6e006735dc4f46c0cd886e06df18e71

---
+++ leetcode/src/test/java/com/wilmol/leetcode/problemset/algorithms/easy/P594LongestHarmoniousSubsequenceTest.java
@@ -0,0 +1,32 @@
+package com.wilmol.leetcode.problemset.algorithms.easy;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+/**
+ * P594LongestHarmoniousSubsequenceTest.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P594LongestHarmoniousSubsequenceTest {
+
+  private final P594LongestHarmoniousSubsequence p594LongestHarmoniousSubsequence =
+      new P594LongestHarmoniousSubsequence();
+
+  @ParameterizedTest
+  @MethodSource
+  void examples(int[] nums, int expected) {
+    assertThat(p594LongestHarmoniousSubsequence.findLhs(nums)).isEqualTo(expected);
+  }
+
+  static Stream<Arguments> examples() {
+    return Stream.of(
+        Arguments.of(new int[] {1, 3, 2, 2, 5, 2, 3, 7}, 5),
+        Arguments.of(new int[] {1, 2, 3, 4}, 2),
+        Arguments.of(new int[] {1, 1, 1, 1}, 0));
+  }
+}
