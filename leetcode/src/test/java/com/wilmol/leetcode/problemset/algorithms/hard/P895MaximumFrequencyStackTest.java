7ab63bb89db8d5177b7e0b0558059a3ae0232fe9
Will Molloy <willjoemolloy@gmail.com>
2021-03-01 16:45:47 +1300

    leetcode P895MaximumFrequencyStack (hard)
    GitOrigin-RevId: 2b851655b2e066c24f6e924af31e82ed0b1e18f0

---
+++ leetcode/src/test/java/com/wilmol/leetcode/problemset/algorithms/hard/P895MaximumFrequencyStackTest.java
@@ -0,0 +1,32 @@
+package com.wilmol.leetcode.problemset.algorithms.hard;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * P895MaximumFrequencyStackTest.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P895MaximumFrequencyStackTest {
+
+  @Test
+  void example() {
+    P895MaximumFrequencyStack.FreqStack freqStack = new P895MaximumFrequencyStack.FreqStack();
+
+    freqStack.push(5);
+    freqStack.push(7);
+    freqStack.push(5);
+    freqStack.push(7);
+    freqStack.push(4);
+    freqStack.push(5);
+
+    assertThat(freqStack.pop()).isEqualTo(5);
+    assertThat(freqStack.pop()).isEqualTo(7);
+    assertThat(freqStack.pop()).isEqualTo(5);
+    assertThat(freqStack.pop()).isEqualTo(4);
+    assertThat(freqStack.pop()).isEqualTo(7);
+    assertThat(freqStack.pop()).isEqualTo(5);
+  }
+}
