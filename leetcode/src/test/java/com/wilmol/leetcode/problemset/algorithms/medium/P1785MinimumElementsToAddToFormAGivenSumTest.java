77185b8bd9cef45fa9989cc3d7c8d806a48222a2
Will Molloy <willjoemolloy@gmail.com>
2021-03-09 20:25:50 +1300

    leetcode P1785MinimumElementsToAddToFormAGivenSum (medium)
    Weekly Contest 231.
    
    GitOrigin-RevId: a393449525d01575597ab8237a6bda0af054116c

---
+++ leetcode/src/test/java/com/wilmol/leetcode/problemset/algorithms/medium/P1785MinimumElementsToAddToFormAGivenSumTest.java
@@ -0,0 +1,32 @@
+package com.wilmol.leetcode.problemset.algorithms.medium;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+/**
+ * P1785MinimumElementsToAddToFormAGivenSumTest.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P1785MinimumElementsToAddToFormAGivenSumTest {
+
+  private final P1785MinimumElementsToAddToFormAGivenSum p1785MinimumElementsToAddToFormAGivenSum =
+      new P1785MinimumElementsToAddToFormAGivenSum();
+
+  @ParameterizedTest
+  @MethodSource
+  void examples(int[] nums, int limit, int goal, int expected) {
+    assertThat(p1785MinimumElementsToAddToFormAGivenSum.minElements(nums, limit, goal))
+        .isEqualTo(expected);
+  }
+
+  static Stream<Arguments> examples() {
+    return Stream.of(
+        Arguments.of(new int[] {1, -1, 1}, 3, -4, 2),
+        Arguments.of(new int[] {1, -10, 9, 1}, 100, 0, 1));
+  }
+}
