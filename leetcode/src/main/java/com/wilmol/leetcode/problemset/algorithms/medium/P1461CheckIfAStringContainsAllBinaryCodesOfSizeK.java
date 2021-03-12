c54f19c664bc8037f45f2c28b6b670f861d6eaae
Will Molloy <willjoemolloy@gmail.com>
2021-03-13 11:58:28 +1300

    leetcode P1461CheckIfAStringContainsAllBinaryCodesOfSizeK (medium)
    GitOrigin-RevId: f981f1c31ebfb931e82e70a2b12e5d7ca7c218db

---
+++ leetcode/src/main/java/com/wilmol/leetcode/problemset/algorithms/medium/P1461CheckIfAStringContainsAllBinaryCodesOfSizeK.java
@@ -0,0 +1,33 @@
+package com.wilmol.leetcode.problemset.algorithms.medium;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * <a
+ * href=https://leetcode.com/problems/check-if-a-string-contains-all-binary-codes-of-size-k>https://leetcode.com/problems/check-if-a-string-contains-all-binary-codes-of-size-k/</a>
+ *
+ * <p>Runtime: O(s * k) TODO rolling hash is O(s)
+ *
+ * <p>Extra memory: O(2<sup>k</sup> * k)
+ *
+ * <p>Key: Need all binary substrings of length k, which there are 2<sup>k</sup> of. So keep taking
+ * unique substrings until we have that many. Set/hashing is perfect for this.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P1461CheckIfAStringContainsAllBinaryCodesOfSizeK {
+
+  public boolean hasAllCodes(String s, int k) {
+    int requiredSize = 1 << k;
+
+    Set<String> set = new HashSet<>();
+    for (int i = 0; i <= s.length() - k; i++) {
+      String sub = s.substring(i, i + k);
+      if (set.add(sub) && set.size() == requiredSize) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
