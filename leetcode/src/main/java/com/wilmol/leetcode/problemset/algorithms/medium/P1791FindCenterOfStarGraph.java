758c806992040720f519f48ae40f432f9a79a706
Will Molloy <willjoemolloy@gmail.com>
2021-03-15 15:37:35 +1300

    leetcode P1791FindCenterOfStarGraph (medium)
    Weekly Contest 232.
    
    GitOrigin-RevId: ed2df906c75d36b013111048d515b15698bb9bfc

---
+++ leetcode/src/main/java/com/wilmol/leetcode/problemset/algorithms/medium/P1791FindCenterOfStarGraph.java
@@ -0,0 +1,32 @@
+package com.wilmol.leetcode.problemset.algorithms.medium;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * <a
+ * href=https://leetcode.com/problems/find-center-of-star-graph>https://leetcode.com/problems/find-center-of-star-graph/</a>
+ *
+ * <p>Runtime: O(1) - exactly 2 iterations over edges
+ *
+ * <p>Extra memory: O(1)
+ *
+ * <p>Key: All edges point to the center node, i.e. the center node is common/duplicate across all
+ * edges.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P1791FindCenterOfStarGraph {
+
+  public int findCenter(int[][] edges) {
+    Set<Integer> set = new HashSet<>();
+    for (int[] edge : edges) {
+      for (int node : edge) {
+        if (!set.add(node)) {
+          return node;
+        }
+      }
+    }
+    throw new AssertionError("Unreachable");
+  }
+}
