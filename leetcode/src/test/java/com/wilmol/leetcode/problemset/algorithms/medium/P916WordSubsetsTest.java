459d51e5c04137ef5aa9f07cccad553c20171fc1
Will Molloy <willjoemolloy@gmail.com>
2021-03-27 10:34:59 +1300

    leetcode P916WordSubsets (medium)
    GitOrigin-RevId: 253317a9a17edb90e27727bce2e710e08066ee2b

---
+++ leetcode/src/test/java/com/wilmol/leetcode/problemset/algorithms/medium/P916WordSubsetsTest.java
@@ -0,0 +1,49 @@
+package com.wilmol.leetcode.problemset.algorithms.medium;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+/**
+ * P916WordSubsetsTest.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P916WordSubsetsTest {
+
+  private final P916WordSubsets p916WordSubsets = new P916WordSubsets();
+
+  @ParameterizedTest
+  @MethodSource
+  void examples(String[] as, String[] bs, List<String> expected) {
+    assertThat(p916WordSubsets.wordSubsets(as, bs)).containsExactlyElementsIn(expected);
+  }
+
+  static Stream<Arguments> examples() {
+    return Stream.of(
+        Arguments.of(
+            new String[] {"amazon", "apple", "facebook", "google", "leetcode"},
+            new String[] {"e", "o"},
+            List.of("facebook", "google", "leetcode")),
+        Arguments.of(
+            new String[] {"amazon", "apple", "facebook", "google", "leetcode"},
+            new String[] {"l", "e"},
+            List.of("apple", "google", "leetcode")),
+        Arguments.of(
+            new String[] {"amazon", "apple", "facebook", "google", "leetcode"},
+            new String[] {"e", "oo"},
+            List.of("facebook", "google")),
+        Arguments.of(
+            new String[] {"amazon", "apple", "facebook", "google", "leetcode"},
+            new String[] {"lo", "eo"},
+            List.of("google", "leetcode")),
+        Arguments.of(
+            new String[] {"amazon", "apple", "facebook", "google", "leetcode"},
+            new String[] {"ec", "oc", "ceo"},
+            List.of("facebook", "leetcode")));
+  }
+}
