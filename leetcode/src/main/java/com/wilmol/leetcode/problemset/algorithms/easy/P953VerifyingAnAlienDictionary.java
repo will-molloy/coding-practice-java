92bbd16544c1774c95226820d163bb326e848dab
Will Molloy <willjoemolloy@gmail.com>
2021-04-11 11:01:46 +1200

    leetcode P953VerifyingAnAlienDictionary (easy)
    GitOrigin-RevId: bf0cd497800a3d293ceaaeae3f971f46feb2df42

---
+++ leetcode/src/main/java/com/wilmol/leetcode/problemset/algorithms/easy/P953VerifyingAnAlienDictionary.java
@@ -0,0 +1,44 @@
+package com.wilmol.leetcode.problemset.algorithms.easy;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <a
+ * href=https://leetcode.com/problems/verifying-an-alien-dictionary>https://leetcode.com/problems/verifying-an-alien-dictionary/</a>
+ *
+ * <p>Runtime: O({@code order} + sum(word : {@code words})
+ *
+ * <p>Extra memory: O({@code order})
+ *
+ * <p>Key: Create {@code Map<Character, Integer>} to encode the alien order. Then its a standard
+ * 'isSorted' algorithm - check adjacent elements are in order.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P953VerifyingAnAlienDictionary {
+
+  public boolean isAlienSorted(String[] words, String order) {
+    Map<Character, Integer> map = new HashMap<>();
+    for (int i = 0; i < order.length(); i++) {
+      map.put(order.charAt(i), i);
+    }
+
+    Comparator<String> comparator =
+        (a, b) -> {
+          for (int i = 0; i < Math.min(a.length(), b.length()); i++) {
+            if (a.charAt(i) != b.charAt(i)) {
+              return Integer.compare(map.get(a.charAt(i)), map.get(b.charAt(i)));
+            }
+          }
+          return Integer.compare(a.length(), b.length());
+        };
+    for (int i = 0; i < words.length - 1; i++) {
+      if (comparator.compare(words[i], words[i + 1]) > 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
