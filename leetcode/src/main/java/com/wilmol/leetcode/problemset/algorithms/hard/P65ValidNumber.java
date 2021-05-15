09170df5f1776a7c7a3eaa7170a63d1ee1843762
Will Molloy <willjoemolloy@gmail.com>
2021-05-16 11:41:57 +1200

    leetcode P65ValidNumber (hard)
    GitOrigin-RevId: 30a6feeeff22ef2d6d71628ed7367c51863fa3b5

---
+++ leetcode/src/main/java/com/wilmol/leetcode/problemset/algorithms/hard/P65ValidNumber.java
@@ -0,0 +1,27 @@
+package com.wilmol.leetcode.problemset.algorithms.hard;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * <a
+ * href=https://leetcode.com/problems/valid-number>https://leetcode.com/problems/valid-number/</a>
+ *
+ * <p>Runtime: O(n)
+ *
+ * <p>Extra memory: O(1)
+ *
+ * <p>Key: Regex.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P65ValidNumber {
+
+  public boolean isNumber(String s) {
+    String decimal = "[+-]?(\\d+[.]|\\d+[.]\\d+|[.]\\d+)";
+    String integer = "[+-]?\\d+";
+    Pattern p = Pattern.compile(String.format("(%s|%s)([eE]%s)?", decimal, integer, integer));
+    Matcher m = p.matcher(s);
+    return m.matches();
+  }
+}
