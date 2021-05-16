19be810ce030a8e40e3c98835f5f6d0ff114e571
Will Molloy <willjoemolloy@gmail.com>
2021-05-16 12:04:13 +1200

    leetcode P816AmbiguousCoordinates (medium)
    GitOrigin-RevId: 3aeadaecf9a270772a8c0a7c3203772f8d753ffb

---
+++ leetcode/src/test/java/com/wilmol/leetcode/problemset/algorithms/medium/P816AmbiguousCoordinatesTest.java
@@ -0,0 +1,36 @@
+package com.wilmol.leetcode.problemset.algorithms.medium;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+/**
+ * P816AmbiguousCoordinatesTest.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P816AmbiguousCoordinatesTest {
+
+  private final P816AmbiguousCoordinates p816AmbiguousCoordinates = new P816AmbiguousCoordinates();
+
+  @ParameterizedTest
+  @MethodSource
+  void examples(String s, List<String> expected) {
+    assertThat(p816AmbiguousCoordinates.ambiguousCoordinates(s))
+        .containsExactlyElementsIn(expected);
+  }
+
+  static Stream<Arguments> examples() {
+    return Stream.of(
+        Arguments.of("(123)", List.of("(1, 2.3)", "(1, 23)", "(1.2, 3)", "(12, 3)")),
+        Arguments.of(
+            "(0123)",
+            List.of("(0, 1.23)", "(0, 12.3)", "(0, 123)", "(0.1, 2.3)", "(0.1, 23)", "(0.12, 3)")),
+        Arguments.of("(00011)", List.of("(0, 0.011)", "(0.001, 1)")),
+        Arguments.of("(100)", List.of("(10, 0)")));
+  }
+}
