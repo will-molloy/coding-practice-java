613d40887683daac167e6d3f9f3cf315a8f661f3
Will Molloy <willjoemolloy@gmail.com>
2021-05-25 21:00:19 +1200

    leetcode P150EvaluateReversePolishNotation (medium)
    GitOrigin-RevId: ad165af50c1ca38950b24431a3164c35f35f62ee

---
+++ leetcode/src/main/java/com/wilmol/leetcode/problemset/algorithms/medium/P150EvaluateReversePolishNotation.java
@@ -0,0 +1,48 @@
+package com.wilmol.leetcode.problemset.algorithms.medium;
+
+import java.util.ArrayDeque;
+
+/**
+ * <a
+ * href=https://leetcode.com/problems/evaluate-reverse-polish-notation>https://leetcode.com/problems/evaluate-reverse-polish-notation/</a>
+ *
+ * <p>Runtime: O(n)
+ *
+ * <p>Extra memory: O(n)
+ *
+ * <p>Key: Stack.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P150EvaluateReversePolishNotation {
+
+  // CHECKSTYLE IGNORE AbbreviationAsWordInName FOR NEXT 1 LINES
+  public int evalRPN(String[] tokens) {
+    ArrayDeque<Integer> stack = new ArrayDeque<>();
+    for (String s : tokens) {
+      if (!s.matches("[+\\-*/]")) {
+        stack.push(Integer.parseInt(s));
+        continue;
+      }
+      int i1 = stack.pop();
+      int i2 = stack.pop();
+      switch (s) {
+        case "+":
+          stack.push(i2 + i1);
+          break;
+        case "-":
+          stack.push(i2 - i1);
+          break;
+        case "*":
+          stack.push(i2 * i1);
+          break;
+        case "/":
+          stack.push(i2 / i1);
+          break;
+        default:
+          throw new IllegalArgumentException();
+      }
+    }
+    return stack.peek();
+  }
+}
