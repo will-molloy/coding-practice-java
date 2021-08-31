f8bb54ca7b9574b4db4ecc3e574ba5cc17b33834
Will Molloy <willjoemolloy@gmail.com>
2021-08-31 16:29:59 +1200

    leetcode P331VerifyPreorderSerializationOfABinaryTree (medium)
    GitOrigin-RevId: 3f88894898cdd394c92970a3d2c7781257f1a853

---
+++ leetcode/src/test/java/com/wilmol/leetcode/problemset/algorithms/medium/P331VerifyPreorderSerializationOfABinaryTreeTest.java
@@ -0,0 +1,34 @@
+package com.wilmol.leetcode.problemset.algorithms.medium;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+/**
+ * P331VerifyPreorderSerializationOfABinaryTreeTest.
+ *
+ * @author <a href=https://wilmol.com>Will Molloy</a>
+ */
+class P331VerifyPreorderSerializationOfABinaryTreeTest {
+
+  private final P331VerifyPreorderSerializationOfABinaryTree
+      p331VerifyPreorderSerializationOfABinaryTree =
+          new P331VerifyPreorderSerializationOfABinaryTree();
+
+  @ParameterizedTest
+  @MethodSource
+  void examples(String preorder, boolean expected) {
+    assertThat(p331VerifyPreorderSerializationOfABinaryTree.isValidSerialization(preorder))
+        .isEqualTo(expected);
+  }
+
+  static Stream<Arguments> examples() {
+    return Stream.of(
+        Arguments.of("9,3,4,#,#,1,#,#,2,#,6,#,#", true),
+        Arguments.of("1,#", false),
+        Arguments.of("9,#,#,1", false));
+  }
+}
