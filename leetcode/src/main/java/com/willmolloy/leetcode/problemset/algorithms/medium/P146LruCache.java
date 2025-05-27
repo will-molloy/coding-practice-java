5bdc67de3bc6576e68275f80dbe6f4f378bfae89
Will Molloy <willjoemolloy@gmail.com>
2025-05-27 22:23:53 +1200

    leetcode: LRU Cache - doubly-linked list approach
    GitOrigin-RevId: 970294e6a1460e2db2e75734cfc8b960305c25b8

--- leetcode/src/main/java/com/willmolloy/leetcode/problemset/algorithms/medium/P146LruCache.java
+++ leetcode/src/main/java/com/willmolloy/leetcode/problemset/algorithms/medium/P146LruCache.java
@@ -1,50 +1,108 @@
 package com.willmolloy.leetcode.problemset.algorithms.medium;
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
-import java.util.LinkedHashMap;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * <a href=https://leetcode.com/problems/lru-cache>https://leetcode.com/problems/lru-cache</a>
+ * <a href=https://leetcode.com/problems/lru-cache>LRU Cache.</a>
  *
- * <p>Key idea: map for O(1) operations + linked list to track least recently used
+ * <p>Key idea: hashmap + doubly linked-list. Maintain LRU as either head or tail.
+ *
+ * <p>Helps to draw out linked-list pointers, to understand how to manipulate them.
+ *
+ * <p>Note you could also use a {@link java.util.LinkedHashMap}.
+ *
+ * <p>Complexity: O(1) time, O(n) space.
  *
  * @author <a href=https://willmolloy.com>Will Molloy</a>
  */
 final class P146LruCache {
 
+  /** Doubly linked-list node. */
+  private static final class Node {
+    private final int key;
+    private final int value;
+    private Node next;
+    private Node prev;
+
+    Node(int key, int value) {
+      this.key = key;
+      this.value = value;
+    }
+  }
+
   private final int capacity;
 
-  // in an interview may be required to implement our own linked map...
-  private final LinkedHashMap<Integer, Integer> map = new LinkedHashMap<>();
+  // map allows O(1) get/put
+  private final Map<Integer, Node> map;
 
-  @SuppressFBWarnings("CT_CONSTRUCTOR_THROW")
+  // maintain tail as LRU - allowing O(1) eviction
+  // (head/tail created as dummy nodes to avoid null checks)
+  private final Node head;
+  private final Node tail;
+
   P146LruCache(int capacity) {
     if (capacity <= 0) {
       throw new IllegalArgumentException(String.format("Invalid capacity: %s", capacity));
     }
     this.capacity = capacity;
+    map = new HashMap<>();
+    head = new Node(0, 0);
+    tail = new Node(0, 0);
+    head.next = tail;
+    tail.prev = head;
   }
 
-  // O(1)
   public int get(int key) {
-    int value = map.getOrDefault(key, -1);
-    if (value != -1) {
-      // refresh key
-      map.remove(key);
-      map.put(key, value);
+    Node node = map.get(key);
+    if (node != null) {
+      // move node to front, in MRU position
+      removeNode(node);
+      addNodeToFront(node);
+      return node.value;
+    } else {
+      return -1;
     }
-    return value;
   }
 
-  // O(1)
   public void put(int key, int value) {
-    if (map.containsKey(key)) {
-      // remove existing key so its refreshed
-      map.remove(key);
-    } else if (map.size() == capacity) {
-      // evict head
-      map.remove(map.keySet().iterator().next());
+    Node node = map.get(key);
+    if (node != null) {
+      // remove existing node, we'll update it by inserting a new one
+      removeNode(node);
     }
-    map.put(key, value);
+
+    // insert node in MRU position
+    Node newNode = new Node(key, value);
+    map.put(key, newNode);
+    addNodeToFront(newNode);
+
+    if (map.size() > capacity) {
+      // evict the LRU
+      Node lru = tail.prev;
+      removeNode(lru);
+      map.remove(lru.key);
+    }
+  }
+
+  private void removeNode(Node node) {
+    // have: prev <-> node <-> next
+    // want: prev <-> next
+    Node next = node.next;
+    Node prev = node.prev;
+    next.prev = prev;
+    prev.next = next;
+    node.next = null;
+    node.prev = null;
+  }
+
+  private void addNodeToFront(Node node) {
+    // have: head <-> next
+    // want: head <-> node <-> next
+    Node next = head.next;
+    head.next = node;
+    next.prev = node;
+    node.next = next;
+    node.prev = head;
   }
 }
