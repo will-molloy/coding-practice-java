5464f5d68d7a495e63130933fa7cf44fc415abad
Will Molloy <willjoemolloy@gmail.com>
2025-12-06 09:30:40 +1100

    adventofcode 2025 Day 5
    GitOrigin-RevId: 1380e700f63edd9fc1e9805130933a11703dfc80

--- advent-of-code/src/main/kotlin/com/willmolloy/adventofcode/_2025/Day5.kt
+++ advent-of-code/src/main/kotlin/com/willmolloy/adventofcode/_2025/Day5.kt
@@ -2,15 +2,54 @@ package com.willmolloy.adventofcode._2025
 
 import com.willmolloy.adventofcode.common.Day
 import com.willmolloy.adventofcode.common.Input
+import kotlin.math.max
 
 /** https://adventofcode.com/2025/day/5 */
 object Day5 : Day(2025, 5) {
 
   override fun part1(input: Input): Any {
-    return 1111111111111111
+    val ranges = getMergedRanges(input)
+
+    return input
+      .readLines()
+      .filterNot { it.contains('-') || it.isBlank() }
+      .map { it.toLong() }
+      .count { ranges.any { range -> range.contains(it) } }
   }
 
   override fun part2(input: Input): Any {
-    return 2222222222222222
+    return getMergedRanges(input).sumOf { it.size }
   }
+
+  private fun getMergedRanges(input: Input): List<LongRange> {
+    val ranges =
+      input
+        .readLines()
+        .filter { it.contains('-') }
+        .map { line ->
+          val split = line.split('-')
+          split[0].toLong()..split[1].toLong()
+        }
+
+    // merge ranges
+    val sorted = ranges.sortedBy { it.start }.toMutableList()
+
+    val merged = mutableListOf<LongRange>()
+
+    for (range in sorted) {
+      if (merged.isEmpty() || range.start > merged.last().endInclusive) {
+        // non-overlapping case
+        merged.add(range)
+      } else {
+        // overlapping case
+        merged[merged.size - 1] =
+          merged.last().start..max(merged.last().endInclusive, range.endInclusive)
+      }
+    }
+
+    return merged
+  }
+
+  private val LongRange.size: Long
+    get() = endInclusive - start + 1
 }
--- advent-of-code/src/test/kotlin/com/willmolloy/adventofcode/_2025/Day5Test.kt
+++ advent-of-code/src/test/kotlin/com/willmolloy/adventofcode/_2025/Day5Test.kt
@@ -4,7 +4,7 @@ import com.willmolloy.adventofcode.common.DayTest
 
 object Day5Test : DayTest(Day5) {
 
-  override fun part1() = Answer(1111111111111111, 1111111111111111)
+  override fun part1() = Answer(3, 529)
 
-  override fun part2() = Answer(2222222222222222, 2222222222222222)
+  override fun part2() = Answer(14, 344260049617193)
 }
