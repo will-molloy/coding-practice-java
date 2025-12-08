dd21bcba6ad7b6918c35559b05e140853c0a2424
Will Molloy <willjoemolloy@gmail.com>
2025-12-08 18:02:04 +1100

    adventofcode 2025 Day 8
    Disjoint set.
    
    GitOrigin-RevId: f73e216a70a96a373fba7c85909d8796b86778c3

---
+++ advent-of-code/src/test/kotlin/com/willmolloy/adventofcode/_2025/Day8Test.kt
@@ -0,0 +1,10 @@
+package com.willmolloy.adventofcode._2025
+
+import com.willmolloy.adventofcode.common.DayTest
+
+object Day8Test : DayTest(Day8) {
+
+  override fun part1() = Answer(40, 29406)
+
+  override fun part2() = Answer(25272, 7499461416)
+}
