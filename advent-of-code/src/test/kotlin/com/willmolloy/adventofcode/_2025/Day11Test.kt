61ffa43e2bb8694740554f3ac71e2286630f2500
Will Molloy <willjoemolloy@gmail.com>
2025-12-11 16:51:52 +1100

    Revert "adventofcode 2025 Day 11 try optimise with bitset"
    This reverts commit 2fa5c677d5580cf62021dd3ffb37db413f33b1bb.
    
    GitOrigin-RevId: 5699b26dcbb93a0e2f887e52c4fda2ee16e74b43

--- advent-of-code/src/test/kotlin/com/willmolloy/adventofcode/_2025/Day11Test.kt
+++ advent-of-code/src/test/kotlin/com/willmolloy/adventofcode/_2025/Day11Test.kt
@@ -4,8 +4,7 @@ import com.willmolloy.adventofcode.common.DayTest
 
 object Day11Test : DayTest(Day11) {
 
-  override fun part1() = Answer(5, 791)
+  override fun part1() = Answer(5, 1111111111111111)
 
-  // TODO the example inputs differ today...
   override fun part2() = Answer(2, 2222222222222222)
 }
